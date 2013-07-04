025480108f8ecfd07ca1bdccb6432cf46097e4df
boris_van_sosin <boris.van.sosin@gmail.com>
2013-07-04 15:53:12 +0300

    Configured feature

--- SpartanRefactoring/test/il/ac/technion/cs/ssdl/spartan/refactoring/SpartanizationTests.java
+++ SpartanRefactoring/test/il/ac/technion/cs/ssdl/spartan/refactoring/SpartanizationTests.java
@@ -16,243 +16,248 @@ import org.eclipse.jface.text.Document;
 import org.eclipse.text.edits.MalformedTreeException;
 import org.junit.Test;
 
+/**
+ * @author Boris van Sosin
+ *
+ */
+@SuppressWarnings({"javadoc", "static-method"})
 public class SpartanizationTests {
-  @SuppressWarnings("static-method") @Test public void testConvertToTernary1() throws IOException, IllegalArgumentException,
+  @Test public void testConvertToTernary1() throws IOException, IllegalArgumentException,
       MalformedTreeException, BadLocationException {
     runTestCase(new ConvertToTernaryRefactoring(), "ConvertToTernary1");
   }
   
-  @SuppressWarnings("static-method") @Test public void testConvertToTernary2() throws IOException, IllegalArgumentException,
+  @Test public void testConvertToTernary2() throws IOException, IllegalArgumentException,
       MalformedTreeException, BadLocationException {
     runTestCase(new ConvertToTernaryRefactoring(), "ConvertToTernary2");
   }
   
-  @SuppressWarnings("static-method") @Test public void testConvertToTernary3() throws IOException, IllegalArgumentException,
+  @Test public void testConvertToTernary3() throws IOException, IllegalArgumentException,
       MalformedTreeException, BadLocationException {
     runTestCase(new ConvertToTernaryRefactoring(), "ConvertToTernary3");
   }
   
-  @SuppressWarnings("static-method") @Test public void testConvertToTernary4() throws IOException, IllegalArgumentException,
+  @Test public void testConvertToTernary4() throws IOException, IllegalArgumentException,
       MalformedTreeException, BadLocationException {
     runTestCase(new ConvertToTernaryRefactoring(), "ConvertToTernary4");
   }
   
-  @SuppressWarnings("static-method") @Test public void testConvertToTernary5() throws IOException, IllegalArgumentException,
+  @Test public void testConvertToTernary5() throws IOException, IllegalArgumentException,
       MalformedTreeException, BadLocationException {
     runTestCaseNoChange(new ConvertToTernaryRefactoring(), "ConvertToTernary5");
   }
   
-  @SuppressWarnings("static-method") @Test public void testConvertToTernary6() throws IOException, IllegalArgumentException,
+  @Test public void testConvertToTernary6() throws IOException, IllegalArgumentException,
       MalformedTreeException, BadLocationException {
     runTestCase(new ConvertToTernaryRefactoring(), "ConvertToTernary6");
   }
   
-  @SuppressWarnings("static-method") @Test public void testConvertToTernary7() throws IOException, IllegalArgumentException,
+  @Test public void testConvertToTernary7() throws IOException, IllegalArgumentException,
       MalformedTreeException, BadLocationException {
     runTestCaseNoChange(new ConvertToTernaryRefactoring(), "ConvertToTernary7");
   }
   
-  @SuppressWarnings("static-method") @Test public void testConvertToTernary8() throws IOException, IllegalArgumentException,
+  @Test public void testConvertToTernary8() throws IOException, IllegalArgumentException,
       MalformedTreeException, BadLocationException {
     runTestCase(new ConvertToTernaryRefactoring(), "ConvertToTernary8");
   }
   
-  @SuppressWarnings("static-method") @Test public void testConvertToTernary9() throws IOException, IllegalArgumentException,
+  @Test public void testConvertToTernary9() throws IOException, IllegalArgumentException,
       MalformedTreeException, BadLocationException {
     runTestCase(new ConvertToTernaryRefactoring(), "ConvertToTernary9");
   }
   
-  @SuppressWarnings("static-method") @Test public void testConvertToTernary10() throws IOException, IllegalArgumentException,
+  @Test public void testConvertToTernary10() throws IOException, IllegalArgumentException,
       MalformedTreeException, BadLocationException {
     runTestCaseNoChange(new ConvertToTernaryRefactoring(), "ConvertToTernary10");
   }
   
-  @SuppressWarnings("static-method") @Test public void testConvertToTernary11() throws IOException, IllegalArgumentException,
+  @Test public void testConvertToTernary11() throws IOException, IllegalArgumentException,
       MalformedTreeException, BadLocationException {
     runTestCase(new ConvertToTernaryRefactoring(), "ConvertToTernary11");
   }
   
-  @SuppressWarnings("static-method") @Test public void testConvertToTernary12() throws IOException, IllegalArgumentException,
+  @Test public void testConvertToTernary12() throws IOException, IllegalArgumentException,
       MalformedTreeException, BadLocationException {
     runTestCaseNoChange(new ConvertToTernaryRefactoring(), "ConvertToTernary12");
   }
   
-  @SuppressWarnings("static-method") @Test public void testForwardDeclaration1() throws IOException, IllegalArgumentException,
+  @Test public void testForwardDeclaration1() throws IOException, IllegalArgumentException,
       MalformedTreeException, BadLocationException {
     runTestCase(new ForwardDeclarationRefactoring(), "ForwardDeclaration1");
   }
   
-  @SuppressWarnings("static-method") @Test public void testForwardDeclaration2() throws IOException, IllegalArgumentException,
+  @Test public void testForwardDeclaration2() throws IOException, IllegalArgumentException,
       MalformedTreeException, BadLocationException {
     runTestCase(new ForwardDeclarationRefactoring(), "ForwardDeclaration2");
   }
   
-  @SuppressWarnings("static-method") @Test public void testForwardDeclaration3() throws IOException, IllegalArgumentException,
+  @Test public void testForwardDeclaration3() throws IOException, IllegalArgumentException,
       MalformedTreeException, BadLocationException {
     runTestCase(new ForwardDeclarationRefactoring(), "ForwardDeclaration3");
   }
   
-  @SuppressWarnings("static-method") @Test public void testForwardDeclaration4() throws IOException, IllegalArgumentException,
+  @Test public void testForwardDeclaration4() throws IOException, IllegalArgumentException,
       MalformedTreeException, BadLocationException {
     runTestCase(new ForwardDeclarationRefactoring(), "ForwardDeclaration4");
   }
   
-  @SuppressWarnings("static-method") @Test public void testForwardDeclaration5() throws IOException, IllegalArgumentException,
+  @Test public void testForwardDeclaration5() throws IOException, IllegalArgumentException,
       MalformedTreeException, BadLocationException {
     runTestCase(new ForwardDeclarationRefactoring(), "ForwardDeclaration5");
   }
   
-  @SuppressWarnings("static-method") @Test public void testForwardDeclaration6() throws IOException, IllegalArgumentException,
+  @Test public void testForwardDeclaration6() throws IOException, IllegalArgumentException,
       MalformedTreeException, BadLocationException {
     runTestCase(new ForwardDeclarationRefactoring(), "ForwardDeclaration6");
   }
   
-  @SuppressWarnings("static-method") @Test public void testForwardDeclaration7() throws IOException, IllegalArgumentException,
+  @Test public void testForwardDeclaration7() throws IOException, IllegalArgumentException,
       MalformedTreeException, BadLocationException {
     runTestCase(new ForwardDeclarationRefactoring(), "ForwardDeclaration7");
   }
   
-  @SuppressWarnings("static-method") @Test public void testInlineSingleUse1() throws IOException, IllegalArgumentException,
+  @Test public void testInlineSingleUse1() throws IOException, IllegalArgumentException,
       MalformedTreeException, BadLocationException {
     runTestCase(new InlineSingleUseRefactoring(), "InlineSingleUse1");
   }
   
-  @SuppressWarnings("static-method") @Test public void testInlineSingleUse2() throws IOException, IllegalArgumentException,
+  @Test public void testInlineSingleUse2() throws IOException, IllegalArgumentException,
       MalformedTreeException, BadLocationException {
     runTestCase(new InlineSingleUseRefactoring(), "InlineSingleUse2");
   }
   
-  @SuppressWarnings("static-method") @Test public void testInlineSingleUse3() throws IOException, IllegalArgumentException,
+  @Test public void testInlineSingleUse3() throws IOException, IllegalArgumentException,
       MalformedTreeException, BadLocationException {
     runTestCase(new InlineSingleUseRefactoring(), "InlineSingleUse3");
   }
   
-  @SuppressWarnings("static-method") @Test public void testInlineSingleUse4() throws IOException, IllegalArgumentException,
+  @Test public void testInlineSingleUse4() throws IOException, IllegalArgumentException,
       MalformedTreeException, BadLocationException {
     runTestCase(new InlineSingleUseRefactoring(), "InlineSingleUse4");
   }
   
-  @SuppressWarnings("static-method") @Test public void testInlineSingleUse5() throws IOException, IllegalArgumentException,
+  @Test public void testInlineSingleUse5() throws IOException, IllegalArgumentException,
       MalformedTreeException, BadLocationException {
     runTestCase(new InlineSingleUseRefactoring(), "InlineSingleUse5");
   }
   
-  @SuppressWarnings("static-method") @Test public void testInlineSingleUse6() throws IOException, IllegalArgumentException,
+  @Test public void testInlineSingleUse6() throws IOException, IllegalArgumentException,
       MalformedTreeException, BadLocationException {
     runTestCaseNoChange(new InlineSingleUseRefactoring(), "InlineSingleUse6");
   }
   
-  @SuppressWarnings("static-method") @Test public void testInlineSingleUse7() throws IOException, IllegalArgumentException,
+  @Test public void testInlineSingleUse7() throws IOException, IllegalArgumentException,
       MalformedTreeException, BadLocationException {
     runTestCaseNoChange(new InlineSingleUseRefactoring(), "InlineSingleUse7");
   }
   
-  @SuppressWarnings("static-method") @Test public void testInlineSingleUse8() throws IOException, IllegalArgumentException,
+  @Test public void testInlineSingleUse8() throws IOException, IllegalArgumentException,
       MalformedTreeException, BadLocationException {
     runTestCaseNoChange(new InlineSingleUseRefactoring(), "InlineSingleUse8");
   }
   
-  @SuppressWarnings("static-method") @Test public void testRedundantEquality1() throws IOException, IllegalArgumentException,
+  @Test public void testRedundantEquality1() throws IOException, IllegalArgumentException,
       MalformedTreeException, BadLocationException {
     runTestCase(new RedundantEqualityRefactoring(), "RedundantEquality1");
   }
   
-  @SuppressWarnings("static-method") @Test public void testRedundantEquality2() throws IOException, IllegalArgumentException,
+  @Test public void testRedundantEquality2() throws IOException, IllegalArgumentException,
       MalformedTreeException, BadLocationException {
     runTestCase(new RedundantEqualityRefactoring(), "RedundantEquality2");
   }
   
-  @SuppressWarnings("static-method") @Test public void testRedundantEquality3() throws IOException, IllegalArgumentException,
+  @Test public void testRedundantEquality3() throws IOException, IllegalArgumentException,
       MalformedTreeException, BadLocationException {
     runTestCase(new RedundantEqualityRefactoring(), "RedundantEquality3");
   }
   
-  @SuppressWarnings("static-method") @Test public void testShortestBranchFirst1() throws IOException, IllegalArgumentException,
+  @Test public void testShortestBranchFirst1() throws IOException, IllegalArgumentException,
       MalformedTreeException, BadLocationException {
     runTestCase(new ShortestBranchRefactoring(), "ShortestBranchFirst1");
   }
   
-  @SuppressWarnings("static-method") @Test public void testShortestBranchFirst2() throws IOException, IllegalArgumentException,
+  @Test public void testShortestBranchFirst2() throws IOException, IllegalArgumentException,
       MalformedTreeException, BadLocationException {
     runTestCase(new ShortestBranchRefactoring(), "ShortestBranchFirst2");
   }
   
-  @SuppressWarnings("static-method") @Test public void testShortestBranchFirst3() throws IOException, IllegalArgumentException,
+  @Test public void testShortestBranchFirst3() throws IOException, IllegalArgumentException,
       MalformedTreeException, BadLocationException {
     runTestCase(new ShortestBranchRefactoring(), "ShortestBranchFirst3");
   }
   
-  @SuppressWarnings("static-method") @Test public void testShortestBranchFirst4() throws IOException, IllegalArgumentException,
+  @Test public void testShortestBranchFirst4() throws IOException, IllegalArgumentException,
       MalformedTreeException, BadLocationException {
     runTestCase(new ShortestBranchRefactoring(), "ShortestBranchFirst4");
   }
   
-  @SuppressWarnings("static-method") @Test public void testShortestBranchFirst5() throws IOException, IllegalArgumentException,
+  @Test public void testShortestBranchFirst5() throws IOException, IllegalArgumentException,
       MalformedTreeException, BadLocationException {
     runTestCase(new ShortestBranchRefactoring(), "ShortestBranchFirst5");
   }
   
-  @SuppressWarnings("static-method") @Test public void testShortestBranchFirst6() throws IOException, IllegalArgumentException,
+  @Test public void testShortestBranchFirst6() throws IOException, IllegalArgumentException,
       MalformedTreeException, BadLocationException {
     runTestCase(new ShortestBranchRefactoring(), "ShortestBranchFirst6");
   }
   
-  @SuppressWarnings("static-method") @Test public void testShortestBranchFirst7() throws IOException, IllegalArgumentException,
+  @Test public void testShortestBranchFirst7() throws IOException, IllegalArgumentException,
       MalformedTreeException, BadLocationException {
     runTestCase(new ShortestBranchRefactoring(), "ShortestBranchFirst7");
   }
   
-  @SuppressWarnings("static-method") @Test public void testShortestBranchFirst8() throws IOException, IllegalArgumentException,
+  @Test public void testShortestBranchFirst8() throws IOException, IllegalArgumentException,
       MalformedTreeException, BadLocationException {
     runTestCase(new ShortestBranchRefactoring(), "ShortestBranchFirst8");
   }
   
-  @SuppressWarnings("static-method") @Test public void testShortestBranchFirst9() throws IOException, IllegalArgumentException,
+  @Test public void testShortestBranchFirst9() throws IOException, IllegalArgumentException,
       MalformedTreeException, BadLocationException {
     runTestCase(new ShortestBranchRefactoring(), "ShortestBranchFirst9");
   }
   
-  @SuppressWarnings("static-method") @Test public void testChangeReturnVariableToDollar1() throws IOException,
+  @Test public void testChangeReturnVariableToDollar1() throws IOException,
       IllegalArgumentException, MalformedTreeException, BadLocationException {
     runTestCase(new ChangeReturnToDollarRefactoring(), "ChangeReturnVarToDollar1");
   }
   
-  @SuppressWarnings("static-method") @Test public void testChangeReturnVariableToDollar2() throws IOException,
+  @Test public void testChangeReturnVariableToDollar2() throws IOException,
       IllegalArgumentException, MalformedTreeException, BadLocationException {
     runTestCase(new ChangeReturnToDollarRefactoring(), "ChangeReturnVarToDollar2");
   }
   
-  @SuppressWarnings("static-method") @Test public void testChangeReturnVariableToDollar3() throws IOException,
+  @Test public void testChangeReturnVariableToDollar3() throws IOException,
       IllegalArgumentException, MalformedTreeException, BadLocationException {
     runTestCase(new ChangeReturnToDollarRefactoring(), "ChangeReturnVarToDollar3");
   }
   
-  @SuppressWarnings("static-method") @Test public void testChangeReturnVariableToDollar4() throws IOException,
+  @Test public void testChangeReturnVariableToDollar4() throws IOException,
       IllegalArgumentException, MalformedTreeException, BadLocationException {
     runTestCaseNoChange(new ChangeReturnToDollarRefactoring(), "ChangeReturnVarToDollar4");
   }
   
-  @SuppressWarnings("static-method") @Test public void testChangeReturnVariableToDollar5() throws IOException,
+  @Test public void testChangeReturnVariableToDollar5() throws IOException,
       IllegalArgumentException, MalformedTreeException, BadLocationException {
     runTestCase(new ChangeReturnToDollarRefactoring(), "ChangeReturnVarToDollar5");
   }
   
-  @SuppressWarnings("static-method") @Test public void testChangeReturnVariableToDollar6() throws IOException,
+  @Test public void testChangeReturnVariableToDollar6() throws IOException,
       IllegalArgumentException, MalformedTreeException, BadLocationException {
     runTestCase(new ChangeReturnToDollarRefactoring(), "ChangeReturnVarToDollar6");
   }
   
-  @SuppressWarnings("static-method") @Test public void testChangeReturnVariableToDollar7() throws IOException,
+  @Test public void testChangeReturnVariableToDollar7() throws IOException,
       IllegalArgumentException, MalformedTreeException, BadLocationException {
     runTestCase(new ChangeReturnToDollarRefactoring(), "ChangeReturnVarToDollar7");
   }
   
-  @SuppressWarnings("static-method") @Test public void testChangeReturnVariableToDollar8() throws IOException,
+  @Test public void testChangeReturnVariableToDollar8() throws IOException,
       IllegalArgumentException, MalformedTreeException, BadLocationException {
     runTestCase(new ChangeReturnToDollarRefactoring(), "ChangeReturnVarToDollar8");
   }
   
-  @SuppressWarnings("static-method") @Test public void testChangeReturnVariableToDollar9() throws IOException,
+  @Test public void testChangeReturnVariableToDollar9() throws IOException,
       IllegalArgumentException, MalformedTreeException, BadLocationException {
     runTestCaseNoChange(new ChangeReturnToDollarRefactoring(), "ChangeReturnVarToDollar9");
   }
