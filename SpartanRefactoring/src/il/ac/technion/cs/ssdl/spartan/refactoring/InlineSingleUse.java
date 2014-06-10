bf2d5ed34c1d618ed513ba951094fd0e83641785
Yossi Gil <yossi.gil@gmail.com>
2014-06-10 12:52:06 +0300

    Another bug

--- SpartanRefactoring/src/il/ac/technion/cs/ssdl/spartan/refactoring/InlineSingleUse.java
+++ SpartanRefactoring/src/il/ac/technion/cs/ssdl/spartan/refactoring/InlineSingleUse.java
@@ -21,8 +21,12 @@ import org.eclipse.jdt.core.dom.rewrite.ASTRewrite;
 /**
  * @author Artium Nihamkin (original)
  * @author Boris van Sosin <code><boris.van.sosin [at] gmail.com></code> (v2)
- * @author Tomer Zeltzer <code><tomerr90 [at] gmail.com></code> (v3)
- * 
+ * @author Tomer Zeltzer <code><tomerr90 [at] gmail.com></code> (v3) TODO: This
+ *         examples triggers with no justified reason:
+ * <pre>
+ * final int p = s.indexOf(c);
+ * return s.substring(0, p &gt;= 0 ? p : s.length());
+ * </pre>
  * 
  * @since 2013/01/01
  */
@@ -31,38 +35,60 @@ public class InlineSingleUse extends Spartanization {
 	public InlineSingleUse() {
 		super("Inline variable used once", "Inline variable used once");
 	}
-	@Override protected final void fillRewrite(final ASTRewrite r, final AST t, final CompilationUnit cu, final IMarker m) {
+
+	@Override
+	protected final void fillRewrite(final ASTRewrite r, final AST t,
+			final CompilationUnit cu, final IMarker m) {
 		cu.accept(new ASTVisitor() {
-			@Override public boolean visit(final VariableDeclarationFragment n) {
-				if (!inRange(m, n) || !(n.getParent() instanceof VariableDeclarationStatement))
+			@Override
+			public boolean visit(final VariableDeclarationFragment n) {
+				if (!inRange(m, n)
+						|| !(n.getParent() instanceof VariableDeclarationStatement))
 					return true;
 				final SimpleName varName = n.getName();
-				final VariableDeclarationStatement parent = (VariableDeclarationStatement) n.getParent();
-				final List<Expression> uses = Occurrences.USES_SEMANTIC.of(varName).in(parent.getParent());
-				if (1 == uses.size() && (0 != (parent.getModifiers() & Modifier.FINAL)
-						|| 1 == numOfOccur(Occurrences.ASSIGNMENTS, varName, parent.getParent()))) {
-					r.replace(uses.get(0), makeParenthesizedExpression(t, r, n.getInitializer()), null);
+				final VariableDeclarationStatement parent = (VariableDeclarationStatement) n
+						.getParent();
+				final List<Expression> uses = Occurrences.USES_SEMANTIC.of(
+						varName).in(parent.getParent());
+				if (1 == uses.size()
+						&& (0 != (parent.getModifiers() & Modifier.FINAL) || 1 == numOfOccur(
+								Occurrences.ASSIGNMENTS, varName,
+								parent.getParent()))) {
+					r.replace(
+							uses.get(0),
+							makeParenthesizedExpression(t, r,
+									n.getInitializer()), null);
 					r.remove(1 != parent.fragments().size() ? n : parent, null);
 				}
 				return true;
 			}
 		});
 	}
-	@Override protected ASTVisitor fillOpportunities(final List<Range> opportunities) {
+
+	@Override
+	protected ASTVisitor fillOpportunities(final List<Range> opportunities) {
 		return new ASTVisitor() {
-			@Override public boolean visit(final VariableDeclarationFragment node) {
+			@Override
+			public boolean visit(final VariableDeclarationFragment node) {
 				if (!(node.getParent() instanceof VariableDeclarationStatement))
 					return true;
 				final SimpleName varName = node.getName();
-				final VariableDeclarationStatement parent = (VariableDeclarationStatement) node.getParent();
-				if (1 == numOfOccur(Occurrences.USES_SEMANTIC, varName, parent.getParent())
-						&& (0 != (parent.getModifiers() & Modifier.FINAL) || 1 == numOfOccur(Occurrences.ASSIGNMENTS, varName, parent.getParent())))
+				final VariableDeclarationStatement parent = (VariableDeclarationStatement) node
+						.getParent();
+				if (1 == numOfOccur(Occurrences.USES_SEMANTIC, varName,
+						parent.getParent())
+						&& (0 != (parent.getModifiers() & Modifier.FINAL) || 1 == numOfOccur(
+								Occurrences.ASSIGNMENTS, varName,
+								parent.getParent())))
 					opportunities.add(new Range(node));
 				return true;
 			}
 		};
 	}
-	static int numOfOccur(final Occurrences typeOfOccur, final Expression of, final ASTNode in){
-		return typeOfOccur == null || of == null || in == null ? -1 : typeOfOccur.of(of).in(in).size();
+
+	static int numOfOccur(final Occurrences typeOfOccur, final Expression of,
+			final ASTNode in) {
+		return typeOfOccur == null || of == null || in == null ? -1
+				: typeOfOccur.of(of).in(in).size();
 	}
 }
