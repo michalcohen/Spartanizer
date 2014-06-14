81c07d0fae524b5371037e0a4e1acc7a6bb720a4
Yossi Gil <yossi.gil@gmail.com>
2014-06-14 09:39:29 +0300

    Minor demo of how to use {@link }

--- SpartanRefactoring/src/il/ac/technion/cs/ssdl/spartan/refactoring/preferencesTab/SpartanizationPreferencePage.java
+++ SpartanRefactoring/src/il/ac/technion/cs/ssdl/spartan/refactoring/preferencesTab/SpartanizationPreferencePage.java
@@ -10,7 +10,7 @@ import org.eclipse.ui.IWorkbenchPreferencePage;
 /**
  * This class represents a preference page that is contributed to the
  * Preferences dialog. By subclassing <samp>FieldEditorPreferencePage</samp>, we
- * can use the field support built into JFace that allows us to create a page
+ * can use the field support built into {@link JFace} that allows us to create a page
  * that is small and knows how to save, restore and apply itself.
  * <p>
  * This page is used to modify preferences only. They are stored in the
