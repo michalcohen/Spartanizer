4610fc3df2b40bd677e6bf274ae2fb5cb1a5fa87
Tomer Zeltzer <tomerr90@gmail.com>
2014-06-15 18:36:44 +0300

    Deleted some misplaced TODOs and all the bug reporting ones and added test cases to show bugs are fixed

--- SpartanRefactoring/src/il/ac/technion/cs/ssdl/spartan/refactoring/ShortestBranchFirst.java
+++ SpartanRefactoring/src/il/ac/technion/cs/ssdl/spartan/refactoring/ShortestBranchFirst.java
@@ -18,21 +18,22 @@ import org.eclipse.jdt.core.dom.rewrite.ASTRewrite;
  * @author Artium Nihamkin (original)
  * @author Boris van Sosin <code><boris.van.sosin [at] gmail.com></code> (v2)
  * @author Tomer Zeltzer <code><tomerr90 [at] gmail.com></code> (v3)
- *         <P>
- *         TODO: The code looks as if it might try to change the order in "||"
- *         and "&&"; add test cases to make sure this never happens and correct
- *         the code if necessary.
+ * 
  * @since 2013/01/01
  */
 public class ShortestBranchFirst extends Spartanization {
 	/** Instantiates this class */
 	public ShortestBranchFirst() {
-		super("Shortester first",
-		    "Negate the expression of a conditional, and change the order of branches so that shortest branch occurs first");
+		super(
+				"Shortester first",
+				"Negate the expression of a conditional, and change the order of branches so that shortest branch occurs first");
 	}
-	@Override protected final void fillRewrite(final ASTRewrite r, final AST t, final CompilationUnit cu, final IMarker m) {
+
+	@Override
+	protected final void fillRewrite(final ASTRewrite r, final AST t, final CompilationUnit cu, final IMarker m) {
 		cu.accept(new ASTVisitor() {
-			@Override public boolean visit(final IfStatement n) {
+			@Override
+			public boolean visit(final IfStatement n) {
 				if (!inRange(m, n) || !longerFirst(n))
 					return true;
 				final IfStatement newIfStmnt = transpose(n);
@@ -40,7 +41,9 @@ public class ShortestBranchFirst extends Spartanization {
 					r.replace(n, newIfStmnt, null);
 				return true;
 			}
-			@Override public boolean visit(final ConditionalExpression n) {
+
+			@Override
+			public boolean visit(final ConditionalExpression n) {
 				if (!inRange(m, n) || !longerFirst(n))
 					return true;
 				final ParenthesizedExpression newCondExp = transpose(n);
@@ -48,26 +51,29 @@ public class ShortestBranchFirst extends Spartanization {
 					r.replace(n, newCondExp, null);
 				return true;
 			}
+
 			private IfStatement transpose(final IfStatement n) {
 				final Expression negatedOp = negate(t, r, n.getExpression());
 				if (negatedOp == null)
 					return null;
 				final Statement elseStmnt = n.getElseStatement();
-				final Statement thenStatement = n.getThenStatement();
-				if (1 != statementsCount(elseStmnt) || ASTNode.IF_STATEMENT == getBlockSingleStmnt(elseStmnt).getNodeType()) {
-					// TODO: Make sure that you have a test case that covers this block.
+				final Statement thenStmnt = n.getThenStatement();
+				if (1 == statementsCount(elseStmnt) && ASTNode.IF_STATEMENT == getBlockSingleStmnt(elseStmnt).getNodeType()) {
 					final Block newElseBlock = t.newBlock();
 					newElseBlock.statements().add(r.createCopyTarget(elseStmnt));
-					return makeIfStmnt(t, r, negatedOp, newElseBlock, thenStatement);
+					return makeIfStmnt(t, r, negatedOp, newElseBlock,thenStmnt);
 				}
-				return makeIfStmnt(t, r, negatedOp, elseStmnt, thenStatement);
+				return makeIfStmnt(t, r, negatedOp, elseStmnt, thenStmnt);
 			}
+
 			private ParenthesizedExpression transpose(final ConditionalExpression n) {
-				return n == null ? null : makeParenthesizedConditionalExp(t, r, negate(t, r, n.getExpression()), n.getElseExpression(),
-				    n.getThenExpression());
+				return n == null ? null : makeParenthesizedConditionalExp(t, r,
+						negate(t, r, n.getExpression()), n.getElseExpression(),
+						n.getThenExpression());
 			}
 		});
 	}
+
 	/**
 	 * @return a prefix expression that is the negation of the provided
 	 *         expression.
@@ -75,25 +81,33 @@ public class ShortestBranchFirst extends Spartanization {
 	static Expression negate(final AST t, final ASTRewrite r, final Expression e) {
 		if (e instanceof InfixExpression)
 			return tryNegateComparison(t, r, (InfixExpression) e);
-		return e instanceof PrefixExpression ? tryNegatePrefix(r, (PrefixExpression) e) : makePrefixExpression(t, r,
-		    makeParenthesizedExpression(t, r, e), NOT);
+		return e instanceof PrefixExpression ? tryNegatePrefix(r,
+				(PrefixExpression) e) : makePrefixExpression(t, r,
+				makeParenthesizedExpression(t, r, e), NOT);
 	}
+
 	private static Expression tryNegateComparison(final AST ast, final ASTRewrite r, final InfixExpression e) {
 		final Operator op = negate(e.getOperator());
 		if (op == null)
 			return null;
-		return op == CONDITIONAL_AND || op == CONDITIONAL_OR ? makeInfixExpression(ast, r, op, negateExp(ast, r, e.getLeftOperand()),
-		    negateExp(ast, r, e.getRightOperand())) : makeInfixExpression(ast, r, op, e.getLeftOperand(), e.getRightOperand());
+		return op == CONDITIONAL_AND || op == CONDITIONAL_OR ? makeInfixExpression(
+				ast, r, op, negateExp(ast, r, e.getLeftOperand()),
+				negateExp(ast, r, e.getRightOperand())) : makeInfixExpression(
+				ast, r, op, e.getLeftOperand(), e.getRightOperand());
 	}
+
 	private static Expression negateExp(final AST t, final ASTRewrite r, final Expression exp) {
 		if (isInfix(exp))
-			return makePrefixExpression(t, r, makeParenthesizedExpression(t, r, exp), NOT);
-		return isPrefix(exp) && ((PrefixExpression) exp).getOperator().equals(NOT) ? (Expression) r
-		    .createCopyTarget(((PrefixExpression) exp).getOperand()) : makePrefixExpression(t, r, exp, NOT);
+			return makePrefixExpression(t, r,makeParenthesizedExpression(t, r, exp), NOT);
+		return isPrefix(exp) && ((PrefixExpression) exp).getOperator().equals(NOT) ? 
+				(Expression) r.createCopyTarget(((PrefixExpression) exp).getOperand())
+				: makePrefixExpression(t, r, exp, NOT);
 	}
+
 	static Operator negate(final Operator o) {
 		return !negate.containsKey(o) ? null : negate.get(o);
 	}
+
 	private static Map<Operator, Operator> makeNegation() {
 		final Map<Operator, Operator> $ = new HashMap<Operator, Operator>();
 		$.put(EQUALS, NOT_EQUALS);
@@ -102,35 +116,42 @@ public class ShortestBranchFirst extends Spartanization {
 		$.put(GREATER, LESS_EQUALS);
 		$.put(LESS, GREATER_EQUALS);
 		$.put(GREATER_EQUALS, LESS);
-		// TODO: Why do we need these two? This seems like a BUG!
 		$.put(CONDITIONAL_AND, CONDITIONAL_OR);
 		$.put(CONDITIONAL_OR, CONDITIONAL_AND);
 		return $;
 	}
+
 	private static Map<Operator, Operator> negate = makeNegation();
+
 	private static Expression tryNegatePrefix(final ASTRewrite r, final PrefixExpression exp) {
 		return !exp.getOperator().equals(NOT) ? null : (Expression) r.createCopyTarget(exp.getOperand());
 	}
+
 	private static final int threshold = 1;
-	@Override protected ASTVisitor fillOpportunities(final List<Range> opportunities) {
+
+	@Override
+	protected ASTVisitor fillOpportunities(final List<Range> opportunities) {
 		return new ASTVisitor() {
-			@Override public boolean visit(final IfStatement n) {
+			@Override
+			public boolean visit(final IfStatement n) {
 				if (longerFirst(n))
 					opportunities.add(new Range(n));
 				return true;
 			}
-			@Override public boolean visit(final ConditionalExpression n) {
-				// TODO: make sure you have a test case to cover both cases, i.e.,
-				// that longerFirst returns both True and False.
+
+			@Override
+			public boolean visit(final ConditionalExpression n) {
 				if (longerFirst(n))
 					opportunities.add(new Range(n));
 				return true;
 			}
 		};
 	}
+
 	static boolean longerFirst(final IfStatement n) {
 		return null != n.getElseStatement() && countNodes(n.getThenStatement()) > threshold + countNodes(n.getElseStatement());
 	}
+
 	static boolean longerFirst(final ConditionalExpression n) {
 		return n.getThenExpression().getLength() > threshold + n.getElseExpression().getLength();
 	}
