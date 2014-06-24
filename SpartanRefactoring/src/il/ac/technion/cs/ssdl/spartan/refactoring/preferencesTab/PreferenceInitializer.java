ee366b263d19ebb3172070af83439d08125d5c4a
Yossi Gil <yossi.gil@gmail.com>
2014-06-24 13:53:56 +0300

    added TODO

--- SpartanRefactoring/src/il/ac/technion/cs/ssdl/spartan/refactoring/preferencesTab/PreferenceInitializer.java
+++ SpartanRefactoring/src/il/ac/technion/cs/ssdl/spartan/refactoring/preferencesTab/PreferenceInitializer.java
@@ -10,19 +10,16 @@ import org.eclipse.jface.preference.IPreferenceStore;
  * @author Tomer Zeltzer <code><tomerr90 [at] gmail.com></code> @since
  *         16/06/2014
  * @author Ofir Elmakias <code><elmakias [at] outlook.com></code> @since
- *         2014/6/16
- * 
- *         Class used to initialize default preference values.
+ *         2014/6/16 Class used to initialize default preference values.
  */
 public class PreferenceInitializer extends AbstractPreferenceInitializer {
 	/*
+	 * TODO: remove automatic comments like this one, they provide no information
 	 * (non-Javadoc)
-	 * 
 	 * @see org.eclipse.core.runtime.preferences.AbstractPreferenceInitializer#
 	 * initializeDefaultPreferences()
 	 */
-	@Override
-	public void initializeDefaultPreferences() {
+	@Override public void initializeDefaultPreferences() {
 		final IPreferenceStore store = Plugin.getDefault().getPreferenceStore();
 		for (final String rule : allRulesNames())
 			store.setDefault(rule, true);
