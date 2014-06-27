0e6ef1d618cad5658fe539581697121689b2e90a
Tomer Zeltzer <tomerr90@gmail.com>
2014-06-27 21:42:23 +0300

    Converted to FOR loops

--- SpartanRefactoring/test/il/ac/technion/cs/ssdl/spartan/refactoring/TestSuite.java
+++ SpartanRefactoring/test/il/ac/technion/cs/ssdl/spartan/refactoring/TestSuite.java
@@ -3,8 +3,16 @@ package il.ac.technion.cs.ssdl.spartan.refactoring;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.fail;
 
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.RandomAccessFile;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Scanner;
 
 /**
  * An abstract representation of our test suite, which is represented in
@@ -30,24 +38,23 @@ public abstract class TestSuite {
 	 * Folder in which all test cases are found
 	 */
 	public static final File location = new File("TestCases");
+
 	static String readFile(final File f) {
 		try (final BufferedReader r = new BufferedReader(new InputStreamReader(new FileInputStream(f)))) {
-			// TOOD: Convert to FOR LOOP
-			String line = r.readLine();
 			final StringBuilder $ = new StringBuilder();
-			while (null != line) {
+			for (String line = r.readLine(); line != null; line = r.readLine())
 				$.append(line).append(System.lineSeparator());
-				line = r.readLine();
-			}
 			return $.toString();
 		} catch (final IOException e) {
 			fail(e.toString());
 			return null;
 		}
 	}
+
 	static Spartanization makeSpartanizationObject(final File f) {
 		return makeSpartanizationObject(f.getName());
 	}
+
 	static Spartanization makeSpartanizationObject(final String folderForClass) {
 		final Class<?> c = asClass(folderForClass);
 		assertNotNull(c);
@@ -55,12 +62,13 @@ public abstract class TestSuite {
 		assertNotNull($);
 		return (Spartanization) $;
 	}
+
 	/**
 	 * Instantiates a {@link Class} object if possible, otherwise generate an
 	 * assertion failure
 	 * 
 	 * @param c
-	 *          an arbitrary class object
+	 *            an arbitrary class object
 	 * @return an instance of the parameter
 	 */
 	public static Object getInstance(final Class<?> c) {
@@ -77,16 +85,18 @@ public abstract class TestSuite {
 		}
 		return null;
 	}
+
 	private static Spartanization error(final String message, final Class<?> c, final Throwable e) {
 		System.err.println(message + " '" + c.getCanonicalName() + "' " + e.getMessage());
 		return null;
 	}
+
 	/**
 	 * Convert a canonical name of a class into a {@link Class} object, if
 	 * possible, otherwise generate an assertion failure
 	 * 
 	 * @param name
-	 *          the canonical name of some class
+	 *            the canonical name of some class
 	 * @return the object representing this class
 	 * @since 2014/05/23
 	 */
@@ -98,6 +108,7 @@ public abstract class TestSuite {
 			return null;
 		}
 	}
+
 	/**
 	 * An abstract class representing the concept of traversing the
 	 * {@link #location} while generating test cases.
@@ -117,16 +128,18 @@ public abstract class TestSuite {
 				go($, f);
 			return $;
 		}
+
 		/**
 		 * Collect test cases from each file in {@link #location}
 		 * 
 		 * @param $
-		 *          where to save the collected test cases
+		 *            where to save the collected test cases
 		 * @param f
-		 *          an entry in {@link #location}
+		 *            an entry in {@link #location}
 		 */
 		public abstract void go(List<Object[]> $, final File f);
 	}
+
 	/**
 	 ** An abstract class to be extended and implemented by client, while
 	 * overriding {@link #go(List, File)} as per customer's need.
@@ -138,8 +151,8 @@ public abstract class TestSuite {
 	 */
 	public static abstract class Directories extends TestSuite.Traverse {
 		/**
-		 * Adds a test case to the a collection of all test cases generated in the
-		 * traversal
+		 * Adds a test case to the a collection of all test cases generated in
+		 * the traversal
 		 */
 		@Override public final void go(final List<Object[]> $, final File f) {
 			if (f.isDirectory()) {
@@ -148,8 +161,10 @@ public abstract class TestSuite {
 					$.add(c);
 			}
 		}
+
 		abstract Object[] makeCase(File d);
 	}
+
 	/**
 	 ** An abstract class to be extended and implemented by client, while
 	 * overriding {@link #go(List, File)} as per customer's need.
@@ -162,6 +177,7 @@ public abstract class TestSuite {
 	public static abstract class Files extends TestSuite.Traverse {
 		/*
 		 * (non-Javadoc)
+		 * 
 		 * @see
 		 * il.ac.technion.cs.ssdl.spartan.refactoring.TestSuite.Traverse#go(java
 		 * .util.List, java.io.File)
@@ -175,12 +191,15 @@ public abstract class TestSuite {
 						$.add(c);
 				}
 		}
+
 		abstract Object[] makeCase(final Spartanization s, final File d, final File f, final String name);
 	}
+
 	/* Auxiliary function for test suite inherited classes */
 	enum TestDirection {
 		In, Out
 	}
+
 	/**
 	 * Makes an Input file out of a Test file
 	 */
@@ -191,6 +210,7 @@ public abstract class TestSuite {
 			str.delete(str.indexOf(testKeyword), str.length());
 		return createTempFile(str, TestDirection.In, file);
 	}
+
 	/**
 	 * Makes an Output file out of a Test file
 	 */
@@ -201,6 +221,7 @@ public abstract class TestSuite {
 			str.delete(0, str.indexOf(testKeyword) + testKeyword.length() + (str.indexOf("\r\n") > 0 ? 2 : 1));
 		return createTempFile(str, TestDirection.Out, file);
 	}
+
 	/**
 	 * Creates a temporary file - including lazy deletion.
 	 */
@@ -220,6 +241,7 @@ public abstract class TestSuite {
 		}
 		return $;
 	}
+
 	/**
 	 * Creates a StringBuilder object out of a file object.
 	 */
