8520c740c584e419616a7c0320a0e5c3baed1057
Elmak <elmakias@gmail.com>
2015-08-01 04:19:49 +0300

    Added "Cleanup" option to the Spartanization menu.

---
+++ SpartanRefactoring/src/main/java/org/spartan/refactoring/handlers/CleanupHandler.java
@@ -0,0 +1,16 @@
+package org.spartan.refactoring.handlers;
+
+/**
+ * a handler for {@link ShortestOperand}
+ *
+ * @author Boris van Sosin <code><boris.van.sosin [at] gmail.com></code>
+ * @author Yossi Gil <code><yossi.gil [at] gmail.com></code> (major refactoring
+ *         2013/07/11)
+ * @since 2013/07/011
+ */
+public class CleanupHandler extends BaseHandler {
+  /** Instantiates this class */
+  public CleanupHandler() {
+    super(null);
+  }
+}
