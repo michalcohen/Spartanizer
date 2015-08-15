c9de7aed9bb8c0e44692db6a152d7c5f9a268071
Yossi Gil <yossi.gil@gmail.com>
2015-08-15 10:33:06 +0300

    Add missing test for new Wring.

---
+++ SpartanRefactoring/src/test/java/org/spartan/refactoring/wring/IF_SHORTEST_BRANCH_FIRSt.java
@@ -0,0 +1,77 @@
+package org.spartan.refactoring.wring;
+
+import java.util.Collection;
+
+import org.junit.FixMethodOrder;
+import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+import org.spartan.refactoring.wring.AbstractWringTest.OutOfScope;
+import org.spartan.refactoring.wring.AbstractWringTest.Wringed;
+import org.spartan.utils.Utils;
+
+/**
+ * Unit tests for {@link Wrings#ADDITION_SORTER}.
+ *
+ * @author Yossi Gil
+ * @since 2014-07-13
+ */
+@SuppressWarnings("javadoc") //
+@FixMethodOrder(MethodSorters.NAME_ASCENDING) //
+public enum IF_SHORTEST_BRANCH_FIRSt {
+  ;
+  static final Wring WRING = Wrings.IF_SHORTEST_FIRST.inner;
+
+  @RunWith(Parameterized.class) //
+  public static class OutOfScope extends AbstractWringTest.OutOfScope {
+    static String[][] cases = Utils.asArray(//
+        Utils.asArray("Expression vs. Expression", " 6 - 7 < 2 + 1   "), //
+        Utils.asArray("Return only on one side", "if (a) return b; else c;"), //
+        Utils.asArray("Simple if return", "if (a) return b; else return c;"), //
+        Utils.asArray("Simply nested if return", "{if (a)  return b; else return c;}"), //
+        Utils.asArray("Nested if return", "if (a) {;{{;;return b; }}} else {{{;return c;};;};}"), //
+        null);
+    /**
+     * Generate test cases for this parameterized class.
+     *
+     * @return a collection of cases, where each case is an array of three
+     *         objects, the test case name, the input, and the file.
+     */
+    @Parameters(name = DESCRIPTION) //
+    public static Collection<Object[]> cases() {
+      return collect(cases);
+    }
+    /** Instantiates the enclosing class ({@link OutOfScope}) */
+    public OutOfScope() {
+      super(WRING);
+    }
+  }
+
+  @RunWith(Parameterized.class) //
+  @FixMethodOrder(MethodSorters.NAME_ASCENDING) //
+  public static class Wringed extends AbstractWringTest.WringedIfStatement {
+    private static String[][] cases = Utils.asArray(//
+        // Literal
+        Utils.asArray("Simple if throw", "if (a) throw b; else throw c;", "throw a ? b : c;"), //
+        Utils.asArray("Simply nested if throw", "{if (a)  throw b; else throw c;}", "if(a)throw b;else throw c;"), //
+        Utils.asArray("Nested if throw", "if (a) {;{{;;throw b; }}} else {{{;throw c;};;};}", "throw a ? b : c;"), //
+        null);
+    /**
+     * Generate test cases for this parameterized class.
+     *
+     * @return a collection of cases, where each case is an array of three
+     *         objects, the test case name, the input, and the file.
+     */
+    @Parameters(name = DESCRIPTION) //
+    public static Collection<Object[]> cases() {
+      return collect(cases);
+    }
+    /**
+     * Instantiates the enclosing class ({@link Wringed})
+     */
+    public Wringed() {
+      super(WRING);
+    }
+  }
+}
