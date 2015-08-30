474a4c5c8f8658c8ea620c08e10cea824667a15a
Daniel Mittelman <mittelman@campus.technion.ac.il>
2015-08-30 23:31:57 +0300

    Added tests to JavaTypeNameParser

--- SpartanRefactoring/src/test/java/org/spartan/refactoring/utils/JavaTypeNameParserTest.java
+++ SpartanRefactoring/src/test/java/org/spartan/refactoring/utils/JavaTypeNameParserTest.java
@@ -19,4 +19,22 @@ public class JavaTypeNameParserTest {
   @Test public void astNode() {
     assertThat(new JavaTypeNameParser("ASTNode").shortName(), is("n"));
   }
+  @Test public void onlyLowerCase() {
+    assertThat(new JavaTypeNameParser("onlylowercase").shortName(), is("o"));
+  }
+  @Test public void onlyUpperCase() {
+    assertThat(new JavaTypeNameParser("ONLYUPPERCASE").shortName(), is("o"));
+  }
+  @Test public void jUnit() {
+    assertThat(new JavaTypeNameParser("JUnit").shortName(), is("u"));
+  }
+  @Test public void singleChar() {
+    assertThat(new JavaTypeNameParser("Q").shortName(), is("q"));
+  }
+  @Test public void alphaNumericMid() {
+    assertThat(new JavaTypeNameParser("Base64Parser").shortName(), is("p"));
+  }
+  @Test public void alphaNumericPost() {
+    assertThat(new JavaTypeNameParser("Int32").shortName(), is("i"));
+  }
 }
