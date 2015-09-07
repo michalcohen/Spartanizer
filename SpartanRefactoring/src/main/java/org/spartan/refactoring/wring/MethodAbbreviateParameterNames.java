3fe39a5a10c6a88cab49390ae1444c2571a60910
Yossi Gil <yossi.gil@gmail.com>
2015-09-07 23:23:57 +0300

    New initial wring which does nothing useful and is not active yet.

---
+++ SpartanRefactoring/src/main/java/org/spartan/refactoring/wring/MethodAbbreviateParameterNames.java
@@ -0,0 +1,47 @@
+package org.spartan.refactoring.wring;
+
+import static org.spartan.refactoring.wring.Wrings.rename;
+
+import java.util.List;
+
+import org.eclipse.jdt.core.dom.*;
+import org.eclipse.jdt.core.dom.rewrite.ASTRewrite;
+import org.eclipse.text.edits.TextEditGroup;
+import org.spartan.refactoring.utils.*;
+
+/**
+ * @author Artium Nihamkin (original)
+ * @author Boris van Sosin <code><boris.van.sosin [at] gmail.com></code> (v2)
+ * @author Yossi Gil (v3)
+ * @since 2013/01/01
+ */
+public class MethodAbbreviateParameterNames extends Wring<MethodDeclaration> {
+  @Override String description(final MethodDeclaration d) {
+    return d.getName().toString();
+  }
+  @Override Rewrite make(final MethodDeclaration d, final ExclusionManager exclude) {
+    final SingleVariableDeclaration vd = find(d.parameters());
+    if (vd == null)
+      return null;
+    final SimpleName n = vd.getName();
+    if (exclude != null)
+      exclude.exclude(d);
+    return new Rewrite("Rename variable " + n + " to $ (main variable returned by " + description(d) + ")", d) {
+      @Override public void go(final ASTRewrite r, final TextEditGroup g) {
+        rename(n, n, d, r, g);
+      }
+    };
+  }
+  private SingleVariableDeclaration find(final List<SingleVariableDeclaration> ds) {
+    for (final SingleVariableDeclaration $ : ds)
+      if (suitable($))
+        return $;
+    return null;
+  }
+  private boolean suitable(final SingleVariableDeclaration d) {
+    final Type t = d.getType();
+    final SimpleName n = d.getName();
+    // TODO Auto-generated method stub
+    return false;
+  }
+}
