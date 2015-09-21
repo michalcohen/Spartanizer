d70d83f811a49598fa7989f85754745c975930f4
Daniel Mittelman <mittelman@campus.technion.ac.il>
2015-09-21 11:51:28 +0300

    Added more options, line counting and individual statistics to the command line entry point. Also made some bugfixes

--- SpartanRefactoring/src/main/java/org/spartan/refactoring/application/Application.java
+++ SpartanRefactoring/src/main/java/org/spartan/refactoring/application/Application.java
@@ -1,8 +1,7 @@
 package org.spartan.refactoring.application;
 
-import java.io.File;
-import java.io.IOException;
-import java.util.List;
+import java.io.*;
+import java.util.*;
 
 import org.eclipse.core.resources.*;
 import org.eclipse.core.runtime.CoreException;
@@ -28,30 +27,36 @@ import org.spartan.utils.Wrapper;
   IJavaProject javaProject;
   IPackageFragmentRoot srcRoot;
   IPackageFragment pack;
-  boolean optPrintBase64 = false, optDoNotOverwrite = false, optIndividualStatistics = false, optVerbose = false;
+  boolean optDoNotOverwrite = false, optIndividualStatistics = false, optVerbose = false;
+  boolean optStatsLines = false, optStatsChanges = false;
+  int optRounds = 20;
   String optPath;
   @Override public Object start(final IApplicationContext arg0) throws Exception {
-    if (parseArguments((String[]) arg0.getArguments().get(IApplicationContext.APPLICATION_ARGS)))
+    if (parseArguments(Arrays.asList((String[]) arg0.getArguments().get(IApplicationContext.APPLICATION_ARGS))))
       return IApplication.EXIT_OK;
     try {
       final List<String> javaFiles = FileUtils.findAllJavaFiles(optPath);
+      final List<FileStats> fileStats = new ArrayList<>();
       prepareTempIJavaProject();
-      final int[] roundStats = new int[20];
       for (final String f : javaFiles) {
         final ICompilationUnit u = openCompilationUnit(f);
-        for (int i = 0; i < 20; ++i) {
+        final FileStats s = new FileStats(f);
+        for (int i = 0; i < optRounds; ++i) {
           final int n = CleanupHandler.countSuggestions(u);
           if (n == 0)
             break;
-          roundStats[i] += n;
+          s.addRoundStat(n);
           ApplySpartanizationHandler.applySafeSpartanizationsTo(u);
         }
-        FileUtils.writeToFile(f, u.getSource());
+        FileUtils.writeToFile(determineOutputFilename(f), u.getSource());
+        s.countLinesAfter();
+        fileStats.add(s);
       }
       System.out.println("Files processed: " + javaFiles.size());
-      System.out.println("\nTotal modifications made: ");
-      for (int i = 0; i < 20; ++i)
-        System.out.println("    Round #" + (i + 1) + ": " + (i < 9 ? " " : "") + roundStats[i]);
+      if (optStatsChanges)
+        printChangeStatistics(fileStats);
+      if (optStatsLines)
+        printLineStatistics(fileStats);
     } catch (final Exception e) {
       e.printStackTrace();
     } finally {
@@ -69,34 +74,78 @@ import org.spartan.utils.Wrapper;
         + "within the given PATH. Files are spartanized in place by default.");
     System.out.println("");
     System.out.println("Options:");
-    System.out.println("  -b    Prints the output in base64");
-    System.out.println("  -n    Writes the spartanized source to a new file (in the same directory as the original source file)");
-    System.out.println("  -e    Display statistics for each file separately");
-    System.out.println("  -v    Be verbose");
+    System.out.println("  -N       Do not overwrite existing files (writes the Spartanized output to a new file in the same directory)");
+    System.out.println("  -C<num>  Maximum number of Spartanizaion rounds for each file (default: 20)");
+    System.out.println("  -E       Display statistics for each file separately");
+    System.out.println("  -V       Be verbose");
+    System.out.println("");
+    System.out.println("Print statistics:");
+    System.out.println("  -l       Show the number of lines before and after Spartanization");
+    System.out.println("  -r       Show the number of Spartanizaion made in each round");
   }
-  boolean parseArguments(final String[] args) {
-    if (args == null || args.length == 0) {
+  void printLineStatistics(final List<FileStats> files) {
+    System.out.println("\nLine differences:");
+    if (optIndividualStatistics)
+      for (final FileStats f : files) {
+        System.out.println("\n  " + f.getFileName());
+        System.out.println("    Lines before: " + f.getLinesBefore());
+        System.out.println("    Lines after: " + f.getLinesAfter());
+      }
+    else {
+      int totalBefore = 0, totalAfter = 0;
+      for (final FileStats f : files) {
+        totalBefore += f.getLinesBefore();
+        totalAfter += f.getLinesAfter();
+      }
+      System.out.println("  Lines before: " + totalBefore);
+      System.out.println("  Lines after: " + totalAfter);
+    }
+  }
+  private void printChangeStatistics(final List<FileStats> files) {
+    System.out.println("\nTotal changes made: ");
+    if (optIndividualStatistics)
+      for (final FileStats f : files) {
+        System.out.println("\n  " + f.getFileName());
+        for (int i = 0; i < optRounds; ++i)
+          System.out.println("    Round #" + (i + 1) + ": " + (i < 9 ? " " : "") + f.getRoundStat(i));
+      }
+    else
+      for (int i = 0; i < optRounds; ++i) {
+        int roundSum = 0;
+        for (final FileStats f : files)
+          roundSum += f.getRoundStat(i);
+        System.out.println("    Round #" + (i + 1) + ": " + (i < 9 ? " " : "") + roundSum);
+      }
+  }
+  String determineOutputFilename(final String path) {
+    if (optDoNotOverwrite)
+      return path.substring(0, path.lastIndexOf('.')) + "_new.java";
+    return path;
+  }
+  boolean parseArguments(final List<String> args) {
+    if (args == null || args.size() == 0) {
       printHelpPrompt();
       return true;
     }
-    for (final String a : args)
-      switch (a) {
-        case "-b":
-          optPrintBase64 = true;
-          break;
-        case "-n":
-          optDoNotOverwrite = true;
-          break;
-        case "-e":
-          optIndividualStatistics = true;
-          break;
-        case "-v":
-          optVerbose = true;
-          break;
-        default:
-          optPath = a;
-          break;
-      }
+    for (final String a : args) {
+      if (a.equals("-N"))
+        optDoNotOverwrite = true;
+      if (a.equals("-E"))
+        optIndividualStatistics = true;
+      try {
+        if (a.startsWith("-C"))
+          optRounds = Integer.parseUnsignedInt(a.substring(2));
+      } catch (final NumberFormatException e) {
+        /* Ignore */ }
+      if (a.equals("-V"))
+        optVerbose = true;
+      if (a.equals("-l"))
+        optStatsLines = true;
+      if (a.equals("-r"))
+        optStatsChanges = true;
+      if (!a.startsWith("-"))
+        optPath = a;
+    }
     return optPath == null;
   }
   void prepareTempIJavaProject() {
@@ -154,6 +203,53 @@ import org.spartan.utils.Wrapper;
       javaProject.getProject().delete(true, null);
     } catch (final CoreException e) {
       e.printStackTrace();
+    }
+  }
+
+  /**
+   * Data structure designed to hold and compute information about a single
+   * file, in order to produce statistics when completed execution
+   */
+  private class FileStats {
+    String fileName, path;
+    int linesBefore, linesAfter;
+    List<Integer> roundStats;
+    public FileStats(final String path) {
+      this.path = path;
+      fileName = new File(path).getName();
+      linesBefore = countLines(path);
+      roundStats = new ArrayList<>();
+    }
+    public void countLinesAfter() {
+      linesAfter = countLines(determineOutputFilename(path));
+    }
+    public void addRoundStat(final int i) {
+      roundStats.add(i);
+    }
+    @SuppressWarnings("boxing") public int getRoundStat(final int r) {
+      try {
+        return roundStats.get(r);
+      } catch (final IndexOutOfBoundsException e) {
+        return 0;
+      }
+    }
+    public String getFileName() {
+      return fileName;
+    }
+    public int getLinesBefore() {
+      return linesBefore;
+    }
+    public int getLinesAfter() {
+      return linesAfter;
+    }
+    protected int countLines(final String p) {
+      try (LineNumberReader lr = new LineNumberReader(new FileReader(new File(p)))) {
+        lr.skip(Long.MAX_VALUE);
+        return lr.getLineNumber();
+      } catch (final IOException e) {
+        e.printStackTrace();
+        return -1;
+      }
     }
   }
 }
