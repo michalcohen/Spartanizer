3751e22520e830b52ac58fc322f2095b3aa67dd6
Ori Roth <ori.rothh@gmail.com>
2016-04-09 18:35:48 +0300

    First wring.

--- SpartanRefactoring/src/main/java/il/org/spartan/refactoring/wring/WrapperReplaceWithFactory.java
+++ SpartanRefactoring/src/main/java/il/org/spartan/refactoring/wring/WrapperReplaceWithFactory.java
@@ -1,29 +1,40 @@
 package il.org.spartan.refactoring.wring;
 
-import org.eclipse.jdt.core.dom.*;
+import java.util.Arrays;
 
+import org.eclipse.jdt.core.dom.*;
 import il.org.spartan.refactoring.preferences.PluginPreferencesResources.WringGroup;
 
-/* Three TODOs (from Daniel): 1) Ensure the wring can handle all primitive
- * wrappers in Java's JRE 2) Don't assume the argument is a NumberLiteral; it
- * can be any Expression 3) Add Javadoc (just look in other wrings and follow) */
+/**
+ * A {@link Wring} to replace primitive class (and String) instance creation
+ * with recommended factory method valueOf():
+ * <code><pre>Integer x = new Integer(2);</pre></code> can be replaced with
+ * <code><pre>Integer x = Integer.valueOf(2);</pre></code>
+ *
+ * @author Ori Roth <code><ori.rothh [at] gmail.com></code>
+ * @since 2016-04-06
+ */
 public class WrapperReplaceWithFactory extends Wring.ReplaceCurrentNode<ClassInstanceCreation> {
-  @Override ASTNode replacement(final ClassInstanceCreation n) {
-    if (n.getType().toString().equals("Integer")) {
-      final MethodInvocation $ = n.getAST().newMethodInvocation();
-      $.setExpression(n.getAST().newSimpleName("Integer"));
-      $.setName(n.getAST().newSimpleName("valueOf"));
-      final NumberLiteral nl = n.getAST().newNumberLiteral();
-      nl.setToken(((NumberLiteral) n.arguments().get(0)).getToken());
-      $.arguments().add(nl);
-      return $;
+  // String array contains all primitive class (and String) identifiers
+  final String[] pi = { "Byte", "Short", "Integer", "Long", "Float", "Double", "Character", "String", "Boolean" };
+  @Override
+  ASTNode replacement(final ClassInstanceCreation n) {
+    String tn = n.getType().toString();
+    if (!Arrays.asList(pi).contains(tn)) {
+      return null;
     }
-    return null;
+    final MethodInvocation $ = n.getAST().newMethodInvocation();
+    $.setExpression(n.getAST().newSimpleName(tn));
+    $.setName(n.getAST().newSimpleName("valueOf"));
+    $.arguments().add(Expression.copySubtree(n.getAST(), (ASTNode) n.arguments().get(0)));
+    return $;
   }
-  @Override String description(final ClassInstanceCreation n) {
+  @Override
+  String description(final ClassInstanceCreation n) {
     return "Use Java's built-in factory constructor valueOf() instead of initialization";
   }
-  @Override WringGroup wringGroup() {
-    return WringGroup.CONSOLIDATE_ASSIGNMENTS_STATEMENTS;
+  @Override
+  WringGroup wringGroup() {
+    return WringGroup.REPLACE_CLASS_INSTANCE_CREATION;
   }
 }
