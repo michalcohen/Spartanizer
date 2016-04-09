c4564b81b8857dea0a09a6aa63a527ac8ad81297
Ori Roth <ori.rothh@gmail.com>
2016-04-09 18:37:39 +0300

    Added category REPLACE_CLASS_INSTANCE_CREATION

--- SpartanRefactoring/src/main/java/il/org/spartan/refactoring/preferences/PluginPreferencesResources.java
+++ SpartanRefactoring/src/main/java/il/org/spartan/refactoring/preferences/PluginPreferencesResources.java
@@ -8,7 +8,8 @@ public class PluginPreferencesResources {
   /** General preferences **/
   public static final String PLUGIN_STARTUP_BEHAVIOR_ID = "pref_startup_behavior";
   public static final String PLUGIN_STARTUP_BEHAVIOR_TEXT = "Plugin startup behavior:";
-  public static final String[][] PLUGIN_STARTUP_BEHAVIOR_OPTIONS = { { "Remember individual project settings", "remember" }, { "Enable for all projects", "always_on" },
+  public static final String[][] PLUGIN_STARTUP_BEHAVIOR_OPTIONS = {
+      { "Remember individual project settings", "remember" }, { "Enable for all projects", "always_on" },
       { "Disable for all projects", "always_off" } };
   public static final String NEW_PROJECTS_ENABLE_BY_DEFAULT_ID = "pref_enable_by_default_for_new_projects";
   public static final String NEW_PROJECTS_ENABLE_BY_DEFAULT_TEXT = "Enable by default for newly created projects";
@@ -33,6 +34,8 @@ public class PluginPreferencesResources {
   public static final String RENAME_PARAMETERS_TEXT = "Rename method parameters";
   public static final String RENAME_RETURN_VARIABLE_ID = "pref_wrings_rename_return_variable";
   public static final String RENAME_RETURN_VARIABLE_TEXT = "Rename return variable to $";
+  public static final String REPLACE_CLASS_INSTANCE_CREATION_ID = "pref_wrings_replace_class_instance_creation";
+  public static final String REPLACE_CLASS_INSTANCE_CREATION_TEXT = "Replace class instance creation with recommended form";
   public static final String WRING_COMBO_OPTIONS[][] = { { "Enabled", "on" }, { "Disabled", "off" } };
 
   /**
@@ -40,23 +43,34 @@ public class PluginPreferencesResources {
    * allowing to get the set preference value for each of them
    */
   public enum WringGroup {
-    CONSOLIDATE_ASSIGNMENTS_STATEMENTS(CONSOLIDATE_ASSIGNMENTS_STATEMENTS_ID, CONSOLIDATE_ASSIGNMENTS_STATEMENTS_TEXT), SIMPLIFY_NESTED_BLOCKS(SIMPLIFY_NESTED_BLOCKS_ID,
-        SIMPLIFY_NESTED_BLOCKS_TEXT), ELIMINATE_TEMP(ELIMINATE_TEMP_ID, ELIMINATE_TEMP_TEXT), REMOVE_REDUNDANT_PUNCTUATION(REMOVE_REDUNDANT_PUNCTUATION_ID,
-            REMOVE_REDUNDANT_PUNCTUATION_TEXT), IF_TO_TERNARY(IF_TO_TERNARY_ID, IF_TO_TERNARY_TEXT), REFACTOR_INEFFECTIVE(REFACTOR_INEFFECTIVE_ID,
-                REFACTOR_INEFFECTIVE_TEXT), REORDER_EXPRESSIONS(REORDER_EXPRESSIONS_ID, REORDER_EXPRESSIONS_TEXT), OPTIMIZE_ANNOTATIONS(OPTIMIZE_ANNOTATIONS_ID,
-                    OPTIMIZE_ANNOTATIONS_TEXT), RENAME_PARAMETERS(RENAME_PARAMETERS_ID,
-                        RENAME_PARAMETERS_TEXT), RENAME_RETURN_VARIABLE(RENAME_RETURN_VARIABLE_ID, RENAME_RETURN_VARIABLE_TEXT),;
+    CONSOLIDATE_ASSIGNMENTS_STATEMENTS(CONSOLIDATE_ASSIGNMENTS_STATEMENTS_ID,
+        CONSOLIDATE_ASSIGNMENTS_STATEMENTS_TEXT), SIMPLIFY_NESTED_BLOCKS(SIMPLIFY_NESTED_BLOCKS_ID,
+            SIMPLIFY_NESTED_BLOCKS_TEXT), ELIMINATE_TEMP(ELIMINATE_TEMP_ID,
+                ELIMINATE_TEMP_TEXT), REMOVE_REDUNDANT_PUNCTUATION(REMOVE_REDUNDANT_PUNCTUATION_ID,
+                    REMOVE_REDUNDANT_PUNCTUATION_TEXT), IF_TO_TERNARY(IF_TO_TERNARY_ID,
+                        IF_TO_TERNARY_TEXT), REFACTOR_INEFFECTIVE(REFACTOR_INEFFECTIVE_ID,
+                            REFACTOR_INEFFECTIVE_TEXT), REORDER_EXPRESSIONS(REORDER_EXPRESSIONS_ID,
+                                REORDER_EXPRESSIONS_TEXT), OPTIMIZE_ANNOTATIONS(OPTIMIZE_ANNOTATIONS_ID,
+                                    OPTIMIZE_ANNOTATIONS_TEXT), RENAME_PARAMETERS(RENAME_PARAMETERS_ID,
+                                        RENAME_PARAMETERS_TEXT), RENAME_RETURN_VARIABLE(RENAME_RETURN_VARIABLE_ID,
+                                            RENAME_RETURN_VARIABLE_TEXT), REPLACE_CLASS_INSTANCE_CREATION(
+                                                REPLACE_CLASS_INSTANCE_CREATION_ID,
+                                                REPLACE_CLASS_INSTANCE_CREATION_TEXT),;
     private final String id, label;
+
     WringGroup(final String id, final String label) {
       this.id = id;
       this.label = label;
     }
+
     public String getId() {
       return id;
     }
+
     public String getLabel() {
       return label;
     }
+
     public boolean isEnabled() {
       return Plugin.getDefault() == null || Plugin.getDefault().getPreferenceStore().getString(id).equals("on");
     }
