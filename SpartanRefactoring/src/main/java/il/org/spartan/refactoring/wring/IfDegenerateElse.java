0406283669fd2d6ad25f3859e02a1f53d80f69a8
Yossi Gil <yossi.gil@gmail.com>
2016-04-21 14:14:04 +0300

    More meaningful description

--- SpartanRefactoring/src/main/java/il/org/spartan/refactoring/wring/IfDegenerateElse.java
+++ SpartanRefactoring/src/main/java/il/org/spartan/refactoring/wring/IfDegenerateElse.java
@@ -12,11 +12,8 @@ import il.org.spartan.refactoring.utils.Is;
 import il.org.spartan.refactoring.utils.Subject;
 
 /**
- * /** A {@link Wring} to convert <code>if (x)
- *   return b;
- * else {
- * }</code> into <code>if (x)
- *   return b;</code>
+ * /** A {@link Wring} to convert <code>if (x) return b; else { }</code> into
+ * <code>if (x) return b;</code>
  *
  * @author Yossi Gil
  * @since 2015-08-01
@@ -30,13 +27,13 @@ public final class IfDegenerateElse extends Wring.ReplaceCurrentNode<IfStatement
   @Override boolean scopeIncludes(final IfStatement s) {
     return s != null && then(s) != null && degenerateElse(s);
   }
-  @Override String description(@SuppressWarnings("unused") final IfStatement _) {
-    return "Remove vacuous 'else' branch";
+  @Override String description(final IfStatement s) {
+    return "Remove vacuous 'else' branch of if(" + s.getExpression() + ") ...";
   }
   static boolean degenerateElse(final IfStatement s) {
     return elze(s) != null && Is.vacuousElse(s);
   }
   @Override WringGroup wringGroup() {
-	return WringGroup.REFACTOR_INEFFECTIVE;
+    return WringGroup.REFACTOR_INEFFECTIVE;
   }
 }
