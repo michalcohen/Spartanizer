cfd0d06ce73e36893cae2d90e0ff10179bd2b202
Ori Roth <ori.rothh@gmail.com>
2016-05-04 12:14:02 +0300

    Add comments to code

--- SpartanRefactoring/src/main/java/il/org/spartan/refactoring/utils/Comments.java
+++ SpartanRefactoring/src/main/java/il/org/spartan/refactoring/utils/Comments.java
@@ -16,13 +16,15 @@ import org.eclipse.jdt.core.dom.Statement;
 import org.eclipse.jdt.core.dom.rewrite.ASTRewrite;
 import org.eclipse.jdt.core.dom.rewrite.TargetSourceRangeComputer.SourceRange;
 import org.eclipse.text.edits.TextEditGroup;
-import org.eclipse.ui.dialogs.ListSelectionDialog;
 
 /**
- * Center of comments analysis and manipulation.
- * Supports DisableSpartanization option.
- * Supports comments mash into nodes.
- * 
+ * Center of comments analysis and manipulation. Supports DisableSpartanization
+ * option. Supports comments mash into nodes. Each Comments object gather: 1)
+ * base - an {@ilnk ASTNode} about to be replaced 2) core - the {@link ASTNode}
+ * that would come in its place 3) a list of comments meant to be added to the
+ * core node. Then, by calling mash method, a final {@link ASTNode} is created
+ * from the core and the comments, and base is replaced by it
+ *
  * @author Ori Roth
  * @since 2016-04-20
  */
@@ -32,7 +34,12 @@ public class Comments {
   private static ASTRewrite r;
   private ASTNode bs;
   private ASTNode cr;
-  private List<Comment> cl = new LinkedList<>();
+  private final List<Comment> cl = new LinkedList<>();
+
+  /**
+   * Create a Comments object, with up to date source code and current
+   * {@link CompilationUnit} in use
+   */
   public Comments() {
     refresh();
   }
@@ -41,44 +48,46 @@ public class Comments {
     cu = Source.getCompilationUnit();
     r = Source.getASTRewrite();
   }
+  /**
+   * Clear base, core and comments
+   */
   public void clear() {
     refresh();
     bs = null;
     cr = null;
     cl.clear();
   }
+
   /**
    * Disable Spartanization identifier, used by the programmer to indicate a
    * method/class/code line to not be spartanized
    */
   public final static String dsi = "@DisableSpartan";
+
   /**
    * Match row indexes of comment and a node, to see if a dsi inside the comment
    * would disable the node.
-   * 
-   * @param nln
-   *          start of node row index
-   * @param csr
-   *          comment start row index
-   * @param cer
-   *          comment end row index
-   * @param t
-   *          comment type
+   *
+   * @param nln start of node row index
+   * @param csr comment start row index
+   * @param cer comment end row index
+   * @param t comment type
    * @return true iff a dsi in the comment disables the node
    */
   private static boolean matchRowIndexes(int nln, int csr, int cer, int ct, ASTNode n) {
     switch (ct) {
-    case ASTNode.LINE_COMMENT:
-      return nln == cer;
-    case ASTNode.BLOCK_COMMENT:
-      return nln == cer || nln - 1 == cer;
-    case ASTNode.JAVADOC:
-      if (n instanceof BodyDeclaration)
-        // need spacial care because Javadoc is in BodyDeclaration's regular expression
-        return nln == csr;
-      return nln == cer || nln - 1 == cer;
-    default:
-      return false;
+      case ASTNode.LINE_COMMENT:
+        return nln == cer;
+      case ASTNode.BLOCK_COMMENT:
+        return nln == cer || nln - 1 == cer;
+      case ASTNode.JAVADOC:
+        if (n instanceof BodyDeclaration)
+          // need spacial care because Javadoc is in BodyDeclaration's regular
+          // expression
+          return nln == csr;
+        return nln == cer || nln - 1 == cer;
+      default:
+        return false;
     }
   }
   /**
@@ -88,46 +97,40 @@ public class Comments {
    * Second, the go method of the visitor should return false upon reaching a
    * dsi, what would stop it from reaching the the nodes sons (currently
    * implemented in fillRewrite and collect in Trimmer)
-   * 
-   * @param n
-   *          ASTNode
+   *
+   * @param n ASTNode
    * @return true iff the spartanization is disabled for this node and its sons.
    */
-  @SuppressWarnings("unchecked")
-  public static <N extends ASTNode> boolean isSpartanizationDisabled(N n) {
+  @SuppressWarnings("unchecked") public static <N extends ASTNode> boolean isSpartanizationDisabled(N n) {
     refresh();
     // In a failure case, allow all spartanizations
     if (s == null)
       return false;
     if (cu == null)
       return false;
-    int nln = cu.getLineNumber(n.getStartPosition()) - 1;
-    for (Comment c : (List<Comment>) cu.getCommentList()) {
-      CommentVisitor cv = new CommentVisitor();
+    final int nln = cu.getLineNumber(n.getStartPosition()) - 1;
+    for (final Comment c : (List<Comment>) cu.getCommentList()) {
+      final CommentVisitor cv = new CommentVisitor();
       c.accept(cv);
-      if (matchRowIndexes(nln, cv.getStartRow(), cv.getEndRow(), c.getNodeType(), n)
-          && cv.getContent().contains(dsi))
+      if (matchRowIndexes(nln, cv.getStartRow(), cv.getEndRow(), c.getNodeType(), n) && cv.getContent().contains(dsi))
         return true;
     }
     return false;
   }
   /**
    * Adds all comments associated with n to cl
-   * 
-   * @param n
-   *          original node
-   * @return list of comments
+   *
+   * @param n original node
    */
-  @SuppressWarnings("unchecked")
-  public void add(ASTNode n) {
+  @SuppressWarnings("unchecked") public void add(ASTNode n) {
     // In a failure case, allow all spartanizations
     if (s == null || cu == null || r == null)
       return;
-    SourceRange t = r.getExtendedSourceRangeComputer().computeSourceRange(n);
-    int sp = t.getStartPosition();
-    int ep = sp + t.getLength();
-    for (Comment c : (List<Comment>) cu.getCommentList()) {
-      int csp = c.getStartPosition();
+    final SourceRange t = r.getExtendedSourceRangeComputer().computeSourceRange(n);
+    final int sp = t.getStartPosition();
+    final int ep = sp + t.getLength();
+    for (final Comment c : (List<Comment>) cu.getCommentList()) {
+      final int csp = c.getStartPosition();
       if (csp < sp)
         continue;
       else if (csp >= ep)
@@ -137,18 +140,18 @@ public class Comments {
   }
   /**
    * Sets base - original node to be replaced
-   * 
+   *
    * @param n
    */
   public void setBase(ASTNode n) {
-    this.bs = n;
+    bs = n;
   }
   public ASTNode base() {
     return bs;
   }
   /**
    * Sets core - node to be mashed with the comments in cl
-   * 
+   *
    * @param n
    */
   public void setCore(ASTNode cr) {
@@ -159,43 +162,37 @@ public class Comments {
   }
   /**
    * Checks for whitespaces only string
-   * 
-   * @param s
-   *          a String
+   *
+   * @param s a String
    * @return true iff s contains only whitespaces
    */
   public boolean allWhiteSpaces(String s) {
-    for (char c : s.toCharArray())
+    for (final char c : s.toCharArray())
       if (!Character.isWhitespace(c))
         return false;
     return true;
   }
   /**
    * Assuming c is the only comment associated with cr, check whether it should
-   * be at the end of cr or at its start.
-   * TODO complete all cases
-   * 
-   * @param b
-   *          base node
-   * @param cr
-   *          core node
-   * @param c
-   *          comment
+   * be at the end of cr or at its start. TODO Ori: complete all cases
+   *
+   * @param b base node
+   * @param cr core node
+   * @param c comment
    * @return true iff c should be placed at the end of cr
    */
   public boolean shouldMoveComentToEnd(ASTNode b, ASTNode cr, Comment c) {
     if (!c.isLineComment())
       return false;
     switch (cr.getNodeType()) {
-    case ASTNode.IF_STATEMENT:
-      IfStatement is = (IfStatement) cr;
-      return Is.vacuous(is.getElseStatement()) && !(is.getThenStatement() instanceof Block);
+      case ASTNode.IF_STATEMENT:
+        final IfStatement is = (IfStatement) cr;
+        return Is.vacuous(is.getElseStatement()) && !(is.getThenStatement() instanceof Block);
     }
     return true;
   }
   /**
-   * @param i
-   *          code character index
+   * @param i code character index
    * @return index of the start of i's row
    */
   public int rowStartIndex(int i) {
@@ -206,55 +203,63 @@ public class Comments {
   }
   /**
    * Mashing the comments with the previously declared node
-   * 
-   * @return placeholder node
+   *
+   * @param b base node
+   * @param cr core node
+   * @param g current {@link TextEditGroup}
    */
-  public void mash(ASTNode b, ASTNode cr, TextEditGroup g) {
+  @SuppressWarnings("unchecked") public void mash(ASTNode b, ASTNode cr, TextEditGroup g) {
     // Assumes b is original node with real position
     if (cl.size() == 0 || b.getStartPosition() < 0) {
       r.replace(b, cr, g);
       return;
     }
-    SourceRange sr = r.getExtendedSourceRangeComputer().computeSourceRange(b);
-    List<ASTNode> nl = new ArrayList<>();
+    final SourceRange sr = r.getExtendedSourceRangeComputer().computeSourceRange(b);
+    final List<ASTNode> nl = new ArrayList<>();
     if (cr instanceof Block) {
-      Block bl = (Block) cr;
+      final Block bl = (Block) cr;
       Collections.reverse(cl);
-      for (Comment c : cl)
+      for (final Comment c : cl)
         bl.statements().add(0,
-            r.createStringPlaceholder(s.substring(c.getStartPosition(), c.getStartPosition() + c.getLength())
-                , ASTNode.BLOCK));
+            r.createStringPlaceholder(s.substring(c.getStartPosition(), c.getStartPosition() + c.getLength()), ASTNode.BLOCK));
       nl.add(bl);
     } else {
       if (cl.size() == 1 && shouldMoveComentToEnd(b, cr, cl.get(0))) {
-        Comment c = cl.get(0);
+        final Comment c = cl.get(0);
         String f = "";
         // new line fix after line comment
-        f = (!c.isLineComment() || allWhiteSpaces(s.substring(sr.getStartPosition() + sr.getLength()).split("\n")[0]))
-            ? "" : "\n";
+        f = !c.isLineComment() || allWhiteSpaces(s.substring(sr.getStartPosition() + sr.getLength()).split("\n")[0]) ? "" : "\n";
         nl.add(cr);
-        nl.add(r.createStringPlaceholder(
-            " " + s.substring(c.getStartPosition(), c.getStartPosition() + c.getLength()) + f, c.getNodeType()));
+        nl.add(r.createStringPlaceholder(" " + s.substring(c.getStartPosition(), c.getStartPosition() + c.getLength()) + f,
+            c.getNodeType()));
       } else {
-        for (Comment c : cl)
+        for (final Comment c : cl)
           nl.add(r.createStringPlaceholder(s.substring(c.getStartPosition(), c.getStartPosition() + c.getLength())
-              + ((cr instanceof Expression && !c.isLineComment()) ? "" : "\n"), c.getNodeType()));
+              + (cr instanceof Expression && !c.isLineComment() ? "" : "\n"), c.getNodeType()));
         nl.add(cr);
       }
       if (cr instanceof Statement && !allWhiteSpaces(s.substring(rowStartIndex(sr.getStartPosition()), sr.getStartPosition())))
         nl.add(0, r.createStringPlaceholder("", ASTNode.BLOCK));
     }
-//      TODO ctrl+shift+f it
-//      if (!allWhiteSpaces(s.substring(sr.getStartPosition() + sr.getLength()).split("\n")[0])
-//        && b instanceof Statement)
-//      nl.add(r.createStringPlaceholder("", ASTNode.BLOCK));
     r.replace(b, r.createGroupNode(nl.toArray(new ASTNode[nl.size()])), g);
   }
+  /**
+   * Mashing the comments with the previously declared node, using predefined
+   * base and core
+   *
+   * @param g current {@link TextEditGroup}
+   */
   public void mash(TextEditGroup g) {
     if (bs == null || cr == null)
       return;
     mash(bs, cr, g);
   }
+  /**
+   * Get current number of comment saved in this object (and about to be mashed
+   * with the core)
+   *
+   * @return Comments count
+   */
   public int commentsCount() {
     return cl.size();
   }
