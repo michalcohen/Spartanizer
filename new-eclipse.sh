6b634798010de40947c32026ff416b8ffb0b9e5d
Yossi Gil <yossi.gil@gmail.com>
2016-06-02 12:53:00 +0300

    polishing the new-eclipse.sh file

--- new-eclipse.sh
+++ new-eclipse.sh
@@ -12,5 +12,5 @@ cd ~/Downloads &&
 touch eclipse &&
 mv eclipse  "/tmp/`date`.eclipse" 
 shopt -s extglob # likely already set in interactive shells
-tar xfz eclipse-rcp-*.tar.gz
+tar xfz eclipse-$1-*.tar.gz
 eclipse/eclipse -clean -consoleLog -data ~/workspace/
