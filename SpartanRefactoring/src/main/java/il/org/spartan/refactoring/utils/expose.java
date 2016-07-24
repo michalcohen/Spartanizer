f4f4cb4ee6b6506b5a990d95376ee7d87198916e
Yossi Gil <yossi.gil@gmail.com>
2016-07-24 16:15:02 +0300

    Rewrite as interface

--- SpartanRefactoring/src/main/java/il/org/spartan/refactoring/utils/expose.java
+++ SpartanRefactoring/src/main/java/il/org/spartan/refactoring/utils/expose.java
@@ -4,107 +4,91 @@ import java.util.*;
 
 import org.eclipse.jdt.core.dom.*;
 
-/** An empty <code><b>enum</b></code> for fluent programming. The name should say
- * it all: The name, followed by a dot, followed by a method name, should read
- * like a sentence phrase.
- *
+/** An empty <code><b>interface</b></code> for fluent programming. The name
+ * should say it all: The name, followed by a dot, followed by a method name,
+ * should read like a sentence phrase.
  * @author Yossi Gil
  * @since 2015-07-16 */
-public enum expose {
-  ;
+public interface expose {
   /** Expose the list of arguments in a {@link ClassInstanceCreation}
-   *
    * @param c JD
    * @return a reference to the list of arguments in the argument */
-  public static List<Expression> arguments(final ClassInstanceCreation c) {
+  static List<Expression> arguments(final ClassInstanceCreation c) {
     return ast.expressions(c.arguments());
   }
   /** Expose the list of arguments in a {@link MethodInvocation}
-   *
    * @param i JD
    * @return a reference to the list of arguments in the argument */
-  public static List<Expression> arguments(final MethodInvocation i) {
+  static List<Expression> arguments(final MethodInvocation i) {
     return ast.expressions(i.arguments());
   }
   /** Expose the list of arguments in a {@link SuperMethodInvocation}
-   *
    * @param i JD
    * @return a reference to the list of arguments in the argument */
-  public static List<Expression> arguments(final SuperMethodInvocation i) {
+  static List<Expression> arguments(final SuperMethodInvocation i) {
     return ast.expressions(i.arguments());
   }
   /** Expose the list of extended operands in an {@link InfixExpression}
-   *
    * @param e JD
    * @return a reference to the list of extended operands contained in the
    *         parameter */
-  public static List<Expression> extendedOperands(final InfixExpression e) {
+  static List<Expression> extendedOperands(final InfixExpression e) {
     @SuppressWarnings("unchecked") final List<Expression> $ = e.extendedOperands();
     return $;
   }
   /** Expose the list of fragments in a {@link FieldDeclaration}
-   *
    * @param d JD
    * @return a reference to the list of fragments in the argument */
-  public static List<VariableDeclarationFragment> fragments(final FieldDeclaration d) {
+  static List<VariableDeclarationFragment> fragments(final FieldDeclaration d) {
     return ast.fragments(d.fragments());
   }
   /** Expose the list of fragments in a {@link VariableDeclarationExpression}
-   *
    * @param e JD
    * @return a reference to the list of fragments in the argument */
-  public static List<VariableDeclarationFragment> fragments(final VariableDeclarationExpression e) {
+  static List<VariableDeclarationFragment> fragments(final VariableDeclarationExpression e) {
     return ast.fragments(e.fragments());
   }
   /** Expose the list of fragments in a {@link VariableDeclarationStatement}
-   *
    * @param s JD
    * @return a reference to the list of fragments in the argument */
-  public static List<VariableDeclarationFragment> fragments(final VariableDeclarationStatement s) {
+  static List<VariableDeclarationFragment> fragments(final VariableDeclarationStatement s) {
     return ast.fragments(s.fragments());
   }
   /** Expose the list of parameters in a {@link MethodDeclaration}
-   *
    * @param ¢ JD
-   *
    * @return result of method {@link MethodDeclaration#parameters} downcasted to
    *         its correct type */
   @SuppressWarnings("unchecked") public static List<SingleVariableDeclaration> parameters(final MethodDeclaration ¢) {
     return ¢.parameters();
   }
   /** Expose the list of statements contained in a {@link Block}
-   *
    * @param ¢ JD
    * @return a reference to the list of statements contained in the argument */
-  public static List<Statement> statements(final Block ¢) {
+  static List<Statement> statements(final Block ¢) {
     return ast.statements(¢.statements());
   }
-  /** Expose the list of initializers contained in ¢ {@link ForStatement}
-   *
+  /** Expose the list of initializers contained in a {@link ForStatement}
    * @param ¢ JD
    * @return a reference to the list of initializers contained in the argument */
   @SuppressWarnings("unchecked") static List<VariableDeclarationExpression> initializers(final ForStatement ¢) {
     return ¢.initializers();
   }
   /** Expose the list of resources contained in a {@link TryStatement}
-   *
    * @param ¢ JD
    * @return a reference to the list of resources contained in the argument */
   @SuppressWarnings("unchecked") static List<VariableDeclarationExpression> resources(final TryStatement ¢) {
     return ¢.resources();
   }
   /** Expose the list of resources contained in a {@link ParameterizedType}
-   *
    * @param ¢ JD
    * @return a reference to the list of resources contained in the argument */
-  @SuppressWarnings("unchecked") public static List<Type> typeArguments(final ParameterizedType ¢) {
+  @SuppressWarnings("unchecked") static List<Type> typeArguments(final ParameterizedType ¢) {
     return ¢.typeArguments();
   }
   /** Expose the list of resources contained in a {@link ParameterizedType}
-   *
    * @param ¢ JD
    * @return a reference to the list of resources contained in the argument */
-  @SuppressWarnings("unchecked") public static List<TagElement> tags(final Javadoc ¢) {
+  @SuppressWarnings("unchecked") static List<TagElement> tags(final Javadoc ¢) {
     return ¢.tags();
   }
 }
--- SpartanRefactoring/src/main/java/il/org/spartan/refactoring/utils/extract.java
+++ SpartanRefactoring/src/main/java/il/org/spartan/refactoring/utils/extract.java
@@ -1,57 +1,54 @@
 package il.org.spartan.refactoring.utils;
 
-import static il.org.spartan.idiomatic.*;
-import static il.org.spartan.refactoring.utils.Funcs.*;
-import static il.org.spartan.refactoring.utils.Restructure.*;
-import static org.eclipse.jdt.core.dom.ASTNode.*;
 import il.org.spartan.*;
 
 import java.util.*;
 
 import org.eclipse.jdt.core.dom.*;
 
-/** An empty <code><b>enum</b></code> for fluent programming. The name should say
- * it all: The name, followed by a dot, followed by a method name, should read
- * like a sentence phrase.
- *
+import static il.org.spartan.idiomatic.*;
+
+import static il.org.spartan.refactoring.utils.Funcs.*;
+import static il.org.spartan.refactoring.utils.Restructure.*;
+import static org.eclipse.jdt.core.dom.ASTNode.*;
+
+/** An empty <code><b>interface</b></code> for fluent programming. The name
+ * should say it all: The name, followed by a dot, followed by a method name,
+ * should read like a sentence phrase.
  * @author Yossi Gil
  * @since 2015-07-28 */
-public enum extract {
-  ;
+public interface extract {
   /** Retrieve all operands, including parenthesized ones, under an expression
-   *
    * @param e JD
    * @return a {@link List} of all operands to the parameter */
-  public static List<Expression> allOperands(final InfixExpression e) {
+  static List<Expression> allOperands(final InfixExpression e) {
     return extract.operands(flatten(e));
   }
   /** @param n a statement or block to extract the assignment from
    * @return null if the block contains more than one statement or if the
    *         statement is not an assignment or the assignment if it exists */
-  public static Assignment assignment(final ASTNode n) {
+  static Assignment assignment(final ASTNode n) {
     return asAssignment(expression(extract.expressionStatement(n)));
   }
   /** Find the "core" of a given {@link Expression}, by peeling of any
    * parenthesis that may wrap it.
-   *
    * @param $ JD
    * @return the parameter itself, if not parenthesized, or the result of
    *         applying this function (@link {@link #getClass()}) to whatever is
    *         wrapped in these parenthesis. */
-  public static Expression core(final Expression $) {
+  static Expression core(final Expression $) {
     return $ == null || $.getNodeType() != PARENTHESIZED_EXPRESSION ? $ : core(((ParenthesizedExpression) $).getExpression());
   }
   /** Computes the "essence" of a statement, i.e., if a statement is essentially
    * a single, non-empty, non-block statement, possibly wrapped in brackets,
    * perhaps along with any number of empty statements, then its essence is this
    * single non-empty statement.
-   *
    * @param s JD
    * @return the essence of the parameter, or <code><b>null</b></code>, if there
    *         are no non-empty statements within the parameter. If, however there
    *         are multiple non-empty statements inside the parameter then the
    *         parameter itself is returned. */
-  public static Statement core(final Statement s) {
+  static Statement core(final Statement s) {
     if (Scalpel.isInaccessible(s))
       return s;
     final List<Statement> ss = extract.statements(s);
@@ -64,13 +61,13 @@ public enum extract {
         return s;
     }
   }
-  @SuppressWarnings("unchecked") public static List<Expression> dimensions(final ArrayCreation c) {
+  @SuppressWarnings("unchecked") static List<Expression> dimensions(final ArrayCreation c) {
     return c.dimensions();
   }
   /** @param n a node to extract an expression from
    * @return null if the statement is not an expression, nor a return statement,
    *         nor a throw statement. Otherwise, the expression in these. */
-  public static Expression expression(final ASTNode n) {
+  static Expression expression(final ASTNode n) {
     if (n == null)
       return null;
     switch (n.getNodeType()) {
@@ -84,28 +81,26 @@ public enum extract {
         return null;
     }
   }
-  public static List<Expression> expressions(final ArrayCreation c) {
+  static List<Expression> expressions(final ArrayCreation c) {
     return c == null ? null : expressions(c.getInitializer());
   }
   @SuppressWarnings("unchecked") public static List<Expression> expressions(final ArrayInitializer i) {
     return i.expressions();
   }
   /** Convert, is possible, an {@link ASTNode} to a {@link ExpressionStatement}
-   *
    * @param n a statement or a block to extract the expression statement from
    * @return the expression statement if n is a block or an expression statement
    *         or null if it not an expression statement or if the block contains
    *         more than one statement */
-  public static ExpressionStatement expressionStatement(final ASTNode n) {
+  static ExpressionStatement expressionStatement(final ASTNode n) {
     return n == null ? null : asExpressionStatement(extract.singleStatement(n));
   }
   /** Search for a {@link PrefixExpression} in the tree rooted at an
    * {@link ASTNode}.
-   *
    * @param n JD
    * @return the first {@link PrefixExpression} found in an {@link ASTNode n},
    *         or <code><b>null</b> if there is no such statement. */
-  public static PostfixExpression findFirstPostfix(final ASTNode n) {
+  static PostfixExpression findFirstPostfix(final ASTNode n) {
     final maybe<PostfixExpression> $ = maybe.no();
     n.accept(new ASTVisitor() {
       @Override public boolean visit(final PostfixExpression ¢) {
@@ -117,11 +112,10 @@ public enum extract {
     return $.get();
   }
   /** Search for an {@link IfStatement} in the tree rooted at an {@link ASTNode}.
-   *
    * @param n JD
    * @return the first {@link IfStatement} found in an {@link ASTNode n}, or
    *         <code><b>null</b> if there is no such statement. */
-  public static IfStatement firstIfStatement(final ASTNode n) {
+  static IfStatement firstIfStatement(final ASTNode n) {
     if (n == null)
       return null;
     final maybe<IfStatement> $ = maybe.no();
@@ -136,11 +130,10 @@ public enum extract {
   }
   /** Search for an {@link MethodDeclaration} in the tree rooted at an
    * {@link ASTNode}.
-   *
    * @param n JD
    * @return the first {@link IfStatement} found in an {@link ASTNode n}, or
    *         <code><b>null</b> if there is no such statement. */
-  public static MethodDeclaration firstMethodDeclaration(final ASTNode n) {
+  static MethodDeclaration firstMethodDeclaration(final ASTNode n) {
     final maybe<MethodDeclaration> $ = maybe.no();
     n.accept(new ASTVisitor() {
       @Override public boolean visit(final MethodDeclaration d) {
@@ -153,12 +146,11 @@ public enum extract {
   }
   /** Find the first {@link InfixExpression} representing an addition, under a
    * given node, as found in the usual visitation order.
-   *
    * @param n JD
    * @return the first {@link InfixExpression} representing an addition under
    *         the parameter given node, or <code><b>null</b></code> if no such
    *         value could be found. */
-  public static InfixExpression firstPlus(final ASTNode n) {
+  static InfixExpression firstPlus(final ASTNode n) {
     final maybe<InfixExpression> $ = maybe.no();
     n.accept(new ASTVisitor() {
       @Override public boolean visit(final InfixExpression e) {
@@ -172,7 +164,7 @@ public enum extract {
     });
     return $.get();
   }
-  public static Type firstType(final Statement s) {
+  static Type firstType(final Statement s) {
     final maybe<Type> $ = maybe.no();
     s.accept(new ASTVisitor() {
       @Override public boolean preVisit2(final ASTNode n) {
@@ -186,11 +178,10 @@ public enum extract {
   }
   /** Return the first {@link VariableDeclarationFragment} encountered in a visit
    * of the tree rooted a the parameter.
-   *
    * @param n JD
    * @return the first such node encountered in a visit of the tree rooted a the
    *         parameter, or <code><b>null</b></code> */
-  public static VariableDeclarationFragment firstVariableDeclarationFragment(final ASTNode n) {
+  static VariableDeclarationFragment firstVariableDeclarationFragment(final ASTNode n) {
     if (n == null)
       return null;
     final maybe<VariableDeclarationFragment> $ = maybe.no();
@@ -210,30 +201,27 @@ public enum extract {
     return s.fragments();
   }
   /** Extract the single {@link ReturnStatement} embedded in a node.
-   *
    * @param n JD
    * @return the single {@link IfStatement} embedded in the parameter or
    *         <code><b>null</b></code> if not such statements exists. */
-  public static IfStatement ifStatement(final ASTNode n) {
+  static IfStatement ifStatement(final ASTNode n) {
     return asIfStatement(extract.singleStatement(n));
   }
   @SuppressWarnings("unchecked") public static List<VariableDeclarationExpression> initializers(final ForStatement s) {
     return s.initializers();
   }
   /** Find the last statement residing under a given {@link Statement}
-   *
    * @param s JD
    * @return the last statement residing under a given {@link Statement}, or
    *         <code><b>null</b></code> if not such statements exists. */
-  public static ASTNode lastStatement(final Statement s) {
+  static ASTNode lastStatement(final Statement s) {
     return last(statements(s));
   }
   /** Extract the {@link MethodDeclaration} that contains a given node.
-   *
    * @param n JD
    * @return the inner most {@link MethodDeclaration} in which the parameter is
    *         nested, or <code><b>null</b></code>, if no such statement exists. */
-  public static MethodDeclaration methodDeclaration(final ASTNode n) {
+  static MethodDeclaration methodDeclaration(final ASTNode n) {
     for (ASTNode $ = n; $ != null; $ = $.getParent())
       if (Is.methodDeclaration($))
         return asMethodDeclaration($);
@@ -242,50 +230,45 @@ public enum extract {
   /** @param n JD
    * @return the method invocation if it exists or null if it doesn't or if the
    *         block contains more than one statement */
-  public static MethodInvocation methodInvocation(final ASTNode n) {
+  static MethodInvocation methodInvocation(final ASTNode n) {
     return asMethodInvocation(extract.expressionStatement(n).getExpression());
   }
-  public static List<IExtendedModifier> modifers(final VariableDeclarationFragment f) {
+  static List<IExtendedModifier> modifers(final VariableDeclarationFragment f) {
     return modifiers((VariableDeclarationStatement) f.getParent());
   }
   /** Find the {@link Assignment} that follows a given node.
-   *
    * @param n JD
    * @return the {@link Assignment} that follows the parameter, or
    *         <code><b>null</b></code> if not such value exists. */
-  public static Assignment nextAssignment(final ASTNode n) {
+  static Assignment nextAssignment(final ASTNode n) {
     return extract.assignment(nextStatement(n));
   }
   /** Extract the {@link IfStatement} that immediately follows a given node
-   *
    * @param n JD
    * @return the {@link IfStatement} that immediately follows the parameter, or
    *         <code><b>null</b></code>, if no such statement exists. */
-  public static IfStatement nextIfStatement(final ASTNode n) {
+  static IfStatement nextIfStatement(final ASTNode n) {
     return asIfStatement(nextStatement(n));
   }
   /** Extract the {@link ReturnStatement} that immediately follows a given node
-   *
    * @param n JD
    * @return the {@link ReturnStatement} that immediately follows the parameter,
    *         or <code><b>null</b></code>, if no such statement exists. */
-  public static ReturnStatement nextReturn(final ASTNode n) {
+  static ReturnStatement nextReturn(final ASTNode n) {
     return asReturnStatement(nextStatement(n));
   }
   /** Extract the {@link Statement} that immediately follows a given node.
-   *
    * @param n JD
    * @return the {@link Statement} that immediately follows the parameter, or
    *         <code><b>null</b></code>, if no such statement exists. */
-  public static Statement nextStatement(final ASTNode n) {
+  static Statement nextStatement(final ASTNode n) {
     return nextStatement(extract.statement(n));
   }
   /** Extract the {@link Statement} that immediately follows a given statement
-   *
    * @param s JD
    * @return the {@link Statement} that immediately follows the parameter, or
    *         <code><b>null</b></code>, if no such statement exists. */
-  public static Statement nextStatement(final Statement s) {
+  static Statement nextStatement(final Statement s) {
     if (s == null)
       return null;
     final Block b = asBlock(s.getParent());
@@ -293,10 +276,9 @@ public enum extract {
   }
   /** Makes a list of all operands of an expression, comprising the left operand,
    * the right operand, followed by extra operands when they exist.
-   *
    * @param e JD
    * @return a list of all operands of an expression */
-  public static List<Expression> operands(final InfixExpression e) {
+  static List<Expression> operands(final InfixExpression e) {
     if (e == null)
       return null;
     final List<Expression> $ = new ArrayList<>();
@@ -309,52 +291,47 @@ public enum extract {
     return $;
   }
   /** Finds the expression returned by a return statement
-   *
    * @param n a node to extract an expression from
    * @return null if the statement is not an expression or return statement or
    *         the expression if they are */
-  public static Expression returnExpression(final ASTNode n) {
+  static Expression returnExpression(final ASTNode n) {
     return extract.expression(extract.returnStatement(n));
   }
   /** Extract the single {@link ReturnStatement} embedded in a node.
-   *
    * @param n JD
    * @return the single {@link ReturnStatement} embedded in the parameter, and
    *         return it; <code><b>null</b></code> if not such statements exists. */
-  public static ReturnStatement returnStatement(final ASTNode n) {
+  static ReturnStatement returnStatement(final ASTNode n) {
     return asReturnStatement(extract.singleStatement(n));
   }
   /** Finds the single statement in the <code><b>else</b></code> branch of an
    * {@link IfStatement}
-   *
    * @param s JD
    * @return the single statement in the <code><b>else</b></code> branch of the
    *         parameter, or <code><b>null</b></code>, if no such statement
    *         exists. */
-  public static Statement singleElse(final IfStatement s) {
+  static Statement singleElse(final IfStatement s) {
     return extract.singleStatement(elze(s));
   }
   /** @param n JD
    * @return if b is a block with just 1 statement it returns that statement, if
    *         b is statement it returns b and if b is null it returns a null */
-  public static Statement singleStatement(final ASTNode n) {
+  static Statement singleStatement(final ASTNode n) {
     final List<Statement> $ = extract.statements(n);
     return unless($.size() != 1).eval(() -> $.get(0));
   }
   /** Finds the single statement in the "then" branch of an {@link IfStatement}
-   *
    * @param s JD
    * @return the single statement in the "then" branch of the parameter, or
    *         <code><b>null</b></code>, if no such statement exists. */
-  public static Statement singleThen(final IfStatement s) {
+  static Statement singleThen(final IfStatement s) {
     return extract.singleStatement(then(s));
   }
   /** Extract the {@link Statement} that contains a given node.
-   *
    * @param n JD
    * @return the inner most {@link Statement} in which the parameter is nested,
    *         or <code><b>null</b></code>, if no such statement exists. */
-  public static Statement statement(final ASTNode n) {
+  static Statement statement(final ASTNode n) {
     for (ASTNode $ = n; $ != null; $ = $.getParent())
       if (Is.statement($))
         return asStatement($);
@@ -362,10 +339,9 @@ public enum extract {
   }
   /** Extract the list of non-empty statements embedded in node (nesting within
    * control structure such as <code><b>if</b></code> are not removed.)
-   *
    * @param n JD
    * @return the list of such statements. */
-  public static List<Statement> statements(final ASTNode n) {
+  static List<Statement> statements(final ASTNode n) {
     final List<Statement> $ = new ArrayList<>();
     return n == null || !(n instanceof Statement) ? $ : extract.statementsInto((Statement) n, $);
   }
@@ -375,29 +351,28 @@ public enum extract {
   /** @param n a node to extract an expression from
    * @return null if the statement is not an expression or return statement or
    *         the expression if they are */
-  public static Expression throwExpression(final ASTNode n) {
+  static Expression throwExpression(final ASTNode n) {
     final ThrowStatement $ = asThrowStatement(extract.singleStatement(n));
     return $ == null ? null : $.getExpression();
   }
   /** Extract the single {@link ThrowStatement} embedded in a node.
-   *
    * @param n JD
    * @return the single {@link ThrowStatement} embedded in the parameter, and
    *         return it; <code><b>null</b></code> if not such statements exists. */
-  public static ThrowStatement throwStatement(final ASTNode n) {
+  static ThrowStatement throwStatement(final ASTNode n) {
     return asThrowStatement(extract.singleStatement(n));
   }
   /** @param e
    * @return */
-  private static Expression expression(final ExpressionStatement s) {
+  static Expression expression(final ExpressionStatement s) {
     return s == null ? null : s.getExpression();
   }
   /** @param $
    * @return */
-  private static Expression expression(final ReturnStatement $) {
+  static Expression expression(final ReturnStatement $) {
     return $ == null ? null : $.getExpression();
   }
-  private static <T> T last(final List<T> ts) {
+  static <T> T last(final List<T> ts) {
     if (ts == null)
       return null;
     T $ = null;
@@ -405,26 +380,26 @@ public enum extract {
       $ = t;
     return $;
   }
-  @SuppressWarnings("unchecked") private static List<IExtendedModifier> modifiers(final VariableDeclarationStatement s) {
+  @SuppressWarnings("unchecked") static List<IExtendedModifier> modifiers(final VariableDeclarationStatement s) {
     return s.modifiers();
   }
-  private static Statement next(final Statement s, final List<Statement> ss) {
+  static Statement next(final Statement s, final List<Statement> ss) {
     for (int i = 0; i < ss.size() - 1; ++i)
       if (ss.get(i) == s)
         return ss.get(i + 1);
     return null;
   }
-  private static List<Statement> statementsInto(final Block b, final List<Statement> $) {
+  static List<Statement> statementsInto(final Block b, final List<Statement> $) {
     for (final Object statement : b.statements())
       extract.statementsInto((Statement) statement, $);
     return $;
   }
-  private static List<Statement> statementsInto(final Statement s, final List<Statement> $) {
+  static List<Statement> statementsInto(final Statement s, final List<Statement> $) {
     switch (s.getNodeType()) {
       case EMPTY_STATEMENT:
         return $;
       case BLOCK:
-        return extract.statementsInto((Block) s, $);
+        return extract.statementsInto(asBlock(s), $);
       default:
         $.add(s);
         return $;
