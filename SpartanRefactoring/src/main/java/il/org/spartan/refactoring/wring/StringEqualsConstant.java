e57ebff6f4cbd7b63f85cc922553c849e51e2724
Yossi Gil <yossi.gil@gmail.com>
2016-08-06 21:13:45 +0300

    format file and remove compilation errors

--- SpartanRefactoring/src/main/java/il/org/spartan/refactoring/wring/StringEqualsConstant.java
+++ SpartanRefactoring/src/main/java/il/org/spartan/refactoring/wring/StringEqualsConstant.java
@@ -1,40 +1,32 @@
 package il.org.spartan.refactoring.wring;
 
 import il.org.spartan.refactoring.preferences.*;
+import il.org.spartan.refactoring.preferences.PluginPreferencesResources.*;
 import il.org.spartan.refactoring.utils.*;
 import il.org.spartan.refactoring.wring.Wring.ReplaceCurrentNode;
-
 import java.util.*;
-
 import org.eclipse.jdt.core.dom.*;
 
-/**
- * Used to replace
+/** Used to replace
  *
  * <pre>
- * <code>
  * s.equals("s")
  * </pre>
  *
  * </code> with safer
  *
  * <pre>
- * <code>
  * "s".equals(s)
  * </pre>
  *
- * </code>
- *
  * @author Ori Roth
- * @since 2016/05/08
- */
-public class StringEqualsConstant extends ReplaceCurrentNode<MethodInvocation> implements Kind.ReorganizeExpression {
+ * @since 2016/05/08 */
+public class StringEqualsConstant extends ReplaceCurrentNode<MethodInvocation> {
   final static String[] _mns = { "equals", "equalsIgnoreCase" };
   final static List<String> mns = Arrays.asList(_mns);
-
   @SuppressWarnings("unchecked") @Override ASTNode replacement(final MethodInvocation i) {
-    if (!mns.contains(i.getName().toString()) || i.arguments().size() != 1 || i.getExpression() == null
-        || i.getExpression() instanceof StringLiteral || !(i.arguments().get(0) instanceof StringLiteral))
+    if (!mns.contains(i.getName().toString()) || i.arguments().size() != 1 || i.getExpression() == null || i.getExpression() instanceof StringLiteral
+        || !(i.arguments().get(0) instanceof StringLiteral))
       return null;
     final MethodInvocation $ = i.getAST().newMethodInvocation();
     $.setExpression(Funcs.duplicate((Expression) i.arguments().get(0)));
@@ -43,7 +35,9 @@ public class StringEqualsConstant extends ReplaceCurrentNode<MethodInvocation> i
     return $;
   }
   @Override String description(final MethodInvocation i) {
-    return "use " + i.arguments().get(0) + "." + i.getName().toString() + "(" + i.getExpression().toString() + ") instead of "
-        + i.toString();
+    return "use " + i.arguments().get(0) + "." + i.getName() + "(" + i.getExpression() + ") instead of " + i;
+  }
+  @Override WringGroup wringGroup() {
+    return WringGroup.REORDER_EXPRESSIONS;
   }
 }
