661d91695eb5e57a2c357dc40007e8f2b1026e0f
Yossi Gil <yossi.gil@gmail.com>
2016-08-08 11:45:36 +0300

    Formatting

--- SpartanRefactoring/src/main/java/il/org/spartan/refactoring/wring/VariableRenameUnderscoreToDoubleUnderscore.java
+++ SpartanRefactoring/src/main/java/il/org/spartan/refactoring/wring/VariableRenameUnderscoreToDoubleUnderscore.java
@@ -1,17 +1,14 @@
 package il.org.spartan.refactoring.wring;
 
 import org.eclipse.jdt.core.dom.*;
-
 import il.org.spartan.refactoring.preferences.*;
+import il.org.spartan.refactoring.preferences.PluginPreferencesResources.*;
 
-/**
- * Replaces name of variables named "_" into "__"
- *
+/** Replaces name of variables named "_" into "__"
  * @author Ori Roth
  * @param <N> either SingleVariableDeclaration or VariableDeclarationFragment
- * @since 2016/05/08
- */
-public class VariableRenameUnderscoreToDoubleUnderscore<N extends VariableDeclaration> extends Wring.VariableChangeName<N>  {
+ * @since 2016/05/08 */
+public class VariableRenameUnderscoreToDoubleUnderscore<N extends VariableDeclaration> extends VariableChangeName<N> {
   @Override boolean change(final N n) {
     return "_".equals(n.getName().toString());
   }
@@ -20,5 +17,8 @@ public class VariableRenameUnderscoreToDoubleUnderscore<N extends VariableDeclar
   }
   @Override String description(@SuppressWarnings("unused") final N __) {
     return "Use double underscore instead a single underscore";
+  }
+  @Override WringGroup wringGroup() {
+    return WringGroup.RENAME_PARAMETERS;
   }
 }
