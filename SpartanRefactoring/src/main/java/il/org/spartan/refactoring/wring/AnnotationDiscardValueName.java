ce3b2928f673ab186e8ed4f567f96d69c4a2fedd
Yossi Gil <yossi.gil@gmail.com>
2016-08-08 12:13:42 +0300

    Import annotation wrings

---
+++ SpartanRefactoring/src/main/java/il/org/spartan/refactoring/wring/AnnotationDiscardValueName.java
@@ -0,0 +1,33 @@
+package il.org.spartan.refactoring.wring;
+
+import static il.org.spartan.refactoring.utils.Funcs.*;
+
+import org.eclipse.jdt.core.dom.*;
+
+import il.org.spartan.refactoring.preferences.*;
+
+/**
+ * A {@link Wring} to remove the "value" member from annotations that only have
+ * a single member, converting
+ * <code>@SuppressWarnings(value = "unchecked")</code> to
+ * <code>@SuppressWarnings("unchecked")</code>
+ *
+ * @author Daniel Mittelman <code><mittelmania [at] gmail.com></code>
+ * @since 2016-04-02
+ */
+public class AnnotationDiscardValueName extends Wring.ReplaceCurrentNode<NormalAnnotation> implements Kind.OPTIMIZE_ANNOTATIONS {
+  @Override ASTNode replacement(final NormalAnnotation a) {
+    if (a.values().size() != 1)
+      return null;
+    final MemberValuePair p = (MemberValuePair) a.values().get(0);
+    if (!"value".equals(p.getName().toString()))
+      return null;
+    final SingleMemberAnnotation $ = a.getAST().newSingleMemberAnnotation();
+    $.setTypeName(newSimpleName(a, a.getTypeName().getFullyQualifiedName()));
+    $.setValue(duplicate(p.getValue()));
+    return $;
+  }
+  @Override String description(final NormalAnnotation a) {
+    return "Discard the \"value\" member from the @" + a.getTypeName().getFullyQualifiedName() + " annotation";
+  }
+}
---
+++ SpartanRefactoring/src/main/java/il/org/spartan/refactoring/wring/AnnotationRemoveEmptyParentheses.java
@@ -0,0 +1,29 @@
+package il.org.spartan.refactoring.wring;
+
+import static il.org.spartan.refactoring.utils.Funcs.*;
+
+import org.eclipse.jdt.core.dom.*;
+
+import il.org.spartan.refactoring.preferences.*;
+
+/**
+ * A {@link Wring} to remove the parentheses from annotations that do not take
+ * arguments, converting <code><pre>@Override()</pre></code> to
+ * <code><pre>@Override</pre></code>
+ *
+ * @author Daniel Mittelman <code><mittelmania [at] gmail.com></code>
+ * @since 2016-04-02
+ */
+public class AnnotationRemoveEmptyParentheses extends Wring.ReplaceCurrentNode<NormalAnnotation> implements
+    Kind.OPTIMIZE_ANNOTATIONS {
+  @Override ASTNode replacement(final NormalAnnotation a) {
+    if (a.values().size() > 0)
+      return null;
+    final MarkerAnnotation $ = a.getAST().newMarkerAnnotation();
+    $.setTypeName(duplicate(a.getTypeName()));
+    return $;
+  }
+  @Override String description(final NormalAnnotation a) {
+    return "Remove redundant parentheses from the @" + a.getTypeName().getFullyQualifiedName() + " annotation";
+  }
+}
