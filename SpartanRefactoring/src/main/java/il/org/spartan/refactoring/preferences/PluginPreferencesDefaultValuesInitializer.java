c2d5f9ff4c1c6036e9e738ee120f04e0fa96e78f
Yossi Gil <yossi.gil@gmail.com>
2016-08-13 20:15:29 +0300

    Duplicate imports

--- SpartanRefactoring/src/main/java/il/org/spartan/refactoring/preferences/PluginPreferencesDefaultValuesInitializer.java
+++ SpartanRefactoring/src/main/java/il/org/spartan/refactoring/preferences/PluginPreferencesDefaultValuesInitializer.java
@@ -4,7 +4,6 @@ import static il.org.spartan.refactoring.preferences.PluginPreferencesResources.
 import static il.org.spartan.refactoring.preferences.PluginPreferencesResources.WringGroup.*;
 import org.eclipse.core.runtime.preferences.*;
 import org.eclipse.jface.preference.*;
-import il.org.spartan.refactoring.preferences.PluginPreferencesResources.*;
 
 /** This class is called by Eclipse when the plugin is first loaded and has no
  * default preference values. These are set by the values specified here.
