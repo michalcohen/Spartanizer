0ae5618debaf326fc01d7f9c9934c6427b85fc80
Yossi Gil <yossi.gil@gmail.com>
2016-08-18 11:05:43 +0300

    Use our ASTYPE convention for wrings

--- src/main/java/il/org/spartan/refactoring/wring/CastToDouble2Multiply1.java
+++ src/main/java/il/org/spartan/refactoring/wring/CastToDouble2Multiply1.java
@@ -9,23 +9,24 @@ import org.eclipse.jdt.core.dom.*;
 import il.org.spartan.refactoring.preferences.PluginPreferencesResources.*;
 import il.org.spartan.refactoring.utils.*;
 
-/** A {@link Wring} that replaces the (double)X cast {@link Expressions} with the form of '1.*X'
- * @author Alex and Dan
- * @since 2016-09-15 */
-public final class DoubleCastConversion extends Wring.ReplaceCurrentNode<CastExpression> {
+/** Replace <code>(double)X</code> by <code>1.*X</code>
+ * @author Alex Kopzon 
+ * @author Dan Greenstein
+ * @since 2016 */
+public final class CastToDouble2Multiply1 extends Wring.ReplaceCurrentNode<CastExpression> {
   @Override String description(final CastExpression e) {
     return "Replace all (double) casts to 1.* in " + e;
   }
   @Override ASTNode replacement(CastExpression e) {
     return (!(e.getType().isPrimitiveType())) ? null : ((e.getType().toString() != "double") ? null : replaceDoubleToOne(e));
   }
-  /**The actual replace in done here.
+  /** The actual replace in done here.
    * @param e JD
-   * @return {@link ASTNode} which represents cast to (double).
-   */
+   * @return {@link ASTNode} which represents cast to (double). */
   private static ASTNode replaceDoubleToOne(final CastExpression e) {
     List<Expression> $ = new ArrayList<>();
-    $.add((Expression)MakeAST.EXPRESSION.from("1.")); //how to convert a string to Expression
+    $.add((Expression) MakeAST.EXPRESSION.from("1.")); // how to convert a
+                                                       // string to Expression
     $.add(e.getExpression());
     return subject.operands($).to(TIMES);
   }
