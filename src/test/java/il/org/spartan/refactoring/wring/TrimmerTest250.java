bf1225d8391a5bbf6ab8ec6c2650a38a9fef22eb
M4tt30rru <matteo.orru@diee.unica.it>
2016-08-21 15:25:40 +0300

    added tests for issue #75

--- src/test/java/il/org/spartan/refactoring/wring/TrimmerTest250.java
+++ src/test/java/il/org/spartan/refactoring/wring/TrimmerTest250.java
@@ -480,4 +480,96 @@ public class TrimmerTest250 {
     trimming("static abstract final enum a {x,y,z; void f() {}}")//
         .to("enum a {x,y,z; void f() {}}");//
   }
+  
+  @Test public void issue75a() {
+    trimming("int i = 0").to(null);
+  }
+
+  @Test public void issue75b() {
+    trimming("int i = +1;").to("int i = 1;");
+  }
+
+  @Test public void issue75c() {
+    trimming("int i = +a;").to("int i = a;");
+  }
+
+  @Test public void issue75d() {
+    trimming("+ 0").to("0");
+  }
+
+  @Test public void issue75e() {
+    trimming("a = +0").to("a = 0");
+  }
+
+  @Test public void issue75f() {
+    trimming("a = 1+0").to("a = 1");
+  }
+
+  @Test public void issue75g() {
+    trimming("i=0").to(null);
+  }
+
+  @Test public void issue75h() {
+    trimming("int i; i = +0;").to("int i = +0;").to("int i=0;");
+  }
+
+  @Test public void issue75i() {
+    trimming("+0").to("0");
+  }
+
+  @Test public void issue75i0() {
+    trimming("-+-+2").to("--+2");
+  }
+
+  @Test public void issue75i1() {
+    trimming("+0").to("0");
+  }
+
+  @Test public void issue75i2() {
+    trimming("+1").to("1");
+  }
+
+  @Test public void issue75i3() {
+    trimming("+-1").to("-1");
+  }
+
+  @Test public void issue75i4() {
+    trimming("+1.0").to("1.0");
+  }
+
+  @Test public void issue75i5() {
+    trimming("+'0'").to("'0'");
+  }
+
+  @Test public void issue75i6() {
+    trimming("+1L").to("1L");
+  }
+
+  @Test public void issue75i7() {
+    trimming("+0F").to("0F");
+  }
+
+  @Test public void issue75i8() {
+    trimming("+0L").to("0L");
+  }
+
+  @Test public void issue75j() {
+    trimming("+1E3").to("1E3");
+  }
+
+  @Test public void issue75k() {
+    trimming("(+(+(+x)))").to("(x)");
+  }
+
+  @Test public void issue75il() {
+    trimming("+(a+b)").to("a+b");
+  }
+
+  @Test public void issue75m() {
+    trimming("+ + + i").to("i");
+  }
+
+  @Test public void issue75n() {
+    trimming("(2*+(a+b))").to("(2*(a+b))");
+  }
 }
