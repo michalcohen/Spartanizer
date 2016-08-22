d4e129e7dd250793cc222b1e169938f705c4e7f2
Yossi Gil <yossi.gil@gmail.com>
2016-08-22 12:50:17 +0300

    Addeded tests

--- src/main/java/il/org/spartan/refactoring/utils/AdditiveTermsCollector.java
+++ src/main/java/il/org/spartan/refactoring/utils/AdditiveTermsCollector.java
@@ -1,11 +1,16 @@
 package il.org.spartan.refactoring.utils;
 
+import static il.org.spartan.azzert.*;
 import static il.org.spartan.refactoring.utils.Funcs.*;
+import static il.org.spartan.refactoring.utils.Into.*;
 import static il.org.spartan.refactoring.utils.extract.*;
 
 import java.util.*;
 
 import org.eclipse.jdt.core.dom.*;
+import org.junit.*;
+
+import il.org.spartan.*;
 
 public class AdditiveTermsCollector {
   /** Opens the parenthesis in +/-_expressions, e.g., <code>a-(b+c-(d+e))</code>
@@ -16,11 +21,12 @@ public class AdditiveTermsCollector {
     if (e == null || isLeafTerm(e)) // Nothing to do
       return null;
     collectPlusPrefix(e);
-    return merge();
+    return null;
   }
 
   static boolean isLeafTerm(final Expression e) {
-    return e == null || Is.infixPlus(e) && !Is.infixMinus(e);
+    assert e != null;
+    return !Is.infixPlus(e) && !Is.infixMinus(e);
   }
 
   Void collectPlusPrefix(final InfixExpression e) {
@@ -35,15 +41,15 @@ public class AdditiveTermsCollector {
     return isLeafTerm(e) ? addPlusTerm(e) : collectPlusPrefix(asInfixExpression(e));
   }
 
-  private Void collectPlusPrefixMinusExpression(final InfixExpression e) {
+  Void collectPlusPrefixMinusExpression(final InfixExpression e) {
     final List<Expression> es = operands(e);
-    collectPositiveTerm(first(es));
+    collectPositiveTerm(core(first(es)));
     return collectNegativeTerms(rest(es));
   }
 
   private Void collectNegativeTerms(final Iterable<Expression> es) {
     for (final Expression e : es)
-      collectNegativeTerm(e);
+      collectNegativeTerm(core(e));
     return null;
   }
 
@@ -54,7 +60,7 @@ public class AdditiveTermsCollector {
   }
 
   private Void collectMinusPrefix(final InfixExpression e) {
-    return e.getOperator() == PLUS2 ? collectMinusPrefixPlusExpression(e) : collectMinusPrefixMinusExprssion(e);
+    return e == null ? null : e.getOperator() == PLUS2 ? collectMinusPrefixPlusExpression(e) : collectMinusPrefixMinusExprssion(e);
   }
 
   private Void collectMinusPrefixPlusExpression(final InfixExpression e) {
@@ -63,13 +69,13 @@ public class AdditiveTermsCollector {
 
   private Void collectMinusPrefixMinusExprssion(final InfixExpression e) {
     final List<Expression> es = operands(e);
-    collectNegativeTerm(first(es));
+    collectNegativeTerm(core(first(es)));
     return collectPositiveTerms(rest(es));
   }
 
   private Void collectPositiveTerms(final Iterable<Expression> es) {
     for (final Expression e : es)
-      collectPositiveTerm(e);
+      collectPositiveTerm(core(e));
     return null;
   }
 
@@ -83,11 +89,128 @@ public class AdditiveTermsCollector {
     return null;
   }
 
-  private Expression merge() {
-    plus.addAll(minus);
-    return null;
-  }
-
   public final List<Expression> plus = new ArrayList<>();
   public final List<Expression> minus = new ArrayList<>();
+
+  @SuppressWarnings("static-method") public static class TEST {
+    @Test public void seriesA_1() {
+      azzert.aye(isLeafTerm(e("i")))//
+          .andAye(isLeafTerm(e("i*j"))) //
+          .andAye(isLeafTerm(e("(x)")));
+    }
+
+    @Test public void seriesA_2() {
+      azzert.aye(isLeafTerm(e("(i*j)")));
+      azzert.aye(isLeafTerm(e("(i+j)")));
+      azzert.nay(isLeafTerm(e("i+j")));
+      azzert.nay(isLeafTerm(e("i-j")));
+    }
+
+    @Test public void seriesA_3() {
+      azzert.nay(Is.infixPlus(e("(i+j)")));
+      azzert.aye(Is.infixPlus(core(e("(i+j)"))));
+      azzert.nay(Is.infixMinus(e("(i-j)")));
+      azzert.aye(Is.infixMinus(core(e("(i-j)"))));
+    }
+
+    @Test public void seriesA_4() {
+      final AdditiveTermsCollector c = new AdditiveTermsCollector();
+      final InfixExpression i = i("a + b +c");
+      c.collect(i);
+      azzert.that(c.plus.size(), is(3));
+      azzert.that(c.minus.size(), is(0));
+    }
+
+    @Test public void seriesA_5() {
+      final AdditiveTermsCollector c = new AdditiveTermsCollector();
+      final InfixExpression i = i("a-c");
+      azzert.aye(i.getOperator() == MINUS2);
+      azzert.that(left(i), iz("a"));
+      azzert.that(right(i), iz("c"));
+      c.collect(i);
+      azzert.that(c.plus.size(), is(1));
+      azzert.that(c.minus.size(), is(1));
+    }
+
+    @Test public void seriesA_5a() {
+      final AdditiveTermsCollector c = new AdditiveTermsCollector();
+      final InfixExpression i = i("a-c");
+      azzert.aye(i.getOperator() == MINUS2);
+      azzert.that(left(i), iz("a"));
+      azzert.that(right(i), iz("c"));
+      c.collectPlusPrefix(i);
+      azzert.that(c.plus.size(), is(1));
+      azzert.that(c.minus.size(), is(1));
+    }
+
+    @Test public void seriesA_6() {
+      final AdditiveTermsCollector c = new AdditiveTermsCollector();
+      final InfixExpression i = i("a + b -c");
+      azzert.aye(i.getOperator() == MINUS2);
+      azzert.aye(asInfixExpression(left(i)).getOperator() == PLUS2);
+      c.collect(i);
+      azzert.that(c.plus.size(), is(2));
+      azzert.that(c.minus.size(), is(1));
+    }
+
+    @Test public void seriesA_7() {
+      final AdditiveTermsCollector c = new AdditiveTermsCollector();
+      final InfixExpression i = i("a + (b -c)");
+      c.collect(i);
+      azzert.that(c.plus.size(), is(2));
+      azzert.that(c.minus.size(), is(1));
+    }
+
+    @Test public void seriesA_8() {
+      final AdditiveTermsCollector c = new AdditiveTermsCollector();
+      final InfixExpression i = i("a + (b +(d + c))");
+      c.collect(i);
+      azzert.that(c.plus.size(), is(4));
+      azzert.that(c.minus.size(), is(0));
+    }
+
+    @Test public void seriesB_1() {
+      final AdditiveTermsCollector c = new AdditiveTermsCollector();
+      final InfixExpression i = i("a - (b - c - (d - e - f - g))");
+      c.collect(i);
+      azzert.that(c.plus, iz("[a,c,d]"));
+      azzert.that(c.minus, iz("[b,e,f,g]"));
+    }
+
+    @Test public void seriesB_2() {
+      final AdditiveTermsCollector c = new AdditiveTermsCollector();
+      final InfixExpression i = i("a - (b - c - d - e )");
+      c.collect(i);
+      azzert.that(c.plus.size(), is(4));
+      azzert.that(c.minus.size(), is(1));
+    }
+
+    @Test public void seriesB_3() {
+      final AdditiveTermsCollector c = new AdditiveTermsCollector();
+      final InfixExpression i = i("a - (b - c)");
+      c.collect(i);
+      azzert.that(c.plus.size(), is(2));
+    }
+
+    @Test public void seriesB_4() {
+      final AdditiveTermsCollector c = new AdditiveTermsCollector();
+      final InfixExpression i = i("a - (b - c)");
+      c.collect(i);
+      azzert.that(c.minus.size(), is(1));
+    }
+
+    @Test public void seriesB_5() {
+      final AdditiveTermsCollector c = new AdditiveTermsCollector();
+      final InfixExpression i = i("a - (b - c)");
+      c.collect(i);
+      azzert.that(c.minus, iz("[b]"));
+    }
+
+    @Test public void seriesB_6() {
+      final AdditiveTermsCollector c = new AdditiveTermsCollector();
+      final InfixExpression i = i("a - (b - c)");
+      c.collectPlusPrefixMinusExpression(i);
+      azzert.that(c.minus, iz("[b]"));
+    }
+  }
 }
