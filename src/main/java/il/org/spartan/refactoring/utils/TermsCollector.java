bd0e273d160cf0533ce00cd583ed1d3656792eb2
Yossi Gil <yossi.gil@gmail.com>
2016-08-23 10:26:36 +0300

    Move SignedExpression to its own file

--- src/main/java/il/org/spartan/refactoring/utils/TermsCollector.java
+++ src/main/java/il/org/spartan/refactoring/utils/TermsCollector.java
@@ -145,38 +145,3 @@ public class TermsCollector {
     return all;
   }
 }
-
-class SignedExpression {
-  private final boolean negative;
-  public final Expression expression;
-
-  SignedExpression(final boolean minus, final Expression expression) {
-    this.negative = minus;
-    this.expression = expression;
-  }
-
-  static SignedExpression plus(Expression e) {
-    return new SignedExpression(false, e);
-  }
-
-  static SignedExpression minus(Expression e) {
-    return new SignedExpression(true, e);
-  }
-
-  boolean negative() {
-    return negative;
-  }
-
-  Expression asExpression() {
-    if (!negative)
-      return expression;
-    PrefixExpression $ = expression.getAST().newPrefixExpression();
-    $.setOperand(expression);
-    $.setOperator(MINUS1);
-    return $;
-  }
-
-  public boolean positive() {
-    return !negative;
-  }
-}
