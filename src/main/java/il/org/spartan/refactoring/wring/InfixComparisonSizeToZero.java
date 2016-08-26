d064698549095f2905b7200e7d7ac8a7e0b11278
dormaayn <dor.d.ma@gmail.com>
2016-08-26 22:10:16 +0300

    New Implementation to InfixComparisonSizeToZero, Doesnt include True&False (Issue #83)

--- src/main/java/il/org/spartan/refactoring/wring/InfixComparisonSizeToZero.java
+++ src/main/java/il/org/spartan/refactoring/wring/InfixComparisonSizeToZero.java
@@ -1,12 +1,12 @@
 package il.org.spartan.refactoring.wring;
 
 import static il.org.spartan.refactoring.utils.Funcs.*;
+//import static il.org.spartan.refactoring.utils.Is.*;
 import static il.org.spartan.refactoring.utils.extract.*;
 import static org.eclipse.jdt.core.dom.PrefixExpression.Operator.*;
 
 import org.eclipse.jdt.core.dom.*;
 import org.eclipse.jdt.core.dom.InfixExpression.*;
-
 import il.org.spartan.refactoring.utils.*;
 
 /** A {@link Wring} to change emptiness check from
@@ -14,69 +14,138 @@ import il.org.spartan.refactoring.utils.*;
  * <pre>
  * x.size() != 0
  * </pre>
- *
  * or
- *
  * <pre>
  * x.size() &gt; 0
  * </pre>
- *
  * or
- *
  * <pre>
- * x.size() &gt;= 1
+ * x.size() == 1
  * </pre>
- *
+ * or
+ * <pre>
+ * x.size() == 2
+ * </pre>
  * to
- *
  * <pre>
  * !x.isEmpty()
  * </pre>
+ * _____________
+ * <pre>
+ * x.size() == 0
+ * </pre>
+ * to
+ * <pre>
+ * x.isEmpty()
+ * </pre>
+ * _____________
+ * <pre>
+ * x.size() &gt;= 0
+ * </pre>
+ * to
+ * <pre>
+ * true
+ * </pre>
+ * _____________
+ * <pre>
+ * x.size() &lt; 0
+ * </pre>
+ * to
+ * <pre>
+ * false
+ * </pre>
  *
  * .
+ * @author Dor Ma'ayan<code><dor.d.ma [at] gmail.com></code>
+ * @author Niv Shalmon <code><shalmon.niv [at] gmail.com></code>
  * @author Ori Roth <code><ori.rothh [at] gmail.com></code>
+ * @author Stav Namir <code><stav1472 [at] gmail.com></code>
  * @since 2016-04-24 */
 public final class InfixComparisonSizeToZero extends Wring.ReplaceCurrentNode<InfixExpression> implements Kind.Canonicalization {
-  static boolean invalidTypes(final Expression ¢1, final Expression ¢2) {
-    return ¢2 instanceof MethodInvocation == ¢1 instanceof MethodInvocation //
-        && ¢2 instanceof NumberLiteral == ¢1 instanceof NumberLiteral;
+  static boolean validTypes(final Expression ¢1, final Expression ¢2) {
+    return (¢2 instanceof MethodInvocation && ¢1 instanceof NumberLiteral) //
+        || (¢2 instanceof NumberLiteral && ¢1 instanceof MethodInvocation);
   }
 
-  private static ASTNode replacement(final InfixExpression e, final Operator o, final MethodInvocation i, final NumberLiteral l) {
-    if (!"size".equals(name(i).getIdentifier()) || Double.parseDouble(l.getToken()) != 0)
-      return null;
-    final CompilationUnit u = compilationUnit(e);
-    if (u == null)
-      return null;
+  @SuppressWarnings("fallthrough") private static ASTNode replacement(final InfixExpression e, final Operator o, final MethodInvocation i,
+      final NumberLiteral l) {
+    /* final CompilationUnit u = compilationUnit(e); if (u == null) return
+     * null; */
     final Expression receiver = receiver(i);
-    final IMethodBinding b = BindingUtils.getVisibleMethod(receiver == null ? BindingUtils.container(e) : receiver.resolveTypeBinding(), "isEmpty",
-        null, e, u);
-    if (b == null)
-      return null;
-    final ITypeBinding t = b.getReturnType();
-    if (!"boolean".equals("" + t) && !"java.lang.Boolean".equals(t.getBinaryName()))
-      return null;
+    /* final IMethodBinding b = BindingUtils.getVisibleMethod(receiver == null ?
+     * BindingUtils.container(e) : receiver.resolveTypeBinding(), "isEmpty",
+     * null, e, u); if (b == null) return null; final ITypeBinding t =
+     * b.getReturnType(); if (!"boolean".equals("" + t) &&
+     * !"java.lang.Boolean".equals(t.getBinaryName())) return null; final
+     * MethodInvocation $ = subject.operand(receiver).toMethod("isEmpty");
+     * return o.equals(InfixExpression.Operator.EQUALS) ? $ :
+     * subject.operand($).to(NOT); */ // The original case assumes there is
+                                      // Binding
     final MethodInvocation $ = subject.operand(receiver).toMethod("isEmpty");
-    return o.equals(InfixExpression.Operator.EQUALS) ? $ : subject.operand($).to(NOT);
+    int number = Integer.parseInt(l.getToken());
+    switch (o.toString()) {
+      case "==":
+        if (number == 0)
+          return $;
+        if (number == 1 || number == 2)
+          return subject.operand($).to(NOT);
+        return null;
+      case "!=":
+        if (number == 0)
+          return subject.operand($).to(NOT);
+        return null;
+      case ">":
+        if (number == 0)
+          return subject.operand($).to(NOT);
+      case ">=":
+        if (number <= 0)
+          // return true
+          return null;
+      case "<=":
+        if (number == 0)
+          return $;
+      case "<":
+        if (number >= 0)
+          // return false
+          return null;
+      default:
+        return null;
+    }
   }
 
-  @Override String description(final InfixExpression n) {
-    final Expression e = ((MethodInvocation) n.getLeftOperand()).getExpression();
+  private static String descriptionAux(final Expression e) {
     return e == null ? "Use isEmpty()" : "Use " + e + ".isEmpty()";
   }
 
-  @Override ASTNode replacement(final InfixExpression e) {
-    if (!e.getAST().hasResolvedBindings())
-      return null;
-    final Operator o = e.getOperator();
-    if (!Is.isComparison(o))
-      return null;
+  @Override String description(final InfixExpression e) {
     final Expression right = right(e);
     final Expression left = left(e);
-    return invalidTypes(right, left) ? null //
-        : left instanceof MethodInvocation ? //
-            replacement(e, o, (MethodInvocation) left, (NumberLiteral) right) //
-            : replacement(e, conjugate(o), (MethodInvocation) right, (NumberLiteral) left)//
+    return left instanceof MethodInvocation ? descriptionAux(left) : descriptionAux(right);
+  }
+
+  @Override ASTNode replacement(final InfixExpression e) {
+    /* if (!e.getAST().hasResolvedBindings()) return null; */ // Yossi Told To
+                                                              // Remove That For
+                                                              // Tests
+    final Operator o = e.getOperator();
+    final Expression right = right(e);
+    final Expression left = left(e);
+    return left instanceof MethodInvocation ? //
+        replacement(e, o, (MethodInvocation) left, (NumberLiteral) right) //
+        : replacement(e, conjugate(o), (MethodInvocation) right, (NumberLiteral) left)//
     ;
+  }
+
+  @Override boolean scopeIncludes(final InfixExpression e) {
+    final Operator o = e.getOperator();
+    if (!Is.isComparison(o))
+      return false;
+    final Expression right = right(e);
+    final Expression left = left(e);
+    if (!validTypes(right, left)) {
+      return false;
+    }
+    return left instanceof MethodInvocation ? "size".equals(name((MethodInvocation) left).getIdentifier())
+        : "size".equals(name((MethodInvocation) right).getIdentifier());
   }
 }
