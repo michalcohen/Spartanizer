281ab029e4b13cab143caed9a0249d196c0e939e
Yossi Gil <yossi.gil@gmail.com>
2016-08-27 17:35:17 +0300

    new Environment

--- src/test/java/il/org/spartan/refactoring/utils/Environment.java
+++ src/test/java/il/org/spartan/refactoring/utils/Environment.java
@@ -1,101 +1,163 @@
 package il.org.spartan.refactoring.utils;
 
 import java.util.*;
+import java.util.Map.*;
 
 import org.eclipse.jdt.core.dom.*;
 
-/** Mumbo jumbo of stuff we will do later.Document it, but do not maintaing it
- * for now */
-class Entry {
-  /** The containing block, whose death marks the death of this entry; not sure,
-   * but I think this entry can be shared by many nodes at the same leve */
-  public final ASTNode blockScope;
-  /** The node at which this entry was created */
-  public final ASTNode self;
-  /** What do we know about the type of this definition */
-  public final Type type;
-  /** What do we know about an entry hidden by this one */
-  public final Entry hiding;
-
-  // For now, nothing is known, we only maintain lists
-  Entry() {
-    blockScope = self = null;
-    type = null;
-    hiding = null;
-  }
-}
-
+/** TODO: Document what is it for users? how should clients use this
+ * class/method, same for all TODO: Document */
 public interface Environment {
-  boolean isEmpty();
-
-  boolean containsKey(String key);
-
-  Entry get(String e);
-
-  Entry put(String key, Entry e);
-
-  Set<String> keySet();
-
-  default Environment newChild() {
-    return new Child(this);
-  }
-
   /** Document properly, this is the parent of all all */
   static final Environment EMPTY = new Environment() {
-    @Override public boolean isEmpty() {
-      return true;
-    }
-
-    @Override public boolean containsKey(String __) {
-      return false;
-    }
-
-    @Override public Entry get(String e) {
-      return null;
-    }
-
-    @Override public Entry put(String key, Entry e) {
-      throw new IllegalArgumentException(key + "/" + e);
-    }
-
-    @Override public Set<String> keySet() {
-      return null;
-    }
+    /* Empty */
   };
+  /** TODO: Document */
+  static final Set<Entry<String, Information>> emptyEntries = Collections.unmodifiableSet(new HashSet<>());
+  /** TODO: Document */
+  static final Set<String> emptySet = Collections.unmodifiableSet(new HashSet<>());
 
-  public static Environment newRoot() {
-    return EMPTY.newChild();
+  /** TODO: Document */
+  public static Environment genesis() {
+    return EMPTY.spawn();
+  }
+
+  default int size() {
+    return 0;
+  }
+
+  default Set<Entry<String, Information>> entries() {
+    return emptyEntries;
+  }
+
+  default int totalSize() {
+    return size() + (nest() == null ? 0 : nest().totalSize());
+  }
+
+  /** TODO: Document */
+  default boolean doesntHave(final String name) {
+    return !has(name);
+  }
+
+  /** TODO: Document */
+  default boolean empty() {
+    return true;
+  }
+
+  default Set<Entry<String, Information>> totalEntries() {
+    final Set<Entry<String, Information>> $ = new HashSet<>(entries());
+    if (nest() != null)
+      $.addAll(nest().totalEntries())
+    return $;
+  }
+
+  /** May return null! Document */
+  default Information get(final String name) {
+    return null;
+  }
+
+  /** TODO: Document */
+  default boolean has(final String name) {
+    return false;
+  }
+
+  /** May return null! Document when and document it from the client side */
+  default Information hiding(final String name) {
+    return nest().get(name);
+  }
+
+  /** upper level only. If you want the parent, go to the parent TODO:
+   * Document */
+  default Set<String> names() {
+    return emptySet;
+  }
+
+  /** Document */
+  default Set<String> totalNames() {
+    final Set<String> $ = new HashSet<>(names());
+    if (nest() != null)
+      $.addAll(nest().totalNames());
+    return $;
+  }
+
+  /** May return null! Document why */
+  default Environment nest() {
+    return null;
+  }
+
+  /** Should return the hidden entry, or null if no entry hidden by this one.
+   * Note: you will have to assume multiple definitions in the same block, this
+   * is a compilation error, but nevertheless, let a later entry with of a
+   * certain name to "hide" a former entry with the same name. */
+  default Information put(final String name, final Information i) {
+    throw new IllegalArgumentException(name + "/" + i);
+  }
+
+  default Environment spawn() {
+    return new Nested(this);
   }
 
   /** TODO: document propertly, but essentially is a dictionary with a parent.
    * Insertions go the current node, searches start at the current note and
    * deleegate to the parent unless it is null. */
-  class Child implements Environment {
-    public final Environment parent;
-    public final Map<String, Entry> inner = new LinkedHashMap<>();
+  final class Nested implements Environment {
+    public final Map<String, Information> flat = new LinkedHashMap<>();
+    public final Environment nest;
 
-    @Override public boolean isEmpty() {
-      return inner.isEmpty() || parent.isEmpty();
+    Nested(final Environment parent) {
+      nest = parent;
     }
 
-    @Override public boolean containsKey(String key) {
-      return inner.containsKey(key) || parent.containsKey(key);
+    @Override public boolean empty() {
+      return flat.isEmpty() || nest.empty();
     }
 
-    @Override public Entry get(String key) {
-      return inner.get(key);
+    public Set<Map.Entry<String, Information>> entries() {
+      return flat.entrySet();
     }
 
-    @Override public Entry put(String key, Entry value) {
-      return inner.put(key, value);
+    @Override public Information get(final String name) {
+      final Information $ = flat.get(name);
+      return $ != null ? $ : nest.get(name);
     }
 
-    @Override public Set<String> keySet() {
-      return inner.keySet();
+    @Override public boolean has(final String name) {
+      return flat.containsKey(name) || nest.has(name);
     }
 
-    Child(Environment parent) {
-      this.parent = parent;
+    @Override public Set<String> names() {
+      return flat.keySet();
     }
+
+    @Override public Environment nest() {
+      return nest;
+    }
+
+    @Override public Information put(final String name, final Information value) {
+      flat.put(name, value);
+      return hiding(name);
+    }
+  }
+}
+
+/** Mumbo jumbo of stuff we will do later. Document it, but do not maintaing it
+ * for now, this class is intentionally package level, and intenrationally
+ * defined locall. For now, cients should not be messing with it */
+class Information {
+  /** The containing block, whose death marks the death of this entry; not sure,
+   * but I think this entry can be shared by many nodes at the same leve */
+  public final ASTNode blockScope;
+  /** What do we know about an entry hidden by this one */
+  public final Information hiding;
+  /** The node at which this entry was created */
+  public final ASTNode self;
+  /** What do we know about the type of this definition */
+  public final Type type;
+
+  // For now, nothing is known, we only maintain lists
+  Information() {
+    blockScope = self = null;
+    type = null;
+    hiding = null;
   }
 }
