2fec88ca0c01736218486b7ab393a570614f81e9
dormaayn <dor.d.ma@gmail.com>
2016-08-28 10:33:36 +0300

    Evaluate the addition of Integers,updated (Issue #92)

---
+++ src/main/java/il/org/spartan/refactoring/wring/EvaluateAdditionInt.java
@@ -0,0 +1,47 @@
+package il.org.spartan.refactoring.wring;
+
+
+import static org.eclipse.jdt.core.dom.InfixExpression.Operator.*;
+
+import java.util.*;
+
+import org.eclipse.jdt.core.dom.*;
+
+import il.org.spartan.refactoring.utils.*;
+
+/**
+ * Evaluate the addition of integer numbers :
+ * <pre>
+ * 3+4+1
+ * </pre>
+ * to:
+ * <pre>
+ * 8
+ * </pre>
+ * @author Dor Ma'ayan 
+ * @since 2016
+ */
+public class EvaluateAdditionInt extends Wring.ReplaceCurrentNode<InfixExpression> implements Kind.NoImpact {
+
+  @Override public String description() {
+      return "Evaluate addition of int numbers";
+  }
+
+  @Override String description(@SuppressWarnings("unused") InfixExpression e) {
+    return "Evaluate addition of int numbers";
+  }
+
+  @Override ASTNode replacement(InfixExpression e) {
+    return e.getOperator() != PLUS ? null : replacement(extract.allOperands(e),e);
+  }
+  
+  private static ASTNode replacement(final List<Expression> es, InfixExpression e) {
+    int sum = 0;
+    for (final Expression ¢ : es){
+      if (!(¢ instanceof NumberLiteral))
+        return null;
+      sum=sum + Integer.parseInt(((NumberLiteral) ¢).getToken());
+    }  
+    return e.getAST().newNumberLiteral(Integer.toString(sum));
+  }
+}
