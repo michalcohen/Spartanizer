d89bd8c3f6f8bbfaed00b7f36a1669f67caa9df1
dormaayn <dor.d.ma@gmail.com>
2016-08-28 14:12:27 +0300

    Evaluation, now with division +tests (Issue #92)

---
+++ src/main/java/il/org/spartan/refactoring/wring/EvaluateDivisionInt.java
@@ -0,0 +1,72 @@
+package il.org.spartan.refactoring.wring;
+
+import static org.eclipse.jdt.core.dom.InfixExpression.Operator.*;
+
+import java.util.*;
+
+import org.eclipse.jdt.core.dom.*;
+
+import il.org.spartan.refactoring.utils.*;
+
+/**
+ * Evaluate the division of integer numbers --> the result is also an integer :
+ * <pre>
+ * 7/8
+ * </pre>
+ * to:
+ * <pre>
+ * 0
+ * </pre>
+ * @author Dor Ma'ayan 
+ * @since 2016
+ */
+public class EvaluateDivisionInt extends Wring.ReplaceCurrentNode<InfixExpression> implements Kind.NoImpact {
+
+  @Override public String description() {
+      return "Evaluate division of int numbers";
+  }
+
+  @Override String description(@SuppressWarnings("unused") InfixExpression e) {
+    return "Evaluate division of int numbers";
+  }
+
+  @Override ASTNode replacement(InfixExpression e) {
+    return e.getOperator() != DIVIDE ? null : replacement(extract.allOperands(e),e);
+  }
+  
+  private static int extractNumber(Expression e){
+    if(!(e instanceof PrefixExpression))
+      return  Integer.parseInt(((NumberLiteral) e).getToken());
+    return -1* Integer.parseInt(((NumberLiteral) ((PrefixExpression)e).getOperand()).getToken());
+  }
+  
+  private static boolean isInt(Expression e){
+    if(!(e instanceof NumberLiteral))
+      return false;
+    return ((NumberLiteral) e).getToken().matches("[0-9]+");
+  }
+
+  private static boolean isCompitable(Expression e){
+    return (!((e instanceof NumberLiteral &&
+        isInt(e) || (e instanceof PrefixExpression &&
+        ((PrefixExpression)e).getOperator()==PrefixExpression.Operator.MINUS &&
+        ((PrefixExpression)e).getOperand() instanceof NumberLiteral))));
+  }
+  
+  private static ASTNode replacement(final List<Expression> es, InfixExpression e) {
+    if(es.isEmpty())
+      return null;
+    if (isCompitable(es.get(0)))
+      return null;
+    int divide = extractNumber(es.get(0));
+    int index=0;
+    for (final Expression ¢ : es){
+      if (isCompitable(¢))
+        return null;
+      if(index!=0)
+        divide = divide /  extractNumber(¢);
+      index++;
+    }  
+    return e.getAST().newNumberLiteral(Integer.toString(divide));
+  }
+}
---
+++ src/main/java/il/org/spartan/refactoring/wring/EvaluateMultiplicationInt.java
@@ -0,0 +1,62 @@
+package il.org.spartan.refactoring.wring;
+
+import static org.eclipse.jdt.core.dom.InfixExpression.Operator.*;
+
+import java.util.*;
+
+import org.eclipse.jdt.core.dom.*;
+
+import il.org.spartan.refactoring.utils.*;
+
+/**
+ * Evaluate the multiplication of integer numbers :
+ * <pre>
+ * 3*4*2
+ * </pre>
+ * to:
+ * <pre>
+ * 24
+ * </pre>
+ * @author Dor Ma'ayan 
+ * @since 2016
+ */
+public class EvaluateMultiplicationInt extends Wring.ReplaceCurrentNode<InfixExpression> implements Kind.NoImpact {
+
+  @Override public String description() {
+      return "Evaluate multiplication of int numbers";
+  }
+
+  @Override String description(@SuppressWarnings("unused") InfixExpression e) {
+    return "Evaluate multiplication of int numbers";
+  }
+
+  @Override ASTNode replacement(InfixExpression e) {
+    return e.getOperator() != TIMES ? null : replacement(extract.allOperands(e),e);
+  }
+  
+  
+  private static int extractNumber(Expression e){
+    if(!(e instanceof PrefixExpression))
+      return  Integer.parseInt(((NumberLiteral) e).getToken());
+    return -1* Integer.parseInt(((NumberLiteral) ((PrefixExpression)e).getOperand()).getToken());
+  }
+  
+  private static boolean isInt(Expression e){
+    if(!(e instanceof NumberLiteral))
+      return false;
+    return ((NumberLiteral) e).getToken().matches("[0-9]+");
+  }
+  
+  private static ASTNode replacement(final List<Expression> es, InfixExpression e) {
+    int mul = 1;
+    for (final Expression ¢ : es){
+      if (!((¢ instanceof NumberLiteral &&
+          isInt(¢)) || (¢ instanceof PrefixExpression &&
+          ((PrefixExpression)¢).getOperator()==PrefixExpression.Operator.MINUS &&
+          ((PrefixExpression)¢).getOperand() instanceof NumberLiteral)))
+        return null;
+        mul = mul * extractNumber(¢);
+    }  
+    return e.getAST().newNumberLiteral(Integer.toString(mul));
+  }
+}
