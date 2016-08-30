a56c6a33b013b03925730495fbab779e51faf396
Yossi Gil <yossi.gil@gmail.com>
2016-08-30 20:46:53 +0300

    Issue#94: Could not prove that the type char exists. See this new file.

---
+++ src/main/java/il/org/spartan/refactoring/java/atomic.java
@@ -0,0 +1,184 @@
+package il.org.spartan.refactoring.java;
+
+/** Fluent API ... */
+public interface atomic {
+  static boolean isChar(@SuppressWarnings("unused") boolean __) {
+    return false;
+  }
+
+  static boolean isChar(@SuppressWarnings("unused") byte __) {
+    return false;
+  }
+
+  static boolean isChar(@SuppressWarnings("unused") char __) {
+    return true;
+  }
+
+  static boolean isChar(@SuppressWarnings("unused") double __) {
+    return false;
+  }
+
+  static boolean isChar(@SuppressWarnings("unused") float __) {
+    return false;
+  }
+
+  static boolean isChar(@SuppressWarnings("unused") int __) {
+    return false;
+  }
+
+  static boolean isChar(@SuppressWarnings("unused") long __) {
+    return false;
+  }
+
+  static boolean isChar(@SuppressWarnings("unused") Object __) {
+    return false;
+  }
+
+  static boolean isChar(@SuppressWarnings("unused") String __) {
+    return false;
+  }
+
+  static boolean isDouble(@SuppressWarnings("unused") boolean __) {
+    return false;
+  }
+
+  static boolean isDouble(@SuppressWarnings("unused") byte __) {
+    return false;
+  }
+
+  static boolean isDouble(@SuppressWarnings("unused") char __) {
+    return false;
+  }
+
+  static boolean isDouble(@SuppressWarnings("unused") double __) {
+    return true;
+  }
+
+  static boolean isDouble(@SuppressWarnings("unused") float __) {
+    return false;
+  }
+
+  static boolean isDouble(@SuppressWarnings("unused") int __) {
+    return false;
+  }
+
+  static boolean isDouble(@SuppressWarnings("unused") long __) {
+    return false;
+  }
+
+  static boolean isDouble(@SuppressWarnings("unused") Object __) {
+    return false;
+  }
+
+  static boolean isDouble(@SuppressWarnings("unused") String __) {
+    return false;
+  }
+
+  static boolean isFloat(@SuppressWarnings("unused") boolean __) {
+    return false;
+  }
+
+  static boolean isFloat(@SuppressWarnings("unused") byte __) {
+    return false;
+  }
+
+  static boolean isFloat(@SuppressWarnings("unused") char __) {
+    return false;
+  }
+
+  static boolean isFloat(@SuppressWarnings("unused") double __) {
+    return false;
+  }
+
+  static boolean isFloat(@SuppressWarnings("unused") float __) {
+    return true;
+  }
+
+  static boolean isFloat(@SuppressWarnings("unused") int __) {
+    return false;
+  }
+
+  static boolean isFloat(@SuppressWarnings("unused") long __) {
+    return false;
+  }
+
+  static boolean isFloat(@SuppressWarnings("unused") Object __) {
+    return false;
+  }
+
+  static boolean isFloat(@SuppressWarnings("unused") String __) {
+    return false;
+  }
+
+  static boolean isInt(@SuppressWarnings("unused") boolean __) {
+    return false;
+  }
+
+  static boolean isInt(@SuppressWarnings("unused") byte __) {
+    return false;
+  }
+
+  static boolean isInt(@SuppressWarnings("unused") char __) {
+    return false;
+  }
+
+  static boolean isInt(@SuppressWarnings("unused") double __) {
+    return false;
+  }
+
+  static boolean isInt(@SuppressWarnings("unused") float __) {
+    return false;
+  }
+
+  static boolean isInt(@SuppressWarnings("unused") int __) {
+    return true;
+  }
+
+  static boolean isInt(@SuppressWarnings("unused") long __) {
+    return false;
+  }
+
+  static boolean isInt(@SuppressWarnings("unused") Object __) {
+    return false;
+  }
+
+  static boolean isInt(@SuppressWarnings("unused") String __) {
+    return false;
+  }
+
+  static boolean isLong(@SuppressWarnings("unused") boolean __) {
+    return false;
+  }
+
+  static boolean isLong(@SuppressWarnings("unused") byte __) {
+    return false;
+  }
+
+  static boolean isLong(@SuppressWarnings("unused") char __) {
+    return false;
+  }
+
+  static boolean isLong(@SuppressWarnings("unused") double __) {
+    return false;
+  }
+
+  static boolean isLong(@SuppressWarnings("unused") float __) {
+    return false;
+  }
+
+  static boolean isLong(@SuppressWarnings("unused") int __) {
+    return true;
+  }
+
+  static boolean isLong(@SuppressWarnings("unused") long __) {
+    return true;
+  }
+
+  static boolean isLong(@SuppressWarnings("unused") Object __) {
+    return false;
+  }
+
+  static boolean isLong(@SuppressWarnings("unused") String __) {
+    return true;
+  }
+}
---
+++ src/test/java/il/org/spartan/refactoring/java/AtomicConversionsTest.java
@@ -0,0 +1,31 @@
+package il.org.spartan.refactoring.java;
+
+import org.junit.*;
+import org.junit.runners.*;
+
+import il.org.spartan.*;
+
+@FixMethodOrder(MethodSorters.NAME_ASCENDING) //
+@SuppressWarnings({ "javadoc", "static-method" }) //
+public class AtomicConversionsTest {
+  @Test public void floatAndFloat() {
+    azzert.nay(atomic.isDouble(1L));
+    azzert.aye(atomic.isLong(1L));
+    azzert.aye(atomic.isFloat(2F));
+    float f = 3.1F;
+    assert f != 0;
+    azzert.aye(atomic.isFloat(2F + f));
+    azzert.aye(atomic.isFloat(1L + 2F));
+  }
+  @Test public void charAndChar() {
+    char c = 'c';
+    assert c != 0;
+    azzert.nay(atomic.isDouble(c));
+    azzert.nay(atomic.isLong(c));
+    azzert.nay(atomic.isFloat(c));
+    azzert.nay(atomic.isInt(c));
+    azzert.aye(atomic.isChar(c));
+    azzert.nay(atomic.isChar(c + 'b'));
+    azzert.nay(atomic.isFloat(c + "a"));
+  }
+}
