12af3a0591f44c07131f355dec582f96a76872fd
Phantom1472 <stav1472@gmail.com>
2016-09-02 17:36:30 +0300

    fixed bugs in ToStringToEmptyStringAddition
    and added tests to check for them

--- src/main/java/il/org/spartan/refactoring/wring/ToStringToEmptyStringAddition.java
+++ src/main/java/il/org/spartan/refactoring/wring/ToStringToEmptyStringAddition.java
@@ -4,16 +4,20 @@ import org.eclipse.jdt.core.dom.*;
 
 import il.org.spartan.refactoring.assemble.*;
 import il.org.spartan.refactoring.ast.*;
+import static il.org.spartan.refactoring.assemble.make.parethesized;
 
 /** Transforms x.toString() to "" + x
  * @author Stav Namir
  * @since 2016-8-31 */
 public final class ToStringToEmptyStringAddition extends Wring.ReplaceCurrentNode<MethodInvocation> implements Kind.Canonicalization {
   @Override ASTNode replacement(final MethodInvocation i) {
-    if (!"toString".equals(step.name(i).getIdentifier()))
+    if (!"toString".equals(step.name(i).getIdentifier()) || !i.arguments().isEmpty())
       return null;
     final Expression receiver = step.receiver(i);
-    return receiver == null ? null : subject.pair(i.getAST().newStringLiteral(), receiver).to(InfixExpression.Operator.PLUS);
+    return receiver == null ? null
+        : i.getParent() instanceof MethodInvocation
+            ? parethesized(subject.pair(i.getAST().newStringLiteral(), receiver).to(InfixExpression.Operator.PLUS))
+            : subject.pair(i.getAST().newStringLiteral(), receiver).to(InfixExpression.Operator.PLUS);
   }
 
   @Override String description(final MethodInvocation i) {
