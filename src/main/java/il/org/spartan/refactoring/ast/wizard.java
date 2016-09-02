7aa2f3563909b683ea7fc392ce8a9616c43202b0
alexkopzon <alexkopzon@192.168.1.10>
2016-09-02 13:40:23 +0300

    unsigned right shift is non associative

--- src/main/java/il/org/spartan/refactoring/ast/wizard.java
+++ src/main/java/il/org/spartan/refactoring/ast/wizard.java
@@ -227,7 +227,7 @@ public interface wizard {
   }
 
   static boolean nonAssociative(final InfixExpression e) {
-    return e != null && in(e.getOperator(), MINUS, DIVIDE, REMAINDER, LEFT_SHIFT, RIGHT_SHIFT_SIGNED );
+    return e != null && in(e.getOperator(), MINUS, DIVIDE, REMAINDER, LEFT_SHIFT, RIGHT_SHIFT_SIGNED, RIGHT_SHIFT_UNSIGNED );
   }
 
   static ASTParser parser(final int kind) {
