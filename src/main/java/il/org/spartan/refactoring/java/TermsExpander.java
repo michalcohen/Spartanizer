6c7806d1eb546f605762f5864462b4faad2c319f
Yossi Gil <yossi.gil@gmail.com>
2016-09-04 00:09:04 +0300

    Make $ the last parameter

--- src/main/java/il/org/spartan/refactoring/java/TermsExpander.java
+++ src/main/java/il/org/spartan/refactoring/java/TermsExpander.java
@@ -1,4 +1,6 @@
 package il.org.spartan.refactoring.java;
+import static il.org.spartan.refactoring.ast.wizard.*;
+import static il.org.spartan.refactoring.utils.lisp.*;
 
 import java.util.*;
 
@@ -6,7 +8,6 @@ import org.eclipse.jdt.core.dom.*;
 import org.eclipse.jdt.core.dom.InfixExpression.*;
 
 import il.org.spartan.refactoring.assemble.*;
-import il.org.spartan.refactoring.ast.*;
 import il.org.spartan.refactoring.utils.*;
 
 /** Expands terms of +/- expressions without reordering.
@@ -19,58 +20,58 @@ public class TermsExpander {
     return base(new TermsCollector(x));
   }
 
-  /** @see #recurse(InfixExpression, List) */
-  private static InfixExpression appendMinus(final InfixExpression $, final Term ¢) {
-    return ¢.negative() ? subject.append($, ¢.expression) : subject.pair($, ¢.expression).to(wizard.PLUS2);
+  /** @see #recurse(List, InfixExpression) */
+  private static InfixExpression appendMinus(final Term ¢, final InfixExpression $) {
+    return ¢.negative() ? subject.append($, ¢.expression) : subject.pair($, ¢.expression).to(PLUS2);
   }
 
-  /** @see #recurse(InfixExpression, List) */
-  private static InfixExpression appendPlus(final InfixExpression $, final Term t) {
+  /** @see #recurse(List, InfixExpression) */
+  private static InfixExpression appendPlus(final Term t, final InfixExpression $) {
     final Expression ¢ = duplicate.of(t.expression);
-    return t.positive() ? subject.append($, ¢) : subject.pair($, ¢).to(wizard.MINUS2);
+    return t.positive() ? subject.append($, ¢) : subject.pair($, ¢).to(MINUS2);
   }
 
   private static Expression base(final List<Term> ts) {
     assert ts != null;
     assert !ts.isEmpty();
-    final Term first = lisp.first(ts);
+    final Term first = first(ts);
     assert first != null;
-    final Term second = lisp.second(ts);
+    final Term second = second(ts);
     assert second != null;
     final Expression $ = base(first, second);
     assert $ != null;
-    return step($, lisp.chop(lisp.chop(ts)));
+    return step(chop(chop(ts)), $);
   }
 
   private static InfixExpression base(final Term t1, final Term t2) {
     if (t1.positive())
-      return subject.pair(t1.expression, t2.expression).to(t2.positive() ? wizard.PLUS2 : wizard.MINUS2);
+      return subject.pair(t1.expression, t2.expression).to(t2.positive() ? PLUS2 : MINUS2);
     assert t1.negative();
     return (//
     t2.positive() ? subject.pair(t2.expression, t1.expression) : //
-        subject.pair(subject.operand(t1.expression).to(wizard.MINUS1), t2.expression)//
-    ).to(wizard.MINUS2);
+        subject.pair(subject.operand(t1.expression).to(MINUS1), t2.expression)//
+    ).to(MINUS2);
   }
 
   private static Expression base(final TermsCollector c) {
     return base(c.all());
   }
 
-  /** @param $ The accumulator, to which one more {@link Term} should be added
+  /** @param ts a list
+   * @param $ The accumulator, to which one more {@link Term} should be added
    *        optimally
-   * @param ts a list
    * @return the $ parameter, after all elements of the list parameter are added
    *         to it */
-  private static Expression recurse(final Expression $, final List<Term> ts) {
+  private static Expression recurse(final List<Term> ts, final Expression $) {
     assert $ != null;
     if (ts == null || ts.isEmpty())
       return $;
     assert $ instanceof InfixExpression;
-    return recurse((InfixExpression) $, ts);
+    return recurse(ts, (InfixExpression) $);
   }
 
-  /** @see #recurse(InfixExpression, List) */
-  private static Expression recurse(final InfixExpression $, final List<Term> ts) {
+  /** @see #recurse(List, InfixExpression) */
+  private static Expression recurse(final List<Term> ts, final InfixExpression $) {
     assert $ != null;
     if (ts == null || ts.isEmpty())
       return $;
@@ -78,14 +79,14 @@ public class TermsExpander {
     assert !ts.isEmpty();
     final Operator o = $.getOperator();
     assert o != null;
-    assert o == wizard.PLUS2 || o == wizard.MINUS2;
-    final Term first = lisp.first(ts);
+    assert o == PLUS2 || o == MINUS2;
+    final Term first = first(ts);
     assert first != null;
-    return recurse(o == wizard.PLUS2 ? appendPlus($, first) : appendMinus($, first), lisp.chop(ts));
+    return recurse(chop(ts), o == PLUS2 ? appendPlus(first, $) : appendMinus(first, $));
   }
 
-  private static Expression step(final Expression $, final List<Term> ¢) {
+  private static Expression step(final List<Term> ¢, final Expression $) {
     assert ¢ != null;
-    return ¢.isEmpty() ? $ : recurse($, ¢);
+    return ¢.isEmpty() ? $ : recurse(¢, $);
   }
 }
