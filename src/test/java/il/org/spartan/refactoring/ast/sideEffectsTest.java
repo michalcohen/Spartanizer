80e8fe557466610eb313e4fb332a9fce79c81d33
Yossi Gil <yossi.gil@gmail.com>
2016-09-03 21:52:31 +0300

    add tests

--- src/test/java/il/org/spartan/refactoring/ast/sideEffectsTest.java
+++ src/test/java/il/org/spartan/refactoring/ast/sideEffectsTest.java
@@ -12,19 +12,35 @@ import il.org.spartan.refactoring.java.*;
 @SuppressWarnings({ "static-method", "javadoc" }) //
 @FixMethodOrder(MethodSorters.NAME_ASCENDING) //
 public class sideEffectsTest {
+  @Test public void freeFunctionCall() {
+    azzert.nay(sideEffects.free(e("f()")));
+  }
+
+  @Test public void freeFunctionCalla() {
+    azzert.nay(sideEffects.free(e("i =f()")));
+  }
+
+  @Test public void seriesA01() {
+    azzert.aye(sideEffects.free(e("a")));
+  }
+
+  @Test public void seriesA02() {
+    azzert.aye(sideEffects.free(e("this.a")));
+  }
+
   @Test public void deterministicArray1() {
-    azzert.that(sideEffects.deterministic(e("new a[3]")), is(false));
+    azzert.nay(sideEffects.deterministic(e("new a[3]")));
   }
 
   @Test public void deterministicArray2() {
-    azzert.that(sideEffects.deterministic(e("new int[] {12,13}")), is(false));
+    azzert.nay(sideEffects.deterministic(e("new int[] {12,13}")));
   }
 
   @Test public void deterministicArray3() {
-    azzert.that(sideEffects.deterministic(e("new int[] {12,13, i++}")), is(false));
+    azzert.nay(sideEffects.deterministic(e("new int[] {12,13, i++}")));
   }
 
   @Test public void deterministicArray4() {
-    azzert.that(sideEffects.deterministic(e("new int[f()]")), is(false));
+    azzert.nay(sideEffects.deterministic(e("new int[f()]")));
   }
 }
