93ab307c41ae28abc60849d107f89d8181cf0848
Yossi Gil <yossi.gil@gmail.com>
2016-09-04 20:29:56 +0300

    Fewer warnings

--- src/main/java/il/org/spartan/refactoring/java/Modifiers.java
+++ src/main/java/il/org/spartan/refactoring/java/Modifiers.java
@@ -11,7 +11,7 @@ public enum Modifiers {
   DEFAULT, //
   STATIC, //
   FINAL, //
-  TRANSIENT, // TODO: Yossi, MLU :)
+  TRANSIENT, //
   VOLATILE, //
   SYNCHRONIZED, //
   NATIVE, //
--- src/main/java/il/org/spartan/refactoring/java/atomic.java
+++ src/main/java/il/org/spartan/refactoring/java/atomic.java
@@ -1,336 +1,339 @@
 package il.org.spartan.refactoring.java;
 
-/** Fluent API ... */
+/** Use overloading to determine the type of an expression statically.
+ * @author Yossi Gil
+ * @since 2016 */
+@SuppressWarnings("unused") //
 public interface atomic {
-  static boolean isBoolean(@SuppressWarnings("unused") final boolean __) {
+  static boolean isBoolean(final boolean __) {
     return true;
   }
 
-  static boolean isBoolean(@SuppressWarnings("unused") final byte __) {
+  static boolean isBoolean(final byte __) {
     return false;
   }
 
-  static boolean isBoolean(@SuppressWarnings("unused") final char __) {
+  static boolean isBoolean(final char __) {
     return false;
   }
 
-  static boolean isBoolean(@SuppressWarnings("unused") final double __) {
+  static boolean isBoolean(final double __) {
     return false;
   }
 
-  static boolean isBoolean(@SuppressWarnings("unused") final float __) {
+  static boolean isBoolean(final float __) {
     return false;
   }
 
-  static boolean isBoolean(@SuppressWarnings("unused") final Object __) {
+  static boolean isBoolean(final Object __) {
     return false;
   }
 
-  static boolean isBoolean(@SuppressWarnings("unused") final short __) {
+  static boolean isBoolean(final short __) {
     return false;
   }
 
-  static boolean isBoolean(@SuppressWarnings("unused") final String __) {
+  static boolean isBoolean(final String __) {
     return false;
   }
 
-  static boolean isByte(@SuppressWarnings("unused") final boolean __) {
+  static boolean isByte(final boolean __) {
     return false;
   }
 
-  static boolean isByte(@SuppressWarnings("unused") final byte __) {
+  static boolean isByte(final byte __) {
     return true;
   }
 
-  static boolean isByte(@SuppressWarnings("unused") final char __) {
+  static boolean isByte(final char __) {
     return false;
   }
 
-  static boolean isByte(@SuppressWarnings("unused") final double __) {
+  static boolean isByte(final double __) {
     return false;
   }
 
-  static boolean isByte(@SuppressWarnings("unused") final float __) {
+  static boolean isByte(final float __) {
     return false;
   }
 
-  static boolean isByte(@SuppressWarnings("unused") final int __) {
+  static boolean isByte(final int __) {
     return false;
   }
 
-  static boolean isByte(@SuppressWarnings("unused") final long __) {
+  static boolean isByte(final long __) {
     return false;
   }
 
-  static boolean isByte(@SuppressWarnings("unused") final Object __) {
+  static boolean isByte(final Object __) {
     return false;
   }
 
-  static boolean isByte(@SuppressWarnings("unused") final short __) {
+  static boolean isByte(final short __) {
     return false;
   }
 
-  static boolean isByte(@SuppressWarnings("unused") final String __) {
+  static boolean isByte(final String __) {
     return false;
   }
 
-  static boolean isChar(@SuppressWarnings("unused") final byte __) {
+  static boolean isChar(final byte __) {
     return false;
   }
 
-  static boolean isChar(@SuppressWarnings("unused") final char __) {
+  static boolean isChar(final char __) {
     return true;
   }
 
-  static boolean isChar(@SuppressWarnings("unused") final double __) {
+  static boolean isChar(final double __) {
     return false;
   }
 
-  static boolean isChar(@SuppressWarnings("unused") final float __) {
+  static boolean isChar(final float __) {
     return false;
   }
 
-  static boolean isChar(@SuppressWarnings("unused") final int __) {
+  static boolean isChar(final int __) {
     return false;
   }
 
-  static boolean isChar(@SuppressWarnings("unused") final long __) {
+  static boolean isChar(final long __) {
     return false;
   }
 
-  static boolean isChar(@SuppressWarnings("unused") final Object __) {
+  static boolean isChar(final Object __) {
     return false;
   }
 
-  static boolean isChar(@SuppressWarnings("unused") final short __) {
+  static boolean isChar(final short __) {
     return false;
   }
 
-  static boolean isChar(@SuppressWarnings("unused") final String __) {
+  static boolean isChar(final String __) {
     return false;
   }
 
-  static boolean isDouble(@SuppressWarnings("unused") final boolean __) {
+  static boolean isDouble(final boolean __) {
     return false;
   }
 
-  static boolean isDouble(@SuppressWarnings("unused") final byte __) {
+  static boolean isDouble(final byte __) {
     return false;
   }
 
-  static boolean isDouble(@SuppressWarnings("unused") final char __) {
+  static boolean isDouble(final char __) {
     return false;
   }
 
-  static boolean isDouble(@SuppressWarnings("unused") final double __) {
+  static boolean isDouble(final double __) {
     return true;
   }
 
-  static boolean isDouble(@SuppressWarnings("unused") final float __) {
+  static boolean isDouble(final float __) {
     return false;
   }
 
-  static boolean isDouble(@SuppressWarnings("unused") final int __) {
+  static boolean isDouble(final int __) {
     return false;
   }
 
-  static boolean isDouble(@SuppressWarnings("unused") final long __) {
+  static boolean isDouble(final long __) {
     return false;
   }
 
-  static boolean isDouble(@SuppressWarnings("unused") final Object __) {
+  static boolean isDouble(final Object __) {
     return false;
   }
 
-  static boolean isDouble(@SuppressWarnings("unused") final short __) {
+  static boolean isDouble(final short __) {
     return false;
   }
 
-  static boolean isDouble(@SuppressWarnings("unused") final String __) {
+  static boolean isDouble(final String __) {
     return false;
   }
 
-  static boolean isFloat(@SuppressWarnings("unused") final boolean __) {
+  static boolean isFloat(final boolean __) {
     return false;
   }
 
-  static boolean isFloat(@SuppressWarnings("unused") final byte __) {
+  static boolean isFloat(final byte __) {
     return false;
   }
 
-  static boolean isFloat(@SuppressWarnings("unused") final char __) {
+  static boolean isFloat(final char __) {
     return false;
   }
 
-  static boolean isFloat(@SuppressWarnings("unused") final double __) {
+  static boolean isFloat(final double __) {
     return false;
   }
 
-  static boolean isFloat(@SuppressWarnings("unused") final float __) {
+  static boolean isFloat(final float __) {
     return true;
   }
 
-  static boolean isFloat(@SuppressWarnings("unused") final int __) {
+  static boolean isFloat(final int __) {
     return false;
   }
 
-  static boolean isFloat(@SuppressWarnings("unused") final long __) {
+  static boolean isFloat(final long __) {
     return false;
   }
 
-  static boolean isFloat(@SuppressWarnings("unused") final Object __) {
+  static boolean isFloat(final Object __) {
     return false;
   }
 
-  static boolean isFloat(@SuppressWarnings("unused") final short __) {
+  static boolean isFloat(final short __) {
     return false;
   }
 
-  static boolean isFloat(@SuppressWarnings("unused") final String __) {
+  static boolean isFloat(final String __) {
     return false;
   }
 
-  static boolean isInt(@SuppressWarnings("unused") final boolean __) {
+  static boolean isInt(final boolean __) {
     return false;
   }
 
-  static boolean isInt(@SuppressWarnings("unused") final byte __) {
+  static boolean isInt(final byte __) {
     return false;
   }
 
-  static boolean isInt(@SuppressWarnings("unused") final char __) {
+  static boolean isInt(final char __) {
     return false;
   }
 
-  static boolean isInt(@SuppressWarnings("unused") final double __) {
+  static boolean isInt(final double __) {
     return false;
   }
 
-  static boolean isInt(@SuppressWarnings("unused") final float __) {
+  static boolean isInt(final float __) {
     return false;
   }
 
-  static boolean isInt(@SuppressWarnings("unused") final int __) {
+  static boolean isInt(final int __) {
     return true;
   }
 
-  static boolean isInt(@SuppressWarnings("unused") final long __) {
+  static boolean isInt(final long __) {
     return false;
   }
 
-  static boolean isInt(@SuppressWarnings("unused") final Object __) {
+  static boolean isInt(final Object __) {
     return false;
   }
 
-  static boolean isInt(@SuppressWarnings("unused") final short __) {
+  static boolean isInt(final short __) {
     return false;
   }
 
-  static boolean isInt(@SuppressWarnings("unused") final String __) {
+  static boolean isInt(final String __) {
     return false;
   }
 
-  static boolean isLong(@SuppressWarnings("unused") final boolean __) {
+  static boolean isLong(final boolean __) {
     return false;
   }
 
-  static boolean isLong(@SuppressWarnings("unused") final byte __) {
+  static boolean isLong(final byte __) {
     return false;
   }
 
-  static boolean isLong(@SuppressWarnings("unused") final char __) {
+  static boolean isLong(final char __) {
     return false;
   }
 
-  static boolean isLong(@SuppressWarnings("unused") final double __) {
+  static boolean isLong(final double __) {
     return false;
   }
 
-  static boolean isLong(@SuppressWarnings("unused") final float __) {
+  static boolean isLong(final float __) {
     return false;
   }
 
-  static boolean isLong(@SuppressWarnings("unused") final int __) {
+  static boolean isLong(final int __) {
     return true;
   }
 
-  static boolean isLong(@SuppressWarnings("unused") final long __) {
+  static boolean isLong(final long __) {
     return true;
   }
 
-  static boolean isLong(@SuppressWarnings("unused") final Object __) {
+  static boolean isLong(final Object __) {
     return false;
   }
 
-  static boolean isLong(@SuppressWarnings("unused") final short __) {
+  static boolean isLong(final short __) {
     return false;
   }
 
-  static boolean isLong(@SuppressWarnings("unused") final String __) {
+  static boolean isLong(final String __) {
     return true;
   }
 
-  static boolean isObject(@SuppressWarnings("unused") final short __) {
+  static boolean isObject(final short __) {
     return false;
   }
 
-  static boolean isShort(@SuppressWarnings("unused") final boolean __) {
+  static boolean isShort(final boolean __) {
     return false;
   }
 
-  static boolean isShort(@SuppressWarnings("unused") final byte __) {
+  static boolean isShort(final byte __) {
     return true;
   }
 
-  static boolean isShort(@SuppressWarnings("unused") final char __) {
+  static boolean isShort(final char __) {
     return false;
   }
 
-  static boolean isShort(@SuppressWarnings("unused") final double __) {
+  static boolean isShort(final double __) {
     return false;
   }
 
-  static boolean isShort(@SuppressWarnings("unused") final float __) {
+  static boolean isShort(final float __) {
     return false;
   }
 
-  static boolean isShort(@SuppressWarnings("unused") final int __) {
+  static boolean isShort(final int __) {
     return false;
   }
 
-  static boolean isShort(@SuppressWarnings("unused") final long __) {
+  static boolean isShort(final long __) {
     return false;
   }
 
-  static boolean isShort(@SuppressWarnings("unused") final Object __) {
+  static boolean isShort(final Object __) {
     return false;
   }
 
-  static boolean isShort(@SuppressWarnings("unused") final short __) {
+  static boolean isShort(final short __) {
     return true;
   }
 
-  static boolean isShort(@SuppressWarnings("unused") final String __) {
+  static boolean isShort(final String __) {
     return false;
   }
 
-  static boolean isString(@SuppressWarnings("unused") final double __) {
+  static boolean isString(final double __) {
     return false;
   }
 
-  static boolean isString(@SuppressWarnings("unused") final float __) {
+  static boolean isString(final float __) {
     return false;
   }
 
-  static boolean isString(@SuppressWarnings("unused") final Object __) {
+  static boolean isString(final Object __) {
     return false;
   }
 
-  static boolean isString(@SuppressWarnings("unused") final short __) {
+  static boolean isString(final short __) {
     return false;
   }
 
-  static boolean isString(@SuppressWarnings("unused") final String __) {
+  static boolean isString(final String __) {
     return true;
   }
 }
