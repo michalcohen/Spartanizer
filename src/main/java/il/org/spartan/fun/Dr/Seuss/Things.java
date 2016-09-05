775133daa384c581e40b88bcb03c4988ea7bd316
Yossi Gil <yossi.gil@gmail.com>
2016-09-05 05:21:19 +0300

    enough fun now

--- src/main/java/il/org/spartan/fun/Dr/Seuss/Things.java
+++ src/main/java/il/org/spartan/fun/Dr/Seuss/Things.java
@@ -1,12 +1,17 @@
 package il.org.spartan.fun.Dr.Seuss;
 
+import il.org.spartan.fun.Dr.Seuss.Cat.in.the.Hat.*;
+
 interface Cat {
   interface in {
     interface the {
       interface Hat {
-        static final Thing thing1 = new Thing(){};
-        static final Thing thing2 = new Thing(){};
+        static final Thing thing1 = new Thing() {
+        };
+        static final Thing thing2 = new Thing() {
+        };
         static final Thing[] things = { thing1, thing2 };
+
         interface Thing {
           /* Empty on purpose */
         }
@@ -16,5 +21,24 @@ interface Cat {
 }
 
 public class Things {
-  
+  public static void main(final String[] args) {
+    nPattern1();
+    nPattern2();
+  }
+
+  private static void nPattern1() {
+    int i = 0;
+    for (final Thing t : il.org.spartan.fun.Dr.Seuss.Cat.in.the.Hat.things)
+      doSomethingWith(i++, t);
+  }
+
+  private static void nPattern2() {
+    int i = 0;
+    for (final Thing t : il.org.spartan.fun.Dr.Seuss.Cat.in.the.Hat.things)
+      doSomethingWith(i++, t);
+  }
+
+  private static void doSomethingWith(final Object... os) {
+    // Here is where we do the main job
+  }
 }
