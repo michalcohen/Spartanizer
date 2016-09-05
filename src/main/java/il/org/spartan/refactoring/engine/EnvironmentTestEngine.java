5431715559e82fdd3c9154ebe71cc4fccc9be6b2
alexkopzon <alexkopzon@192.168.1.10>
2016-09-05 22:52:34 +0300

    formatting

--- src/main/java/il/org/spartan/refactoring/engine/EnvironmentTestEngine.java
+++ src/main/java/il/org/spartan/refactoring/engine/EnvironmentTestEngine.java
@@ -13,6 +13,10 @@ import il.org.spartan.refactoring.java.Environment.*;
 import il.org.spartan.refactoring.utils.*;;
 
 public class EnvironmentTestEngine {
+  enum AnnotationType {
+    FLAT, NESTED, BEGINEND
+  }
+
   static Set<Entry<String, Environment.Information>> generateSet() {
     return Collections.unmodifiableSet(new HashSet<>());
   }
@@ -36,13 +40,6 @@ public class EnvironmentTestEngine {
     return $;
   }
 
-  static Set<Entry<String, Environment.Information>> listToSet(final List<Annotation> as) {
-    final Set<Entry<String, Environment.Information>> $ = Collections.unmodifiableSet(new HashSet<>());
-    for (final Annotation ¢ : as) {
-    }
-    return $;
-  }
-
   private static IJavaProject getJavaProject(final String projectName) {
     final IProject project = ResourcesPlugin.getWorkspace().getRoot().getProject(projectName);
     if (project == null)
@@ -51,8 +48,14 @@ public class EnvironmentTestEngine {
     return $ != null && $.exists() ? $ : null;
   }
 
+  static Set<Entry<String, Environment.Information>> listToSet(final List<Annotation> as) {
+    final Set<Entry<String, Environment.Information>> $ = Collections.unmodifiableSet(new HashSet<>());
+    for (final Annotation ¢ : as) {
+    }
+    return $;
+  }
+
   private final ASTNode cUnit;
-  
   Set<Entry<String, Environment.Information>> testNestedENV;
   Set<Entry<String, Environment.Information>> testFlatENV;
   Set<Entry<String, Environment.Information>> testBeginEnd;
@@ -71,110 +74,31 @@ public class EnvironmentTestEngine {
     testBeginEnd = generateSet();
   }
 
-  enum AnnotationType {
-    FLAT, NESTED, BEGINEND
+  private void addValueToBeginEnd(final List<MemberValuePair> ps) {
   }
-  
-  private void addValueToBeginEnd(List<MemberValuePair> ps) {}
-  
-  private void addValueToNested(List<MemberValuePair> ps) {}
-  //TODO: Information should be instantiated with Type
-  private void addValueToFlat(List<MemberValuePair> ps) {
+
+  // TODO: Information should be instantiated with Type
+  private void addValueToFlat(final List<MemberValuePair> ps) {
     testFlatENV.add(new MyEntry<>(wizard.asString(ps.get(0).getValue()), new Information()));
   }
-  
-  void addValueToSetsDispatch(List<MemberValuePair> ps, AnnotationType t){
-    switch (t){
-      case FLAT : addValueToFlat(ps); break;
-      case NESTED : addValueToNested(ps); break;
-      case BEGINEND : addValueToBeginEnd(ps); break;
+
+  private void addValueToNested(final List<MemberValuePair> ps) {
+  }
+
+  void addValueToSetsDispatch(final List<MemberValuePair> ps, final AnnotationType t) {
+    switch (t) {
+      case FLAT:
+        addValueToFlat(ps);
+        break;
+      case NESTED:
+        addValueToNested(ps);
+        break;
+      case BEGINEND:
+        addValueToBeginEnd(ps);
+        break;
       default:
         break;
     }
-  }
-  
-  /*
-   * define: outer annotation = OutOfOrderNestedENV, InOrderFlatENV, Begin, End.
-   * define: inner annotation = Id.
-   * ASTVisitor that goes over the ASTNodes in which annotations can be defined, and checks if the annotations are
-   * of the kind that interests us. 
-   * An array of inner annotations is defined inside of each outer annotation of interest. 
-   * I think it will be less error prone and more scalable to implement another, 
-   * internal, ASTVisitor that will go over each inner annotation node, 
-   * and send everything to an outside function to add to the Sets as required. 
-   * That means that each inner annotation will be visited twice from the same outer annotation,
-   * but that should not cause worry,
-   * since the outside visitor will do nothing.
-   * 
-   *  TODO: internal node parsing. Think about Nested parsing.
-   */
-  public void runTest() {
-    cUnit.accept(new ASTVisitor() {
-      @Override public boolean visit(final MethodDeclaration d) {
-        /* Set<Entry<String, Information>> useCheckSet = Environment.uses(d);
-         * Set<Entry<String, Information>> declareCheckSet =
-         * Environment.declares(d); */
-        addAnnotations(extract.annotations(d));
-        return true;
-      }
-
-      void addAnnotations(final List<Annotation> as) {
-        for (final Annotation ¢ : as)
-          dispatch(¢);
-      }
-
-      void dispatch(final Annotation a) {
-        if (a.getTypeName() + "" == "nestedENV")
-          nestedHandler(a);
-        else if (a.getTypeName() + "" == "flatENV")
-          flatHandler(a);
-        else if (a.getTypeName() + "" == "BegingEndENV")
-          beginEndHandler(a);
-      }
-
-      void flatHandler(final Annotation ¢) {
-        flatHandler(az.singleMemberAnnotation(¢));
-      }
-
-      void flatHandler(final SingleMemberAnnotation $) {
-        if ($ == null)
-          return;
-        $.accept(new ASTVisitor() {
-          @SuppressWarnings("unchecked") 
-          List<MemberValuePair> values(NormalAnnotation $) {
-            return $.values();
-          }
-          /*
-          @Override public boolean visit(NormalAnnotation ¢){
-            if (isNameId(¢.getTypeName())) {
-              
-              testFlatENV.addAll();
-            }
-            return true;
-          }
-*/
-          private boolean isNameId(Name ¢){
-            return "Id".equals(wizard.asString(¢));
-          }
-        });
-      }
-
-      private void nestedHandler(final Annotation a) {
-      }
-
-      private void beginEndHandler(final Annotation a) {
-      }
-    });
-  }
-
-  /** Compares flat output Set (flat) with provided Set, that will be the result
-   * of the flat version of defines.
-   * @param $ */
-  void compareFlatOutOfOrder(final Set<Entry<String, Information>> $) {
-    // Check that each member of $ is contained in FlatENV, and that the size is
-    // equal.
-    // azzert.fail Otherwise.
-    return;
   }
 
   /** Compares output Set (testFlatENV) with provided set, that will be the
@@ -185,6 +109,16 @@ public class EnvironmentTestEngine {
     // equal.
     // Also, check size, to avoid the case Set A is contained in B.
     // azzert.fail Otherwise.
+  }
+
+  /** Compares flat output Set (flat) with provided Set, that will be the result
+   * of the flat version of defines.
+   * @param $ */
+  void compareFlatOutOfOrder(final Set<Entry<String, Information>> $) {
+    // Check that each member of $ is contained in FlatENV, and that the size is
+    // equal.
+    // azzert.fail Otherwise.
+    return;
   }
 
   /** Compares output Set (testNestedENV) with provided Set, that will be the
@@ -205,6 +139,61 @@ public class EnvironmentTestEngine {
     // equal.
     // Also, check size, to avoid the case Set A is contained in B.
     // azzert.fail Otherwise.
+  }
+
+  /* define: outer annotation = OutOfOrderNestedENV, InOrderFlatENV, Begin, End.
+   * define: inner annotation = Id. ASTVisitor that goes over the ASTNodes in
+   * which annotations can be defined, and checks if the annotations are of the
+   * kind that interests us. An array of inner annotations is defined inside of
+   * each outer annotation of interest. I think it will be less error prone and
+   * more scalable to implement another, internal, ASTVisitor that will go over
+   * each inner annotation node, and send everything to an outside function to
+   * add to the Sets as required. That means that each inner annotation will be
+   * visited twice from the same outer annotation, but that should not cause
+   * worry, since the outside visitor will do nothing.
+   *
+   * TODO: internal node parsing. Think about Nested parsing. */
+  public void runTest() {
+    cUnit.accept(new ASTVisitor() {
+      void addAnnotations(final List<Annotation> as) {
+        for (final Annotation ¢ : as)
+          dispatch(¢);
+      }
+
+      private void beginEndHandler(final Annotation a) {
+      }
+
+      void dispatch(final Annotation a) {
+        if (a.getTypeName() + "" == "nestedENV")
+          nestedHandler(a);
+        else if (a.getTypeName() + "" == "flatENV")
+          flatHandler(a);
+        else if (a.getTypeName() + "" == "BegingEndENV")
+          beginEndHandler(a);
+      }
+
+      void flatHandler(final Annotation ¢) {
+        flatHandler(az.singleMemberAnnotation(¢));
+      }
+
+      void flatHandler(final SingleMemberAnnotation $) {
+        if ($ == null)
+          return;
+        $.accept(new ASTVisitor() {
+        });
+      }
+
+      private void nestedHandler(final Annotation a) {
+      }
+
+      @Override public boolean visit(final MethodDeclaration d) {
+        /* Set<Entry<String, Information>> useCheckSet = Environment.uses(d);
+         * Set<Entry<String, Information>> declareCheckSet =
+         * Environment.declares(d); */
+        addAnnotations(extract.annotations(d));
+        return true;
+      }
+    });
   }
 }
 // Don't delete yet:
