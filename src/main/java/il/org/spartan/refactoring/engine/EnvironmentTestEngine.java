453cddc6480fa0f6d8b0f9e4f875269cfeeb5f27
alexkopzon <alexkopzon@192.168.1.10>
2016-09-05 13:19:44 +0300

    EnvironmentTestEngine first variant

--- src/main/java/il/org/spartan/refactoring/engine/EnvironmentTestEngine.java
+++ src/main/java/il/org/spartan/refactoring/engine/EnvironmentTestEngine.java
@@ -1,25 +1,54 @@
 package il.org.spartan.refactoring.engine;
 
 import org.eclipse.core.resources.*;
+import org.eclipse.core.runtime.*;
 import org.eclipse.jdt.core.*;
 import org.eclipse.jdt.core.dom.*;
 
 public class EnvironmentTestEngine {
-  static void startTest() {
-    IWorkspace workspace = ResourcesPlugin.getWorkspace();
-    IWorkspaceRoot root = workspace.getRoot();
-    // Get all projects in the workspace
-    IProject[] projects = root.getProjects();
-/*
-    IPackageFragment[] packages = JavaCore.create(project).getPackageFragments();
-    IPackageFragment mypackage = packages.get(0); // implement your own logic to select package
-    ICompilationUnit unit = mypackage.getCompilationUnits();
-*/
-    @SuppressWarnings("deprecation")
-    ASTParser parser = ASTParser.newParser(AST.JLS3); 
-    parser.setKind(ASTParser.K_COMPILATION_UNIT);
-    //parser.setSource(unit);
-    parser.setResolveBindings(true);
-    CompilationUnit cUnit = (CompilationUnit) parser.createAST(null);
+  EnvironmentTestEngine() {
+    IJavaProject javaProject = getJavaProject("spartenRefactoring");
+    IType iType;
+    try {
+      iType = javaProject.findType("il.org.spartan.refactoring.java.EnvironmentCodeExamples.java");
+      ICompilationUnit iCompilationUnit = iType.getCompilationUnit();
+
+      @SuppressWarnings("deprecation")
+      ASTParser parser = ASTParser.newParser(AST.JLS3); 
+      parser.setKind(ASTParser.K_COMPILATION_UNIT);
+      parser.setSource(iCompilationUnit);
+      parser.setResolveBindings(true);
+      CompilationUnit cUnit = (CompilationUnit) parser.createAST(null);
+      
+    } catch (JavaModelException e) {
+      e.printStackTrace();
+    }
+  }
+  
+  private static IJavaProject getJavaProject(String projectName) {
+    IProject project = ResourcesPlugin.getWorkspace().getRoot().getProject(projectName);
+    if (project == null)
+      return null;
+    IJavaProject $ = JavaCore.create(project);
+    return $ != null && $.exists() ? $ : null;
   }
 }
+
+// Don't delete yet:
+
+/*
+IWorkspace workspace = ResourcesPlugin.getWorkspace();
+IWorkspaceRoot root = workspace.getRoot();
+// Get all projects in the workspace
+IProject[] projects = root.getProjects();
+*/
+/*
+IPackageFragment[] packages = javaProject.getPackageFragments();
+for (IPackageFragment mypackage : packages) {
+  if (mypackage.getKind() == IPackageFragmentRoot.K_SOURCE) {
+    for (ICompilationUnit unit : mypackage.getCompilationUnits()) {
+      
+    }
+  }
+}
+*/
