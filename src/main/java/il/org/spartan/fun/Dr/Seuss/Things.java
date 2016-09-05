a4ea26b7bdab7a231bbd7c65d58ece4c63d03605
Yossi Gil <yossi.gil@gmail.com>
2016-09-05 05:09:46 +0300

    More fun with this example

---
+++ src/main/java/il/org/spartan/fun/Dr/Seuss/Things.java
@@ -0,0 +1,20 @@
+package il.org.spartan.fun.Dr.Seuss;
+
+interface Cat {
+  interface in {
+    interface the {
+      interface Hat {
+        static final Thing thing1 = new Thing(){};
+        static final Thing thing2 = new Thing(){};
+        static final Thing[] things = { thing1, thing2 };
+        interface Thing {
+          /* Empty on purpose */
+        }
+      }
+    }
+  }
+}
+
+public class Things {
+  
+}
