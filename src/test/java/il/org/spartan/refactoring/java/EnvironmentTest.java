0f15386f5b7d5634c77c7509ca654954160a84ad
Yossi Gil <yossi.gil@gmail.com>
2016-09-06 16:57:40 +0300

    sync

--- src/test/java/il/org/spartan/refactoring/java/EnvironmentTest.java
+++ src/test/java/il/org/spartan/refactoring/java/EnvironmentTest.java
@@ -12,6 +12,7 @@ import org.junit.*;
 
 import il.org.spartan.*;
 import il.org.spartan.refactoring.engine.*;
+import il.org.spartan.refactoring.java.Environment.*;
 import il.org.spartan.refactoring.utils.*;
 
 @SuppressWarnings("static-method") //
