063d7c6d2555cd5ffc3122a7fd6ea8e0abde44e9
dngreenst <dngreenst@gmail.com>
2016-09-06 23:43:48 +0300

    Nested annotation EnvironmentTest implementation

---
+++ src/main/java/il/org/spartan/refactoring/handlers/EnvNestedHandler.java
@@ -0,0 +1,63 @@
+package il.org.spartan.refactoring.handlers;
+
+import java.util.*;
+import java.util.Map.*;
+
+import org.eclipse.jdt.core.dom.*;
+
+import il.org.spartan.refactoring.ast.*;
+import il.org.spartan.refactoring.engine.*;
+import il.org.spartan.refactoring.java.*;
+import il.org.spartan.refactoring.java.Environment.*;
+
+public class EnvNestedHandler extends ENVTestEngineAbstract {
+  Set<Entry<String, Environment.Information>> testSet;
+
+  public EnvNestedHandler(final ASTNode $) {
+    n = $;
+    testSet = generateSet();
+  }
+
+  public EnvNestedHandler(final String ¢) {
+    n = getCompilationUnit(¢);
+    testSet = generateSet();
+  }
+
+  /* TODO Update EnvironmentCodeExamples - currently NestedENV does not
+   * represent the expected results of neither Environment.uses or
+   * Environment.declares. Should be the expected result of
+   * Environment.declares.
+   * 
+   * @see
+   * il.org.spartan.refactoring.engine.ENVTestEngineAbstract#buildEnvironmentSet
+   * (org.eclipse.jdt.core.dom.BodyDeclaration) */
+  @Override protected Set<Entry<String, Information>> buildEnvironmentSet(final BodyDeclaration $) {
+    return Environment.declares($);
+  }
+
+  @Override protected void handler(final Annotation ¢) {
+    handler(az.singleMemberAnnotation(¢));
+  }
+
+  /** Parse the outer annotation to get the inner ones. Add to the flat Set.
+   * Compare uses() and declares() output to the flat Set.
+   * @param $ JD */
+  void handler(final SingleMemberAnnotation a) {
+    assert a != null && !"@OutOfOrderflatENV".equals(a.getTypeName() + "");
+    if (!"OutOfOrderflatENV".equals(a.getTypeName() + ""))
+      return;
+    foundTestedAnnotation = true;
+    a.accept(new ASTVisitor() {
+      @SuppressWarnings("unchecked") List<MemberValuePair> values(final NormalAnnotation ¢) {
+        return ¢.values();
+      }
+
+      // runs on the Ids
+      @Override public boolean visit(final NormalAnnotation ¢) {
+        if (isNameId(¢.getTypeName()))
+          addValueToSet(values(¢));
+        return true;
+      }
+    });
+  }
+}
