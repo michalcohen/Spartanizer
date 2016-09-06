98ca6c45e52c5880991f4d09d9bc98c6922b30f4
alexkopzon <alexkopzon@192.168.1.10>
2016-09-06 20:15:17 +0300

    issue 122 work

--- src/main/java/il/org/spartan/refactoring/handlers/EnvFlatHandler.java
+++ src/main/java/il/org/spartan/refactoring/handlers/EnvFlatHandler.java
@@ -5,11 +5,9 @@ import java.util.Map.*;
 
 import org.eclipse.jdt.core.dom.*;
 
-import il.org.spartan.refactoring.ast.*;
 import il.org.spartan.refactoring.engine.*;
 import il.org.spartan.refactoring.java.*;
 import il.org.spartan.refactoring.java.Environment.*;
-import il.org.spartan.refactoring.utils.*;
 
 public class EnvFlatHandler extends ENVTestEngineAbstract {
   Set<Entry<String, Environment.Information>> testSet;
@@ -24,22 +22,25 @@ public class EnvFlatHandler extends ENVTestEngineAbstract {
     testSet = generateSet();
   }
 
+  @Override protected Set<Entry<String, Information>> buildEnvironmentSet(final BodyDeclaration d) {
+    return Environment.uses(d);
+  }
+
   /** Parse the outer annotation to get the inner ones. Add to the flat Set.
    * Compare uses() and declares() output to the flat Set.
    * @param $ JD */
   @Override protected void handler(final SingleMemberAnnotation a) {
-    if (a == null)
+    assert a != null && !"@OutOfOrderflatENV".equals(a.getTypeName() + "");
+    if (!"OutOfOrderflatENV".equals(a.getTypeName() + ""))
       return;
+    foundTestedAnnotation = true;
     a.accept(new ASTVisitor() {
       @SuppressWarnings("unchecked") List<MemberValuePair> values(final NormalAnnotation ¢) {
         return ¢.values();
       }
 
+      // runs on the Ids
       @Override public boolean visit(final NormalAnnotation ¢) {
-        /** Set<Entry<String, Information>> useSet =
-         * Environment.uses(getDefinition(a)); Set<Entry<String, Information>>
-         * declareSet = Environment.declares(getDefinition(a)); TODO: Run the
-         * code above and compare to the corresponding testSets */
         if (isNameId(¢.getTypeName()))
           addValueToSet(values(¢));
         return true;
