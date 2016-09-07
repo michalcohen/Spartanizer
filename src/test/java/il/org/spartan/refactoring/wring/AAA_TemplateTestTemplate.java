0ec420566db1c7d7e200235960b477f3443a1021
Yossi Gil <yossi.gil@gmail.com>
2016-09-07 19:02:46 +0300

    Add some default methods

--- src/test/java/il/org/spartan/refactoring/wring/AAA_TemplateTestTemplate.java
+++ src/test/java/il/org/spartan/refactoring/wring/AAA_TemplateTestTemplate.java
@@ -4,13 +4,25 @@ import static org.junit.Assert.*;
 
 import org.junit.*;
 import org.junit.runners.*;
+
 /** * Unit tests for {@link NameYourClassHere}
- * @author TODO // Write your name here 
+ * @author TODO // Write your name here
  * @since 2016 */
 @FixMethodOrder(MethodSorters.NAME_ASCENDING) //
 @SuppressWarnings({ "static-method", "javadoc" }) //
 public class AAA_TemplateTestTemplate {
-  @Test public void suiteCompiles() {
+  /** if fails, suite did not compile... */
+  @Test public void aaaaa0() {
+    new Object().toString();
+  }
+
+  /** if fails, assertions do not work */
+  @Test public void aaaaa1() {
     assert new Object() != null;
+  }
+  /** if fails, enable assertions (flag '-va') to the JVM */
+  @Test(expected = AssertionError.class) //
+  public void aaaaa2() {
+    assert new Object() == null;
   }
 }
