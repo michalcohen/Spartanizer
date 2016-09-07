980f4b19935ffb1245d258c93c132028beffc595
Yossi Gil <yossi.gil@gmail.com>
2016-09-07 18:43:59 +0300

    Added a template for Testing Classes

---
+++ src/test/java/il/org/spartan/refactoring/wring/AAA_TemplateTestTemplate.java
@@ -0,0 +1,16 @@
+package il.org.spartan.refactoring.wring;
+
+import static org.junit.Assert.*;
+
+import org.junit.*;
+import org.junit.runners.*;
+/** * Unit tests for {@link NameYourClassHere}
+ * @author TODO // Write your name here 
+ * @since 2016 */
+@FixMethodOrder(MethodSorters.NAME_ASCENDING) //
+@SuppressWarnings({ "static-method", "javadoc" }) //
+public class AAA_TemplateTestTemplate {
+  @Test public void test() {
+    fail("Not yet implemented");
+  }
+}
