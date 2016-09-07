152cbc447ed20337e078318f8e9c6f75ccbbdda6
dngreenst <dngreenst@gmail.com>
2016-09-08 00:06:17 +0300

    MapEntry cleanup

--- src/main/java/il/org/spartan/refactoring/utils/MapEntry.java
+++ src/main/java/il/org/spartan/refactoring/utils/MapEntry.java
@@ -11,6 +11,13 @@ public final class MapEntry<K, V> implements Map.Entry<K, V> {
     this.value = value;
   }
 
+  @Override public boolean equals(final Object o) {
+    return o == null ? false
+        : !(o.getClass() != getClass()) ? false
+            : (((MapEntry<?, ?>) o).getKey() == null && key == null || key.equals(((MapEntry<?, ?>) o).getKey()))
+                && (((MapEntry<?, ?>) o).getValue() == null && value == null || value.equals(((MapEntry<?, ?>) o).getValue()));
+  }
+
   @Override public K getKey() {
     return key;
   }
@@ -19,23 +26,17 @@ public final class MapEntry<K, V> implements Map.Entry<K, V> {
     return value;
   }
 
+  @Override public int hashCode() {
+    final int prime = 31;
+    int result = 1;
+    result = prime * result + (key == null ? 0 : key.hashCode());
+    result = prime * result + (value == null ? 0 : value.hashCode());
+    return result;
+  }
+
   @Override public V setValue(final V value) {
     final V $ = this.value;
     this.value = value;
     return $;
-  }
-  
-  @Override public int hashCode() {
-    final int prime = 31;
-    int result = 1;
-    result = prime * result + ((key == null) ? 0 : key.hashCode());
-    result = prime * result + ((value == null) ? 0 : value.hashCode());
-    return result;
-  }
-  
-  @Override public boolean equals(Object o){
-    return o == null ? false : !(o.getClass() != getClass()) ? false : 
-      (((MapEntry<?,?>) o).getKey() == null && key == null || key.equals(((MapEntry<?,?>) o).getKey())) &&
-      (((MapEntry<?,?>) o).getValue() == null && value == null || value.equals(((MapEntry<?,?>) o).getValue()));
   }
 }
