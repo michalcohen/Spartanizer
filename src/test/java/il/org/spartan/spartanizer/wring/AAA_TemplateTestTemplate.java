e5ccf2c2f3318529fcf1c4e765324908ca9fd838
Yossi Gil <yossi.gil@gmail.com>
2016-09-08 20:00:53 +0300

    More detailed example test class

--- src/test/java/il/org/spartan/spartanizer/wring/AAA_TemplateTestTemplate.java
+++ src/test/java/il/org/spartan/spartanizer/wring/AAA_TemplateTestTemplate.java
@@ -1,7 +1,12 @@
 package il.org.spartan.spartanizer.wring;
 
+import static il.org.spartan.azzert.*;
+import java.util.*;
+
 import org.junit.*;
 import org.junit.runners.*;
+
+import il.org.spartan.*;
 
 /** Unit tests for {@link NameYourClassHere}
  * @author TODO // Write your name here
@@ -10,18 +15,98 @@ import org.junit.runners.*;
 @SuppressWarnings({ "static-method", "javadoc" }) //
 public class AAA_TemplateTestTemplate {
   /** if fails, suite did not compile... */
-  @Test public void aaaaa0() {
+  @Test public void seriesZ_00() {
     new Object().toString();
   }
 
   /** if fails, assertions do not work */
-  @Test public void aaaaa1() {
-    assert new Object() != null;
+  @Test public void seriesZ_01() {
+    assert null == null;
   }
 
   /** if fails, enable assertions (flag '-va') to the JVM */
   @Test(expected = AssertionError.class) //
-  public void aaaaa2() {
-    assert new Object() == null;
+  public void seriesZ_02() {
+    assert null != null;
+  }
+
+  /** This is the incorrect way you should check for nulls, using {@link azzert}
+   * makes sure we get more informative messages */
+  @Test   public void seriesZ_03() {
+    azzert.isNull(null);
+  }
+
+  /** Correct way of checking for nulls. {@link azzert} cannot provide further
+   * information if the test fails, since failures give null which carries no
+   * information informative messages */
+  @Test   public void seriesZ_04() {
+    assert new Object() != null;
+  }
+
+  /** Correct way of checking for nulls. {@link azzert} cannot provide further
+   * information if the test fails, since failures give null which carries no
+   * information informative messages */
+  @Test   public void seriesZ_05() {
+    assert new Object() != null;
+  }
+
+  /** Correct way of checking for true value. {@link azzert} cannot provide
+   * further information if the test fails, since failures give nothing but
+   * boolean value. */
+  @Test   public void seriesZ_06() {
+    assert $true();
+    assert $true() : "Failure in " + object();
+  }
+
+  /** Correct way of checking for false value. {@link azzert} cannot provide
+   * further information if the test fails, since failures give nothing but
+   * boolean value. */
+  @Test  public void seriesZ_07() {
+    assert !$false();
+    assert !$false() : "Failure in " + object();
+  }
+
+  /** Correct way of checking types */
+  @Test  public void seriesZ_08() {
+    azzert.that(new ArrayList<>(), instanceOf(List.class));
+  }
+
+  /** Correct way of checking for inequality of values */
+  @Test   public void seriesZ_09() {
+    azzert.that(object(), not(object()));
+  }
+
+  /** Correct way of checking for equality of values */
+   @Test  public void seriesZ_10() {
+    azzert.that(sameSomeObject(), is(sameSomeObject()));
+  }
+
+  /** Correct way of checking for equality of numbers */
+   @Test  public void seriesZ_11() {
+    azzert.that($0(), is($1()));
+  }
+
+  private final int $0() {
+    return 0;
+  }
+
+  private final int $1() {
+    return 0;
+  }
+
+  private final boolean $false() {
+    return false;
+  }
+
+  private final boolean $true() {
+    return true;
+  }
+
+  private static final Object object() {
+    return new Object();
+  }
+
+  private final Object sameSomeObject() {
+    return this;
   }
 }
