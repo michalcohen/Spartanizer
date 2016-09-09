65c2241dd38f00c1c29d28f7a1ad91693f580036
dngreenst <dngreenst@gmail.com>
2016-09-09 12:23:55 +0300

    Deleted redundant examples

--- src/test/java/il/org/spartan/spartanizer/annotation/NestedENV.java
+++ src/test/java/il/org/spartan/spartanizer/annotation/NestedENV.java
@@ -6,27 +6,3 @@ package il.org.spartan.spartanizer.annotation;
 public @interface NestedENV {
   String[] value();
 }
-
-@Entries({ //
-    @Entry(name = "int", clazz = int.class), //
-    @Entry(name = "void", clazz = void.class), //
-    @Entry(name = "DELME", clazz = DELME.class)//
-}) class DELME {
-  /** Internationally Empty (Dan and ALex: why? */
-}
-
-@interface Entries {
-  Entry[] value();
-}
-
-/** @author Alex Kopakzon
- * @since 2016 */
-@interface Entry {
-  Class<?> clazz();
-
-  String name();
-}
-
-@Entry(name = "int", clazz = int.class) class STAM {
-  /** TODO: Alex and Dan, do not keep this in production code. Remove it */
-}
