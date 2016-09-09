461d3b176ab5832fcad21b8cc8c6ab30625887a8
M4tt30rru <matteo.orru@diee.unica.it>
2016-09-09 16:53:13 +0300

    Add tests for LogManager

---
+++ src/test/java/il/org/spartan/spartanizer/utils/LogManagerTest.java
@@ -0,0 +1,46 @@
+package il.org.spartan.spartanizer.utils;
+
+import static org.junit.Assert.*;
+
+import org.junit.*;
+
+public class LogManagerTest {
+
+  @Test public void testInitialize() {
+    String testDir = "/home/matteo/SpartanLog";
+    LogManager.initialize(testDir);
+    assertTrue(LogManager.getLogDir().equals(testDir));
+  }
+  
+  @Test public void testNotActive(){
+    LogManager.deActivateLog();
+    assertFalse(LogManager.isActive());
+  }
+  
+  @Test public void testActive(){
+    LogManager.activateLog();
+    assertTrue(LogManager.isActive());
+  }
+  
+  @Ignore @Test public void testGetLogDir() {
+    fail("Not yet implemented");
+  }
+
+  @Ignore @Test public void testSetLogDir() {
+    fail("Not yet implemented");
+  }
+
+  @Test public void testGetLogWriterNotNull() {
+    LogManager.initialize("/home/matteo/SpartanLog");
+    assertNotNull(LogManager.getLogWriter());
+  }
+  
+  @Test public void testPrintRow() {
+    LogManager.initialize("/home/matteo/SpartanLog");
+    LogWriter lw = LogManager.getLogWriter();
+    assertNotNull(LogManager.getLogWriter());
+    lw.initializeWriter("/home/matteo/SpartanLog/test.csv");
+    lw.printRow("a", "b", "c");
+    lw.close();
+  }
+}
