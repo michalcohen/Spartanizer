f89b12305280850a59141a1e9458e1f837a2dc67
Yossi Gil <yossi.gil@gmail.com>
2016-09-09 09:04:05 +0300

    Document Mittelmans' work

--- src/main/java/il/org/spartan/plugin/PreferencesDefaultValuesInitializer.java
+++ src/main/java/il/org/spartan/plugin/PreferencesDefaultValuesInitializer.java
@@ -17,7 +17,7 @@ public class PreferencesDefaultValuesInitializer extends AbstractPreferenceIniti
     final IPreferenceStore s = store();
     s.setDefault(PLUGIN_STARTUP_BEHAVIOR_ID, "remember");
     s.setDefault(NEW_PROJECTS_ENABLE_BY_DEFAULT_ID, true);
-    for (final WringGroup w : WringGroup.values())
-      s.setDefault(w.id, "on");
+    for (final WringGroup g : WringGroup.values())
+      s.setDefault(g.id, "on");
   }
 }
