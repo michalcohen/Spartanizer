774f193f3777e383bbef52e770d6f66dee3e59a6
dngreenst <dngreenst@gmail.com>
2016-09-10 21:52:07 +0300

    Issue #157 fix

--- src/main/java/il/org/spartan/spartanizer/engine/spartan.java
+++ src/main/java/il/org/spartan/spartanizer/engine/spartan.java
@@ -52,6 +52,11 @@ public interface spartan {
   }
 
   static String shorten(final ParameterizedType t) {
+    String $;
+    List<?> l = t.typeArguments(); //the type is not ParameterizedType, hence step.typeArguments cannot be used
+    $ = l.get(0) instanceof WildcardType && ((WildcardType)l.get(0)).getBound() == null ? shorten(t.getType()) : shorten(step.typeArguments(t)); 
+    if ($ == null)
+      return null;
     switch (t.getType() + "") {
       case "Collection":
       case "Iterable":
@@ -62,12 +67,9 @@ public interface spartan {
       case "LinkedHashSet":
       case "ArrayList":
       case "TreeSet":
-        final String $ = shorten(step.typeArguments(t));
-        if ($ == null)
-          return null;
         return $ + "s";
       default:
-        return null;
+        return $;
     }
   }
 
