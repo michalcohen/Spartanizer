da70171dc500aa0aacc09c7d4c61e0ea061af862
Ori Roth <ori.rothh@gmail.com>
2016-09-10 21:49:02 +0300

    Minor fixes to VariableRenameUnderscoreToDoubleUnderscore see #161

--- src/main/java/il/org/spartan/spartanizer/wring/VariableRenameUnderscoreToDoubleUnderscore.java
+++ src/main/java/il/org/spartan/spartanizer/wring/VariableRenameUnderscoreToDoubleUnderscore.java
@@ -2,14 +2,14 @@ package il.org.spartan.spartanizer.wring;
 
 import org.eclipse.jdt.core.dom.*;
 
-/** Replaces name of variables named "__" into "____"
+/** Replaces name of variables named "_" into "__"
  * @author Ori Roth
  * @param <N> either SingleVariableDeclaration or VariableDeclarationFragment
  * @since 2016/05/08 */
 public final class VariableRenameUnderscoreToDoubleUnderscore<N extends VariableDeclaration> //
     extends VariableChangeName<N> implements Kind.UnusedArguments {
   @Override boolean change(final N n) {
-    return "_".equals(n.getName() + "");
+    return "_".equals(n.getName());
   }
 
   @Override String description(@SuppressWarnings("unused") final N __) {
