2176667a48c7227e392ee00d87ee60979a099808
Yossi Gil <yossi.gil@gmail.com>
2016-09-12 11:44:37 +0300

    Issue #119: I believe there is a bug here.

--- src/main/java/il/org/spartan/spartanizer/wring/InfixEmptyStringAdditionToString.java
+++ src/main/java/il/org/spartan/spartanizer/wring/InfixEmptyStringAdditionToString.java
@@ -1,5 +1,6 @@
 package il.org.spartan.spartanizer.wring;
 
+import static il.org.spartan.spartanizer.ast.wizard.*;
 import static il.org.spartan.lisp.*;
 import static il.org.spartan.spartanizer.ast.step.*;
 
@@ -10,39 +11,39 @@ import org.eclipse.jdt.core.dom.*;
 import il.org.spartan.spartanizer.assemble.*;
 import il.org.spartan.spartanizer.ast.*;
 import il.org.spartan.spartanizer.engine.*;
+import il.org.spartan.spartanizer.engine.type.Primitive.*;
 
-/** transforms "" + x to x when x is of type String
+/** Converts <code>""+"foo"</code> to <code>"foo"</code> when x is of type
+ * String
  * @author Stav Namir
  * @author Niv Shalmon
  * @since 2016-08-29 */
-public class InfixEmptyStringAdditionToString extends Wring.ReplaceCurrentNode<InfixExpression>
-    implements il.org.spartan.spartanizer.wring.Kind.NoImpact {
-  private static String descriptionAux(final Expression x) {
-    return "Use " + (x != null ? x + "" : "the variable alone");
-  }
-
-  private static boolean isEmptyStringLiteral(final Expression x) {
-    return wizard.same(x, x.getAST().newStringLiteral());
-  }
-
+public class InfixEmptyStringAdditionToString extends Wring.ReplaceCurrentNode<InfixExpression> implements Kind.NoImpact {
   private static boolean validTypes(final Expression ¢1, final Expression ¢2) {
-    return type.get(¢1) == type.Primitive.Certain.STRING && isEmptyStringLiteral(¢2);
+    return type.get(¢1) == Certain.STRING && iz.emptyStringLiteral(¢2);
   }
 
   @Override public String description() {
-    return descriptionAux(null);
+    return "[\"\"+foo]->foo";
   }
 
   @Override String description(final InfixExpression x) {
-    return descriptionAux(isEmptyStringLiteral(right(x)) ? left(x) : right(x));
+    return "Eliminate concatentation of \"\" to" + (iz.emptyStringLiteral(right(x)) ? left(x) : right(x));
   }
 
   @Override Expression replacement(final InfixExpression x) {
-    if (x.getOperator() != wizard.PLUS2)
+    if (!iz.infixPlus(x))
       return null;
+    // TODO: Niv, I believe this is one of the problems here. Extract all
+    // operands opens parenthesis. Maybe use hop.operands?
     final List<Expression> es = extract.allOperands(x);
     assert es.size() > 1;
     final int ¢ = es.size();
+    // TODO: Niv, I am pretty sure the following is buggy. It is too complex to
+    // be correct. In fact, if you apply the plugin to itself, you will find the
+    // bug... I believe that it is in Wrapping. The right way to do this is to
+    // create a new list, and move to it only what's left. Please open an issue,
+    // create test cases to demonstrate the problem, and then fix it.
     for (int i = 0; i < es.size() - 1;)
       if (validTypes(es.get(i), es.get(i + 1)))
         es.remove(i + 1);
@@ -50,6 +51,6 @@ public class InfixEmptyStringAdditionToString extends Wring.ReplaceCurrentNode<I
         ++i;
       else
         es.remove(i);
-    return es.size() == ¢ ? null : es.size() == 1 ? first(es) : subject.operands(es).to(wizard.PLUS2);
+    return es.size() == ¢ ? null : es.size() == 1 ? first(es) : subject.operands(es).to(PLUS2);
   }
 }
