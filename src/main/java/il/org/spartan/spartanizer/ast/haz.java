5f0625b9e22304199a3f5ff744d9af64e91ef450
Yossi Gil <yossi.gil@gmail.com>
2016-09-12 17:12:41 +0300

    Started working on #165

---
+++ src/main/java/il/org/spartan/spartanizer/ast/haz.java
@@ -0,0 +1,48 @@
+package il.org.spartan.spartanizer.ast;
+
+import org.eclipse.jdt.core.dom.*;
+
+import il.org.spartan.*;
+
+/** An empty <code><b>enum</b></code> for fluent programming. The name should
+ * say it all: The name, followed by a dot, followed by a method name, should
+ * read like a sentence phrase.
+ * @author Yossi Gil
+ * @since 2016-09-12 */
+public enum haz {
+  ;
+  @SuppressWarnings("unused") public static boolean variableDefinition(final ASTNode n) {
+    final Wrapper<Boolean> $ = new Wrapper<>(Boolean.FALSE);
+    n.accept(new ASTVisitor() {
+      @Override public boolean visit(final EnumConstantDeclaration __) {
+        return found();
+      }
+
+      @Override public boolean visit(final FieldDeclaration node) {
+        return found();
+      }
+
+      @Override public boolean visit(final SingleVariableDeclaration node) {
+        return found();
+      }
+
+      @Override public boolean visit(final VariableDeclarationExpression node) {
+        return found();
+      }
+
+      @Override public boolean visit(final VariableDeclarationFragment __) {
+        return found();
+      }
+
+      @Override public boolean visit(final VariableDeclarationStatement __) {
+        return found();
+      }
+
+      boolean found() {
+        $.set(Boolean.TRUE);
+        return false;
+      }
+    });
+    return $.get().booleanValue();
+  }
+}
---
+++ src/test/java/il/org/spartan/spartanizer/utils/hazTest.java
@@ -0,0 +1,51 @@
+package il.org.spartan.spartanizer.utils;
+
+import static il.org.spartan.spartanizer.engine.into.*;
+
+import org.junit.*;
+import org.junit.runners.*;
+
+import il.org.spartan.spartanizer.ast.*;
+
+/** Unit tests for {@link haz}
+ * @author TODO // Write your name here
+ * @since 2016 */
+@FixMethodOrder(MethodSorters.NAME_ASCENDING) //
+@SuppressWarnings({ "static-method", "javadoc" }) //
+public class hazTest {
+  @Test public void seriesA_01() {
+    assert !haz.variableDefinition(e("0"));
+  }
+
+  @Test public void seriesA_02() {
+    assert haz.variableDefinition(s("int i;"));
+  }
+
+  @Test public void seriesA_03() {
+    assert !haz.variableDefinition(s("class C {}"));
+  }
+
+  @Test public void seriesA_04() {
+    assert !haz.variableDefinition(s("class C {void f(){}}"));
+  }
+
+  @Test public void seriesA_05() {
+    assert haz.variableDefinition(s("class C {void f(){}int a = f();}"));
+  }
+
+  @Test public void seriesA_06() {
+    assert haz.variableDefinition(s("class C {void f(){int a = new C().hashCode();}}"));
+  }
+
+  @Test public void seriesA_07() {
+    assert haz.variableDefinition(s("class C {void f(){for (int a = 0;i < 100; i++) ++i;}}"));
+  }
+
+  @Test public void seriesA_08() {
+    assert haz.variableDefinition(s("try (O a = new O()) {}"));
+  }
+
+  @Test public void seriesA_09() {
+    assert haz.variableDefinition(s("try {} catch (Exception x) {}"));
+  }
+}
