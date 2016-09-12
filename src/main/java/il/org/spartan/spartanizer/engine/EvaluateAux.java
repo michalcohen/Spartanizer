e48ccf4d6aea15a6e30559ae5d8c2405d8f77846
Yossi Gil <yossi.gil@gmail.com>
2016-09-12 18:38:50 +0300

    More consistent naming

--- src/main/java/il/org/spartan/spartanizer/engine/EvaluateAux.java
+++ src/main/java/il/org/spartan/spartanizer/engine/EvaluateAux.java
@@ -14,33 +14,32 @@ import static il.org.spartan.spartanizer.engine.type.Primitive.Certain.*;
  * wrings
  * @author Dor Ma'ayan
  * @since 2016 */
-
 public interface EvaluateAux {
-      static double extractDouble(final Expression x) {
-        if (!iz.longType(x))
-          return !iz.prefixExpression(x) ? Double.parseDouble(az.numberLiteral(x).getToken())
-              : -1 * Double.parseDouble(az.numberLiteral(az.prefixExpression(x).getOperand()).getToken());
-        final String token = az.numberLiteral(x).getToken();
-        if (!iz.prefixExpression(x))
-          return Double.parseDouble(token.substring(0, token.length() - 1));
-        final String negToken = az.numberLiteral(az.prefixExpression(x).getOperand()).getToken();
-        return -1 * Double.parseDouble(negToken.substring(0, negToken.length() - 1));
-      }
+  static double extractDouble(final Expression x) {
+    if (!iz.longType(x))
+      return !iz.prefixExpression(x) ? Double.parseDouble(az.numberLiteral(x).getToken())
+          : -1 * Double.parseDouble(az.numberLiteral(az.prefixExpression(x).getOperand()).getToken());
+    final String token = az.numberLiteral(x).getToken();
+    if (!iz.prefixExpression(x))
+      return Double.parseDouble(token.substring(0, token.length() - 1));
+    final String negToken = az.numberLiteral(az.prefixExpression(x).getOperand()).getToken();
+    return -1 * Double.parseDouble(negToken.substring(0, negToken.length() - 1));
+  }
 
   static int extractInt(final Expression x) {
     return !iz.prefixExpression(x) ? Integer.parseInt(az.numberLiteral(x).getToken())
         : -1 * Integer.parseInt(az.numberLiteral(az.prefixExpression(x).getOperand()).getToken());
   }
 
-static long extractLong(final Expression x) {
-  final String token = az.numberLiteral(x).getToken();
-  if (iz.intType(x))
-    return Long.parseLong(token);
-  if (!iz.prefixExpression(x))
-    return Long.parseLong(token.substring(0, token.length() - 1));
-  final String negToken = az.numberLiteral((az.prefixExpression(x).getOperand())).getToken();
-  return -1 * Long.parseLong(negToken.substring(0, negToken.length() - 1));
-}
+  static long extractLong(final Expression x) {
+    final String token = az.numberLiteral(x).getToken();
+    if (iz.intType(x))
+      return Long.parseLong(token);
+    if (!iz.prefixExpression(x))
+      return Long.parseLong(token.substring(0, token.length() - 1));
+    final String negToken = az.numberLiteral((az.prefixExpression(x).getOperand())).getToken();
+    return -1 * Long.parseLong(negToken.substring(0, negToken.length() - 1));
+  }
 
   static type getEvaluatedType(final InfixExpression x) {
     boolean isLong = false;
@@ -67,12 +66,12 @@ static long extractLong(final Expression x) {
   }
 
   static boolean isCompatible(final Expression x) {
-    return iz.isNumberLiteral(x) && isNumber(x) || iz.prefixExpression(x)
-        && (az.prefixExpression(x).getOperator() == PrefixExpression.Operator.MINUS && iz.isNumberLiteral(az.prefixExpression(x).getOperand()));
+    //TODO: Dor, i
+    return iz.numberLiteral(x) && isNumber(x) || iz.prefixExpression(x)
+        && (az.prefixExpression(x).getOperator() == PrefixExpression.Operator.MINUS && iz.numberLiteral(az.prefixExpression(x).getOperand()));
   }
 
   static boolean isNumber(final Expression x) {
     return type.isInt(x) || type.isDouble(x) || type.isLong(x);
   }
-
 }
