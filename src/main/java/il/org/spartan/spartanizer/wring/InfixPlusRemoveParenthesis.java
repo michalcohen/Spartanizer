0b62d12ece5c560cc751b817d8410401e1f5c653
NivShalmon <shalmon.niv@gmail.com>
2016-09-12 21:30:56 +0300

    bug fix- first operand doens't need parenthesis unless conditional (issue #162)

--- src/main/java/il/org/spartan/spartanizer/wring/InfixPlusRemoveParenthesis.java
+++ src/main/java/il/org/spartan/spartanizer/wring/InfixPlusRemoveParenthesis.java
@@ -86,7 +86,7 @@ public class InfixPlusRemoveParenthesis extends ReplaceCurrentNode<InfixExpressi
           ¢ = az.parenthesizedExpression(¢).getExpression();
         }
         if (iz.infixExpression(¢)) {
-          if (!canRemove((InfixExpression) ¢))
+          if (i != 0 && !canRemove((InfixExpression) ¢))
             continue;
         } else if (iz.conditional(¢) || iz.is(¢, ASTNode.LAMBDA_EXPRESSION))
           continue;
