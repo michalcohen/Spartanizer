5cae81b16123e010f15e18807c44a897398cfd43
M4tt30rru <matteo.orru@diee.unica.it>
2016-09-12 18:22:12 +0300

    Update

--- src/main/java/il/org/spartan/plugin/PreferencesResources.java
+++ src/main/java/il/org/spartan/plugin/PreferencesResources.java
@@ -4,26 +4,10 @@ import org.eclipse.jface.preference.*;
 
 import il.org.spartan.spartanizer.wring.*;
 
-/** ??
- * @author Daniel Mittelman
- * @since 2016 */
-public class PreferencesResources {
-  /** Page description **/
-  public static final String PAGE_DESCRIPTION = "Preferences for the spartanizer plug-in";
-  /** General preferences **/
-  public static final String PLUGIN_STARTUP_BEHAVIOR_ID = "pref_startup_behavior";
-  public static final String PLUGIN_STARTUP_BEHAVIOR_TEXT = "Plugin startup behavior:";
-  public static final String[][] PLUGIN_STARTUP_BEHAVIOR_OPTIONS = { { "Remember individual project settings", "remember" }, //
-      { "Enable for all projects", "always_on" }, //
-      { "Disable for all projects", "always_off" }, //
-  };
-  public static final String NEW_PROJECTS_ENABLE_BY_DEFAULT_ID = "pref_enable_by_default_for_new_projects";
-  public static final String NEW_PROJECTS_ENABLE_BY_DEFAULT_TEXT = "Enable by default for newly created projects";
 
+public class PreferencesResources {
   /** An enum holding together all the "enabled spartanizations" options, also
-   * allowing to get the set preference value for each of them
-   * @Author Daniel Mittelman
-   * @since 2016 */
+   * allowing to get the set preference value for each of them */
   public enum WringGroup {
     Abbreviation(Kind.Abbreviation.class), //
     Canonicalization(Kind.Collapse.class), //
@@ -38,28 +22,8 @@ public class PreferencesResources {
     Ternarization(Kind.Ternarization.class), //
     UnusedArguments(Kind.UnusedArguments.class),//
     ;
-    public static WringGroup find(final Kind ¢) {
-      return find(¢.getClass());
-    }
-
     static IPreferenceStore store() {
       return Plugin.plugin().getPreferenceStore();
-    }
-
-    private static WringGroup find(final Class<? extends Kind> k) {
-      for (final WringGroup $ : WringGroup.values())
-        if ($.clazz.isAssignableFrom(k))
-          return $;
-      return null;
-    }
-
-    private static Object getLabel(final Class<? extends Kind> k) {
-      try {
-        return k.getField("label").get(null);
-      } catch (IllegalArgumentException | IllegalAccessException | NoSuchFieldException | SecurityException e) {
-        e.printStackTrace();
-        return null;
-      }
     }
 
     private final Class<? extends Kind> clazz;
@@ -69,11 +33,46 @@ public class PreferencesResources {
     private WringGroup(final Class<? extends Kind> clazz) {
       this.clazz = clazz;
       id = clazz.getCanonicalName();
-      label = getLabel(clazz) + "";
+      label = "" + getLabel(clazz);
+    }
+
+    private static Object getLabel(final Class<? extends Kind> c) {
+      try {
+        return c.getField("label").get(null);
+      } catch (IllegalArgumentException | IllegalAccessException | NoSuchFieldException | SecurityException e) {
+        e.printStackTrace();
+        return null;
+      }
     }
 
     public boolean isEnabled() {
       return Plugin.plugin() == null || "on".equals(store().getString(id));
     }
+
+    public static WringGroup find(final Kind ¢) {
+      return find(¢.getClass());
+    }
+
+    private static WringGroup find(final Class<? extends Kind> c) {
+      for (final WringGroup $ : WringGroup.values())
+        if ($.clazz.isAssignableFrom(c))
+          return $;
+      return null;
+    }
   }
+
+  /** Page description **/
+  public static final String PAGE_DESCRIPTION = "Preferences for the Spartan Refactoring plug-in";
+  /** General preferences **/
+  public static final String PLUGIN_STARTUP_BEHAVIOR_ID = "pref_startup_behavior";
+  public static final String PLUGIN_STARTUP_BEHAVIOR_TEXT = "Plugin startup behavior:";
+  public static final String[][] PLUGIN_STARTUP_BEHAVIOR_OPTIONS = {
+      { "Remember individual project settings", //
+          "remember" },
+      { "Enable for all projects", //
+          "always_on" }, //
+      { "Disable for all projects", //
+          "always_off" } };
+  public static final String NEW_PROJECTS_ENABLE_BY_DEFAULT_ID = "pref_enable_by_default_for_new_projects";
+  public static final String NEW_PROJECTS_ENABLE_BY_DEFAULT_TEXT = "Enable by default for newly created projects";
 }
