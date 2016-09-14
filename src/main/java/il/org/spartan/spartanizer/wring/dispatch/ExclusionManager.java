20f7bb23613e5f5fb7b5c6cbf5a8b8c9a5a70716
Yossi Gil <yossi.gil@gmail.com>
2016-09-14 11:28:04 +0300

    Documentation

--- src/main/java/il/org/spartan/spartanizer/wring/dispatch/ExclusionManager.java
+++ src/main/java/il/org/spartan/spartanizer/wring/dispatch/ExclusionManager.java
@@ -6,6 +6,11 @@ import org.eclipse.jdt.core.dom.*;
 
 import il.org.spartan.spartanizer.ast.*;
 
+/** Hack to stop the trimmer from making more suggestions. The class should die.
+ * It serves the purpose of disabling suggestions of spartanization in a method,
+ * whose parameters are changed. But this disabling does not belong here. 
+ * @author Yossi Gil
+ * @year 2015 */
 public class ExclusionManager {
   final Set<ASTNode> inner = new HashSet<>();
 
