8b273f3c7f85975ea280659051b1e8de2d7f8bec
Yossi Gil <yossi.gil@gmail.com>
2016-09-15 21:05:25 +0300

    Consolidate tests for #209

--- src/test/java/il/org/spartan/spartanizer/wring/Issue209Test.java
+++ src/test/java/il/org/spartan/spartanizer/wring/Issue209Test.java
@@ -14,7 +14,30 @@ public class Issue209Test {
   @Test public void issue209_01() {
     trimming("new Integer(3).toString()").stays();
   }
-  
+  @Test public void issue54_01() {
+    trimming("(x.toString())").to("x + \"\"");
+  }
+
+  @Test public void issue54_02() {
+    trimming("if(x.toString() == \"abc\") return a;").to("if(x + \"\" == \"abc\") return a;");
+  }
+
+  @Test public void issue54_03() {
+    trimming("((Integer)6).toString()").to("(Integer)6 + \"\"");
+  }
+
+  @Test public void issue54_04() {
+    trimming("switch(x.toString()){ case \"1\": return; case \"2\": return; default: return; }")
+        .to("switch(x + \"\"){ case \"1\": return; case \"2\": return; default: return; }");
+  }
+
+  @Test public void issue54_05() {
+    trimming("x.toString(5)").stays();
+  }
+
+  @Test public void issue54_06() {
+    trimming("a.toString().length()").to("(a + \"\").length()");
+  }
   @Test public void issue209_02() {
     trimming("new Integer(3).toString();").stays();
   }
--- src/test/java/il/org/spartan/spartanizer/wring/Version250Test.java
+++ src/test/java/il/org/spartan/spartanizer/wring/Version250Test.java
@@ -1009,30 +1009,7 @@ public class Version250Test {
         .to("enum a {x,y,z; void f() {}}");//
   }
 
-  @Test public void issue54_01() {
-    trimming("(x.toString())").to("x + \"\"");
-  }
-
-  @Test public void issue54_02() {
-    trimming("if(x.toString() == \"abc\") return a;").to("if(x + \"\" == \"abc\") return a;");
-  }
-
-  @Test public void issue54_03() {
-    trimming("((Integer)6).toString()").to("(Integer)6 + \"\"");
-  }
-
-  @Test public void issue54_04() {
-    trimming("switch(x.toString()){ case \"1\": return; case \"2\": return; default: return; }")
-        .to("switch(x + \"\"){ case \"1\": return; case \"2\": return; default: return; }");
-  }
-
-  @Test public void issue54_05() {
-    trimming("x.toString(5)").stays();
-  }
-
-  @Test public void issue54_06() {
-    trimming("a.toString().length()").to("(a + \"\").length()");
-  }
+  
 
   @Test public void issue54_1() {
     trimming("(x.toString())").to("(x+\"\")");
