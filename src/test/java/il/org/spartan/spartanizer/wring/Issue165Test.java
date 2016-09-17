3217786176908983d992150888fe479b404b80c2
Yossi Gil <yossi.gil@gmail.com>
2016-09-17 15:53:20 +0300

    Document class

--- src/test/java/il/org/spartan/spartanizer/wring/Issue165Test.java
+++ src/test/java/il/org/spartan/spartanizer/wring/Issue165Test.java
@@ -5,7 +5,8 @@ import static il.org.spartan.spartanizer.wring.TrimmerTestsUtils.*;
 import org.junit.*;
 import org.junit.runners.*;
 
-/** @author Yossi Gil
+/** Unit tests for centification of a single parameter to a function
+ * @author Yossi Gil
  * @since 2016 */
 @FixMethodOrder(MethodSorters.NAME_ASCENDING) //
 @SuppressWarnings({ "static-method", "javadoc" }) //
