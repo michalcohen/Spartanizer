445fc73da181e06d7957bc9da385aa602f12eb6d
alexkopzon <alexkopzon@192.168.1.10>
2016-09-18 01:36:17 +0300

    Learning initializers

---
+++ src/test/java/il/org/spartan/spartanizer/java/InitializerTest.java
@@ -0,0 +1,43 @@
+package il.org.spartan.spartanizer.java;
+
+// TODO: Yossi, did you want me to play with initializers? I moved this from the
+// ToolBox. Didn't finish yet.
+public final class InitializerTest {
+  static {
+    InitializerTest.a++;
+    System.out.println("Static initializer end (0).");
+    // Seems that we can't do here anything that is connected to the instance.
+    // But can be used for initializing some global data structures which are
+    // referenced by
+    // the instances of this class...
+  }
+  static int a = 0;
+
+  @SuppressWarnings("unused") public static final void main(final String[] s) {
+    new InitializerTest(3);
+    // b is not recognized here.
+    System.out.print(InitializerTest.a);
+  }
+
+  {
+    // b is not recognized here.
+    ++a;
+    // a is updated here instead of being incremented in all the constructors.
+    System.out.println("Instance initializer end (1).");
+  }
+
+  InitializerTest() {
+    // b is not recognized here.
+    System.out.println("Empty Constructor");
+  }
+
+  InitializerTest(final int i) {
+    // b is not recognized here.
+    System.out.println("int constructor");
+  }
+
+  InitializerTest(final String s) {
+    // b is not recognized here.
+    System.out.println("String constructor");
+  }
+}
