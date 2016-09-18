6554233684776480d6be67dd71e084b250b19cde
Yossi Gil <yossi.gil@gmail.com>
2016-09-18 11:09:29 +0300

    rename tests of #130

--- src/test/java/il/org/spartan/spartanizer/wrings/Issue130Test.java
+++ src/test/java/il/org/spartan/spartanizer/wrings/Issue130Test.java
@@ -13,29 +13,29 @@ import il.org.spartan.spartanizer.wringing.*;
 @FixMethodOrder(MethodSorters.NAME_ASCENDING) //
 @SuppressWarnings({ "static-method", "javadoc" }) //
 public final class Issue130Test {
-  @Test public void issue130_1() {
+  @Test public void A$01() {
     trimmingOf("while(true){doSomething();if(done())break;}return something();").gives("while(true){doSomething();if(done())return something();}");
   }
 
-  @Test public void issue130_2() {
+  @Test public void A$02() {
     trimmingOf("while(false){doSomething();if(done())break;}return something();").stays();
   }
 
-  @Test public void issue130_3() {
+  @Test public void A$03() {
     trimmingOf("while(true){doSomething();if(done()){t+=2;break;}}return something();")
         .gives("while(true){doSomething();if(done()){t+=2;return something();}}");
   }
 
-  @Test public void issue130_4() {
+  @Test public void A$04() {
     trimmingOf("for(int i=4 ; true ; ++i){doSomething();if(done())break;}return something();")
         .gives("for(int i=4 ; true ; ++i){doSomething();if(done())return something();}");
   }
 
-  @Test public void issue130_5() {
+  @Test public void A$05() {
     trimmingOf("for(int i=4 ; i<s.length() ; ++i){doSomething();if(done())break;}return something();").stays();
   }
 
-  @Test public void issue130_6() {
+  @Test public void A$06() {
     trimmingOf("for(int i=4 ; true ; ++i){doSomething();if(done()){t+=2;break;}}return something();")
         .gives("for(int i=4 ; true ; ++i){doSomething();if(done()){t+=2;return something();}}");
   }
