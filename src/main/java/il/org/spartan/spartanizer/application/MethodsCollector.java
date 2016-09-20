34d026466b332512d7c8e9210426912dae91a68d
Yossi Gil <yossi.gil@gmail.com>
2016-09-20 15:59:23 +0300

    Work for Orit

---
+++ src/main/java/il/org/spartan/spartanizer/application/MethodsCollector.java
@@ -0,0 +1,84 @@
+package il.org.spartan.spartanizer.application;
+
+import java.io.*;
+import java.util.*;
+
+import org.eclipse.jdt.core.dom.*;
+
+import il.org.spartan.*;
+import il.org.spartan.collections.*;
+import il.org.spartan.java.*;
+import il.org.spartan.java.Token.*;
+import il.org.spartan.spartanizer.assemble.*;
+import il.org.spartan.spartanizer.cmdline.*;
+import il.org.spartan.spartanizer.engine.*;
+import il.org.spartan.utils.*;
+
+/** Demonstrates iteration through files.
+ * @author Yossi Gil
+ * @year 2015 */
+public final class MethodsCollector {
+  static Set<String> methods = new LinkedHashSet<>();
+
+  public static void main(final String[] where) throws IOException {
+    collect(where.length != 0 ? where : new String[] { "." });
+    int n = 0;
+    final CSVStatistics w = new CSVStatistics("methods.csv", "property");
+    for (final String s : methods) {
+      if (++n % 10 != 0)
+        continue;
+      w.put("Body", s);
+      w.put("Characters", s.length());
+      w.put("Tokens", tokens(s));
+      w.nl();
+    }
+    System.err.println("Look for your output here: " + w.close());
+  }
+
+  static int tokens(String s) {
+    int $ = 0;
+    for (Tokenizer tokenizer = new Tokenizer(new StringReader(s));;) {
+      Token t = tokenizer.next();
+      if (t == null)
+        return $;
+      if (t.kind == Kind.COMMENT || t.kind == Kind.NONCODE)
+        continue;
+      ++$;
+    }
+  }
+
+  private static void collect(final CompilationUnit u) {
+    u.accept(new ASTVisitor() {
+      @Override public boolean visit(final MethodDeclaration ¢) {
+        MethodDeclaration $ = duplicate.of(¢);
+        if (Modifier.isAbstract($.getModifiers()))
+          return false;
+        if (¢.isConstructor())
+          return false;
+        $.setJavadoc(null);
+        $.setName($.getAST().newSimpleName("f"));
+        $.setFlags(0);
+        String s = BatchApplicator.fixedPoint($ + "");
+        methods.add(tide.clean(s));
+        return false;
+      }
+    });
+  }
+
+  private static void collect(final File f) {
+    try {
+      collect(FileUtils.read(f));
+    } catch (final IOException e) {
+      System.err.println(e.getMessage());
+    }
+  }
+
+  private static void collect(final String javaCode) {
+    collect((CompilationUnit) makeAST.COMPILATION_UNIT.from(javaCode));
+  }
+
+  private static void collect(final String[] where) {
+    for (final File ¢ : new FilesGenerator(".java").from(where))
+      collect(¢);
+  }
+}
