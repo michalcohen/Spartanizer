e2b68bce6f10d93d728c16c29315ae4879f7a10b
M4tt30rru <matteo.orru@diee.unica.it>
2016-09-20 22:05:45 +0300

    update

--- src/test/java/il/org/spartan/spartanizer/wrings/Issue239Test.java
+++ src/test/java/il/org/spartan/spartanizer/wrings/Issue239Test.java
@@ -13,6 +13,16 @@ public class Issue239Test {
         "final Integer i1 = Integer.valueOf(12345);\n" +
         "final Integer i2 = Integer.valueOf(12345);\n" +
         "assertEqualsAndHashCodeContract(i1, i2, testTransients);\n" +
-    "}");
+    "}").gives("private void testInteger(final boolean testTransients) {\n" +
+         "assertEqualsAndHashCodeContract(Integer.valueOf(12345), Integer.valueOf(12345), testTransients);\n" +
+    "}").stays();
+  }
+  
+  @Test public void issue239_02() {
+    trimmingOf("private void testInteger(final boolean testTransients) {\n" +
+        "final Integer i1 = Integer.valueOf(12345);\n" +
+        "final Integer i2 = Integer.valueOf(12345);\n" +
+        "assertEqualsAndHashCodeContract(i1, i2, testTransients);\n" +
+    "}").stays();
   }
 }
