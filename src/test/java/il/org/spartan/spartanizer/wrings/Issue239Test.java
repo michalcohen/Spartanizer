7168e6c2b8c02527032b2e06fef82d8dc1da5f78
Yossi Gil <yossi.gil@gmail.com>
2016-09-21 14:21:57 +0300

    Alex/Done: take a look issue#239

--- src/test/java/il/org/spartan/spartanizer/wrings/Issue239Test.java
+++ src/test/java/il/org/spartan/spartanizer/wrings/Issue239Test.java
@@ -4,10 +4,9 @@ import static il.org.spartan.spartanizer.wrings.TrimmerTestsUtils.*;
 
 import org.junit.*;
 
-/**  
-* @author TODO: ALex? Dan?
+/** @author TODO: ALex? Dan?
  * @year 2016 */
-public class Issue239Test {
+@SuppressWarnings("static-method") public class Issue239Test {
   @Test public void a$01() {
     trimmingOf("private void testInteger(final boolean testTransients) {\n" + "final Integer i1 = Integer.valueOf(12345);\n"
         + "final Integer i2 = Integer.valueOf(12345);\n" + "assertEqualsAndHashCodeContract(i1, i2, testTransients);\n" + "}")
