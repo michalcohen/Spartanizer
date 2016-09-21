f0c281d5b444b51df61a88ea2d02e576eb3c573a
Yossi Gil <yossi.gil@gmail.com>
2016-09-21 14:05:14 +0300

    Renaming

--- src/main/java/il/org/spartan/spartanizer/dispatch/Toolbox.java
+++ src/main/java/il/org/spartan/spartanizer/dispatch/Toolbox.java
@@ -76,7 +76,7 @@ public class Toolbox {
               new InfixAdditionSubtractionExpand(), //
               new InfixEmptyStringAdditionToString(), //
               new InfixConcatenationEmptyStringLeft(), //
-              new InfixFactorNegatives(), //
+              new InfixFactorNegative(), //
               new InfixAdditionEvaluate(), //
               // new ConcatStrings(), //removed for now so it won't break tests,
               // see issue #120
