b121808406f502d9cf4fc26540fab9b5c8603f61
Yossi Gil <yossi.gil@gmail.com>
2016-09-22 16:08:19 +0300

    Initial tests for #231

---
+++ src/test/java/il/org/spartan/spartanizer/wrings/Issue231.java
@@ -0,0 +1,42 @@
+package il.org.spartan.spartanizer.wrings;
+
+import static il.org.spartan.spartanizer.wrings.TrimmerTestsUtils.*;
+
+import org.junit.*;
+import org.junit.runners.*;
+
+/** @author Yossi Gil
+ * @since 2016 */
+@FixMethodOrder(MethodSorters.NAME_ASCENDING) //
+@SuppressWarnings({ "static-method", "javadoc" }) //
+public class Issue231 {
+  @Test public void chocolate1() {
+    trimmingOf("a ? x.f(b) : y.f(b)")//
+        .gives("(a?x:y).f(b)");
+  }
+
+  @Test public void chocolate2() {
+    trimmingOf("a ? myClass.f(b) : yourClass.f(b)")//
+        .gives("(a?myClass:yourClass).f(b)") //
+        .stays();
+
+  }
+
+  @Test public void vanilla1() {
+    trimmingOf("a ? Class.f(b) : y.f(b)")//
+        .stays();
+  }
+
+  @Test public void vanilla2() {
+    trimmingOf("a ? MyClass.f(b) : y.f(b)")//
+        .stays();
+  }
+  @Test public void vanilla3() {
+    trimmingOf("a ? MyClass.f(b) : YourClass.f(b)")//
+        .stays();
+  }
+  @Test public void vanilla4() {
+    trimmingOf("a ? x.f(b) : YourClass.f(b)")//
+        .stays();
+  }
+}
