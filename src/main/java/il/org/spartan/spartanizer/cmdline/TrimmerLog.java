f3a589e972ada762af79aa217a867cb05a28ff7a
M4tt30rru <matteo.orru@diee.unica.it>
2016-09-22 11:33:50 +0300

    sync

--- src/main/java/il/org/spartan/spartanizer/cmdline/TrimmerLog.java
+++ src/main/java/il/org/spartan/spartanizer/cmdline/TrimmerLog.java
@@ -15,15 +15,22 @@ import il.org.spartan.spartanizer.wringing.*;
  * @author Yossi Gil
  * @year 2016 */
 public class TrimmerLog {
-  
   private static CSVStatistics output;
-  
   private static int maxVisitations = 30;
   private static int maxSuggestions = 20;
   private static int maxApplications = 10;
   private static boolean logToScreen = true; // default output
-  private static boolean logToFile = false;
+  private static boolean logToFile;
   private static String outputDir = "/tmp/trimmerlog-output.CSV";
+  private static String fileName;
+
+  public static void activateLogToFile() {
+    logToFile = true;
+  }
+
+  public static void activateLogToScreen() {
+    logToScreen = true;
+  }
 
   public static void application(final ASTRewrite r, final Suggestion s) {
     if (--maxApplications <= 0) {
@@ -37,25 +44,51 @@ public class TrimmerLog {
     System.out.println("       After: " + r);
   }
 
-  private static CSVStatistics init() {
-    try {
-      output = new CSVStatistics(outputDir, "Suggestions");
-    } catch (IOException e) {
-      e.printStackTrace();
-    }
-    return null;
+  public static void fileProperties() {
+    // TODO Auto-generated method stub
+  }
+
+  public static int getMaxApplications() {
+    return maxApplications;
+  }
+
+  public static int getMaxSuggestions() {
+    return maxSuggestions;
+  }
+
+  public static int getMaxVisitations() {
+    return maxVisitations;
+  }
+
+  public static void setFileName(final String $) {
+    fileName = $;
+  }
+
+  public static void setMaxApplications(final int maxApplications) {
+    TrimmerLog.maxApplications = maxApplications;
+  }
+
+  public static void setMaxSuggestions(final int maxSuggestions) {
+    TrimmerLog.maxSuggestions = maxSuggestions;
+  }
+
+  public static void setMaxVisitations(final int maxVisitations) {
+    TrimmerLog.maxVisitations = maxVisitations;
+  }
+
+  public static void setOutputDir(final String $) {
+    TrimmerLog.outputDir = $;
   }
 
   public static <N extends ASTNode> void suggestion(final Wring<N> w, final N n) {
-    
     if (--maxSuggestions <= 0) {
       if (maxSuggestions == 0)
         System.out.println("Stopped logging suggestions");
       return;
     }
-  
     if (logToFile) {
       init();
+      output.put("File", fileName);
       output.put("Wring", clazz(w));
       output.put("Named", w.description());
       output.put("Kind", w.wringGroup());
@@ -64,23 +97,15 @@ public class TrimmerLog {
       output.put("Suggests", w.suggest(n));
       output.nl();
     }
-    
-    if (logToScreen) {
-        System.out.println("       Wring: " + clazz(w));
-        System.out.println("       Named: " + w.description());
-        System.out.println("        Kind: " + w.wringGroup());
-        System.out.println("   Described: " + w.description(n));
-        System.out.println(" Can suggest: " + w.canSuggest(n));
-        System.out.println("    Suggests: " + w.suggest(n));
-    }
-  }
-  
-  public static void activateLogToScreen(){
-    logToScreen = true;
-  }
-  
-  public static void activateLogToFile(){
-    logToFile = true;
+    if (!logToScreen || !logToScreen)
+      return;
+    System.out.println("        File: " + fileName);
+    System.out.println("       Wring: " + clazz(w));
+    System.out.println("       Named: " + w.description());
+    System.out.println("        Kind: " + w.wringGroup());
+    System.out.println("   Described: " + w.description(n));
+    System.out.println(" Can suggest: " + w.canSuggest(n));
+    System.out.println("    Suggests: " + w.suggest(n));
   }
 
   public static void visitation(final ASTNode ¢) {
@@ -95,18 +120,13 @@ public class TrimmerLog {
   private static String clazz(final Object n) {
     return n.getClass().getSimpleName();
   }
-  
-  public static void setOutputDir(String $){
-    TrimmerLog.outputDir = $;
-  }
 
-  public static void setMaxVisitations(int maxVisitations) {
-    TrimmerLog.maxVisitations = maxVisitations;
+  private static CSVStatistics init() {
+    try {
+      output = new CSVStatistics(outputDir, "Suggestions");
+    } catch (final IOException e) {
+      e.printStackTrace();
+    }
+    return null;
   }
-
-  public static void setMaxSuggestions(int maxSuggestions) {
-    TrimmerLog.maxSuggestions = maxSuggestions;
-  }
-  
-  
 }
