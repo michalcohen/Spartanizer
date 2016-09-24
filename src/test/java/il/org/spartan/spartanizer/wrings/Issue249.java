a8e8d0531f0019502114f5ab52bfd32997f15384
Yossi Gil <yossi.gil@gmail.com>
2016-09-24 20:44:26 +0300

    Compile time errors??? Sure. This is how you start your design

--- src/test/java/il/org/spartan/spartanizer/wrings/Issue249.java
+++ src/test/java/il/org/spartan/spartanizer/wrings/Issue249.java
@@ -5,9 +5,15 @@ import static il.org.spartan.spartanizer.wrings.TrimmerTestsUtils.*;
 import org.junit.*;
 import org.junit.runners.*;
 
+import il.org.spartan.*;
+import il.org.spartan.spartanizer.ast.*;
+
 /** @author Yossi Gil
  * @since 2016 */
 @FixMethodOrder(MethodSorters.NAME_ASCENDING) //
 @SuppressWarnings({ "static-method", "javadoc" }) //
 public class Issue249 {
+  @Test public void a00() {
+    azzert.that(metrics.horizontalComplexity(null), is(0));
+  }
 }
