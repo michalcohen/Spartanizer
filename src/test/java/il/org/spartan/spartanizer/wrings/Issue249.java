2fdc2e4f20e7c00f573ab0c2f578a8bbdb992b2c
Yossi Gil <yossi.gil@gmail.com>
2016-09-24 22:11:04 +0300

    #249: @Test function zoom in to find we have such a large value

--- src/test/java/il/org/spartan/spartanizer/wrings/Issue249.java
+++ src/test/java/il/org/spartan/spartanizer/wrings/Issue249.java
@@ -3,8 +3,8 @@ package il.org.spartan.spartanizer.wrings;
 import static il.org.spartan.azzert.*;
 import static il.org.spartan.spartanizer.wrings.TrimmerTestsUtils.*;
 
+import org.eclipse.jdt.core.dom.*;
 import org.junit.*;
-import org.junit.runners.*;
 
 import il.org.spartan.*;
 import il.org.spartan.spartanizer.ast.*;
@@ -12,7 +12,7 @@ import il.org.spartan.spartanizer.engine.*;
 
 /** @author Yossi Gil
  * @since 2016 */
-@FixMethodOrder(MethodSorters.NAME_ASCENDING) //
+// @FixMethodOrder(MethodSorters.NAME_ASCENDING) //
 @SuppressWarnings({ "static-method", "javadoc" }) //
 public class Issue249 {
   @Test public void a00() {
@@ -37,6 +37,15 @@ public class Issue249 {
 
   @Test public void a05() {
     azzert.that(metricUnderTest("{{}}"), is(4));
+  }
+  @Test public void a06() {
+    final Statement s = into.s("{}");
+    assert s != null;
+    azzert.that(s, instanceOf(Block.class));
+    final Block b = az.block(s);
+    assert b != null;
+    assert step.statements(b) != null; 
+    azzert.that(step.statements(b).size(), is(0));
   }
 
   public int metricUnderTest(String javaStatements) {
