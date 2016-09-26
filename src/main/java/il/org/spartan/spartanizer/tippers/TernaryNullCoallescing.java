5a2b2733fc5da21f357e910109acde5f4bfa0a1d
Yossi Gil <yossi.gil@gmail.com>
2016-09-26 15:16:59 +0300

    Take ori's version

--- src/main/java/il/org/spartan/spartanizer/tippers/TernaryNullCoallescing.java
+++ src/main/java/il/org/spartan/spartanizer/tippers/TernaryNullCoallescing.java
@@ -9,33 +9,17 @@ import il.org.spartan.spartanizer.dispatch.*;
 import il.org.spartan.spartanizer.tipping.*;
 import il.org.spartan.spartanizer.utils.*;
 
-/** Replace X != null ? X : Y with X ?? Y <br>
- * replace X == null ? Y : X with X ?? Y <br>
- * replace null == X ? Y : X with X ?? Y <br>
- * replace null != X ? X : Y with X ?? Y <br>
+/** Replace (X != null) ? X : Y with X ?? Y <br>
+ * replace (X == null) ? Y : X with X ?? Y <br>
+ * replace (null == X) ? Y : X with X ?? Y <br>
+ * replace (null != X) ? X : Y with X ?? Y <br>
  * @author Ori Marcovitch
  * @year 2016 */
 public final class TernaryNullCoallescing extends ReplaceCurrentNode<ConditionalExpression> implements Kind.CommnoFactoring {
-<<<<<<< HEAD
   private static ASTNode replacement(final Expression left, final Expression right, final Expression elze) {
     if (!iz.nullLiteral(left) && iz.nullLiteral(right) && wizard.same(left, elze))
       Counter.count(TernaryNullCoallescing.class);
     if (iz.nullLiteral(left) && !iz.nullLiteral(right) && wizard.same(right, elze))
-=======
-  @Override public ASTNode replacement(ConditionalExpression e) {
-    if (!iz.comparison(az.infixExpression(step.expression(e))))
-      return null;
-    InfixExpression condition = az.comparison((step.expression(e)));
-    Expression left = step.left(condition);
-    Expression right = step.right(condition);
-    return step.operator(condition) == EQUALS ? replacement(left, right, step.elze(e))
-        : step.operator(condition) == NOT_EQUALS ? replacement(left, right, step.then(e)) : null;
-  }
-
-  private static ASTNode replacement(Expression left, Expression right, Expression elze) {
-    if ((!iz.nullLiteral(left) && iz.nullLiteral(right) && wizard.same(left, elze))
-        || (iz.nullLiteral(left) && !iz.nullLiteral(right) && wizard.same(right, elze)))
->>>>>>> 3a128f4f03a0ce52ab12a7cef45b01f8c7fd7939
       Counter.count(TernaryNullCoallescing.class);
     return null;
   }
