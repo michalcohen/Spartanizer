f26ab67c9a09dafb885419f7744cbaa740bef113
dormaayn <dor.d.ma@gmail.com>
2016-09-27 18:37:02 +0300

    Implementation of If Complete(Issue #251)

--- src/main/java/il/org/spartan/spartanizer/tippers/RemoveRedundentIf.java
+++ src/main/java/il/org/spartan/spartanizer/tippers/RemoveRedundentIf.java
@@ -11,7 +11,7 @@ import il.org.spartan.spartanizer.java.*;
 import il.org.spartan.spartanizer.tipping.*;
 
 /**
- * Remove if that has no impact on the code if and only if </br>
+ * Simplify if statements as much as possible (or remove them or parts of them) if and only if </br>
  * it doesn't have any side-effect.
  * 
  * @author Dor Ma'ayan
@@ -21,17 +21,27 @@ import il.org.spartan.spartanizer.tipping.*;
 public class RemoveRedundentIf extends ReplaceCurrentNode<IfStatement> implements Kind.Collapse{
 
   
+  @SuppressWarnings("all") private boolean checkVariableDecleration(VariableDeclarationStatement n ){    
+    List<VariableDeclarationFragment> lst = n.fragments();
+    for(VariableDeclarationFragment f : lst){
+      if(f.getInitializer()!=null && !sideEffects.free(f.getInitializer()))
+        return false;
+    }
+    return true;
+  }
   private boolean checkBlock(ASTNode n){
     if(n!= null &&
         (iz.expression(n) && !sideEffects.free(az.expression(n))
-        || iz.expressionStatement(n) && !sideEffects.free(az.expressionStatement(n).getExpression())) || !iz.block(n)&&!iz.variableDeclarationStatement(n))
+        || iz.expressionStatement(n) && !sideEffects.free(az.expressionStatement(n).getExpression())) //
+        || !iz.block(n) && !iz.isVariableDeclarationStatement(n) //
+        ||(iz.variableDeclarationStatement(n) && !checkVariableDecleration(az.variableDeclrationStatement(n))))
       return false;
     if (iz.block(n)) {
       List<Statement> lst = az.block(n).statements();
       for (Statement s : lst) {
         if (iz.expressionStatement(s) && !sideEffects.free(az.expression(az.expressionStatement(s).getExpression())))
             return false;
-        if(!iz.variableDeclarationStatement(s))
+        if(!iz.isVariableDeclarationStatement(s) || iz.variableDeclarationStatement(s) && !checkVariableDecleration(az.variableDeclrationStatement(s)))
           return false;
       }
     }
