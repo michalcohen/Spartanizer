9318695628f97b76b18237917ec7c64967642386
Yossi Gil <yossi.gil@gmail.com>
2016-09-27 21:22:41 +0300

    Test for #295

---
+++ src/test/java/il/org/spartan/spartanizer/tippers/Issue295.java
@@ -0,0 +1,17 @@
+package il.org.spartan.spartanizer.tippers;
+
+import static il.org.spartan.spartanizer.tippers.TrimmerTestsUtils.*;
+
+import org.junit.*;
+import org.junit.runners.*;
+
+/** @author Yossi Gil
+ * @since 2016 */
+@FixMethodOrder(MethodSorters.NAME_ASCENDING) @SuppressWarnings({ "static-method", "javadoc" }) public class Issue295 {
+  /** Correct way of trimming does not change */
+  @Test public void a() {
+    trimmingOf("A a = new A();for (A b: as)sum+=b;")//
+    .gives("for (A b: new A())sum+=b;") //
+    .stays()
+  }
+}
