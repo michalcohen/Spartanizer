09fc654cf3674e7b9e089a23c30df56e6d2e5dc7
Yossi Gil <yossi.gil@gmail.com>
2016-09-27 21:30:54 +0300

    Polish the class under test for #295 and tie the issue to CUT

--- src/test/java/il/org/spartan/spartanizer/tippers/Issue295.java
+++ src/test/java/il/org/spartan/spartanizer/tippers/Issue295.java
@@ -5,13 +5,15 @@ import static il.org.spartan.spartanizer.tippers.TrimmerTestsUtils.*;
 import org.junit.*;
 import org.junit.runners.*;
 
-/** @author Yossi Gil
+/** Unit test for {@link DeclarationInitializerStatementTerminatingScope} for
+ * the case of inlining into the expression of an enhanced for
+ * @author Yossi Gil
  * @since 2016 */
 @FixMethodOrder(MethodSorters.NAME_ASCENDING) @SuppressWarnings({ "static-method", "javadoc" }) public class Issue295 {
   /** Correct way of trimming does not change */
   @Test public void a() {
     trimmingOf("A a = new A();for (A b: as)sum+=b;")//
-    .gives("for (A b: new A())sum+=b;") //
-    .stays();
+        .gives("for (A b: new A())sum+=b;") //
+        .stays();
   }
 }
