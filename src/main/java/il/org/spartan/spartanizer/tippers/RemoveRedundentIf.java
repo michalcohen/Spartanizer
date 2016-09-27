863ccc069d8176337cd90c4e5ec7e293a78af6e7
dormaayn <dor.d.ma@gmail.com>
2016-09-27 15:53:39 +0300

    Implementation of If, no nesting yet(Issue #251)

--- src/main/java/il/org/spartan/spartanizer/tippers/RemoveRedundentIf.java
+++ src/main/java/il/org/spartan/spartanizer/tippers/RemoveRedundentIf.java
@@ -4,6 +4,7 @@ import java.util.*;
 
 import org.eclipse.jdt.core.dom.*;
 
+import il.org.spartan.spartanizer.assemble.*;
 import il.org.spartan.spartanizer.ast.*;
 import il.org.spartan.spartanizer.dispatch.*;
 import il.org.spartan.spartanizer.java.*;
@@ -20,21 +21,46 @@ import il.org.spartan.spartanizer.tipping.*;
 public class RemoveRedundentIf extends ReplaceCurrentNode<IfStatement> implements Kind.Collapse{
 
   @Override public ASTNode replacement(IfStatement n) {
+    boolean condition = true;
+    boolean then = true;
+    boolean elze = true;
     if(n==null)
       return null;
-    if(!sideEffects.free(n.getExpression()))
-      return null;
-   if(n.getThenStatement()!=null && iz.block(n.getThenStatement())){
-     List<Statement> lst = az.block(n.getThenStatement()).statements();
-     for(Statement s :lst){
-       if(az.expressionStatement(s)!=null){
-         if(!sideEffects.free(az.expression(az.expressionStatement(s).getExpression())))
-         return null;
-     }
-   }
+    if (!sideEffects.free(n.getExpression()))
+      condition=false;
+    if(n.getThenStatement() != null &&
+        (iz.expression(n.getThenStatement()) && !sideEffects.free(az.expression(n.getThenStatement()))
+        || iz.expressionStatement(n.getThenStatement()) && !sideEffects.free(az.expressionStatement(n.getThenStatement()).getExpression())))
+      then=false;
+    if(n.getElseStatement() != null &&
+        (iz.expression(n.getElseStatement()) && !sideEffects.free(az.expression(n.getElseStatement()))
+        || iz.expressionStatement(n.getElseStatement()) && !sideEffects.free(az.expressionStatement(n.getElseStatement()).getExpression())))
+      elze=false;
+    if (iz.block(n.getThenStatement())) {
+      List<Statement> lst = az.block(n.getThenStatement()).statements();
+      for (Statement s : lst) {
+        if (az.expressionStatement(s) != null) {
+          if (!sideEffects.free(az.expression(az.expressionStatement(s).getExpression())))
+            then=false;
+        }
+      }
+    }
+    if (iz.block(n.getElseStatement())) {
+      List<Statement> lst = az.block(n.getElseStatement()).statements();
+      for (Statement s : lst) {
+        if (az.expressionStatement(s) != null) {
+          if (!sideEffects.free(az.expression(az.expressionStatement(s).getExpression())))
+          elze=false;
+        }
+      }
+    }
+    if(condition && then && elze || (condition && then && n.getElseStatement()==null))
+      return n.getAST().newBlock();
+    if(condition && then && !elze && (n.getElseStatement()!=null)){
+      return subject.pair(duplicate.of(n.getElseStatement()),null).toNot(duplicate.of(n.getExpression()));
+    }
+    return null;
   }
-   return n.getAST().newBlock();
- }
 
   @Override public String description(IfStatement n) {
     return "remove :" + n;
--- src/test/java/il/org/spartan/spartanizer/tippers/Issue251.java
+++ src/test/java/il/org/spartan/spartanizer/tippers/Issue251.java
@@ -10,19 +10,36 @@ import static il.org.spartan.spartanizer.tippers.TrimmerTestsUtils.*;
 
 @SuppressWarnings({ "static-method", "javadoc" }) public class Issue251 {
   
-  @Test public void t10() {
-    trimmingOf("if(b){int i;}").gives("{}").gives("").stays();
+  @Test public void t01() {
+    trimmingOf("if(b==true){int i;}").gives("{}").gives("").stays();
   }
   
-  @Test public void t20() {
+  @Test public void t02() {
     trimmingOf("if(b){int i;int b; b=i+1;g();}").gives("if(!b)return;int i;int b=i+1;b=i+1;g();");
   }
   
-  @Test public void t30() {
+  @Test public void t03() {
     trimmingOf("if(b){int i;int j;int k;}").gives("{}").gives("").stays();
   }
   
-  @Test public void t40() {
+  @Test public void t04() {
     trimmingOf("if(b){int i;int j;}else{int t;}").gives("{}").gives("").stays();
   }
+  
+  @Test public void t05() {
+    trimmingOf("if(b)g();").stays();
+  }
+  
+  @Test public void t06() {
+    trimmingOf("if(b()){int i;}").gives("if(b())int i;");
+  }
+  
+  @Test public void t07() {
+    trimmingOf("if(b){int i;int j;}else{g();}").gives("if(!b)g();").stays();
+  }
+  
+  @Test public void t08() {
+    trimmingOf("if(b){int i;int j;}else{g();int t;}").gives("if(!b){g();int t;}");
+  }
+  
  }
