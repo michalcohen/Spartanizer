42ac59fa4b03febb20e75d724dfe9d01528b8fdb
Yossi Gil <yossi.gil@gmail.com>
2016-09-28 21:20:59 +0300

    #280

--- src/main/java/il/org/spartan/spartanizer/dispatch/Nominal.java
+++ src/main/java/il/org/spartan/spartanizer/dispatch/Nominal.java
@@ -1,8 +1,13 @@
 package il.org.spartan.spartanizer.dispatch;
 
-/**
- * @author Yossi Gil 
+/** The {@link TipperCategory} of renaming, and renaming related
+ * {@link Tipper}s.
+ * @author Yossi Gil
  * @year 2016 */
 public interface Nominal extends TipperCategory {
+  final String label = "Nominal";
 
+  @Override default String description() {
+    return label;
+  }
 }
