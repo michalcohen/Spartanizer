43bcceb7a18237da63cc170a04e280cc87ec99d3
Yossi Gil <yossi.gil@gmail.com>
2016-09-29 00:17:09 +0300

    Added .stays() clauses to all tests

--- src/test/java/il/org/spartan/spartanizer/tippers/Issue111.java
+++ src/test/java/il/org/spartan/spartanizer/tippers/Issue111.java
@@ -10,70 +10,86 @@ import org.junit.runners.*;
 @Ignore @FixMethodOrder(MethodSorters.NAME_ASCENDING) @SuppressWarnings({ "static-method", "javadoc" }) public class Issue111 {
   @Test public void A$a_1() {
     trimmingOf("public final class A {" + "static public int a;" + "}")//
-        .gives("public final class A {" + "public static int a;" + "}");
+        .gives("public final class A {" + "public static int a;" + "}")//
+        .stays();
   }
 
   @Test public void A$b_1() {
     trimmingOf("public final class A {" + "static final public int a;" + "}")//
-        .gives("public final class A {" + "public static final int a;" + "}");
+        .gives("public final class A {" + "public static final int a;" + "}")//
+        .stays();
   }
 
   @Test public void A$c() {
-    trimmingOf("protected public void func();").gives("public protected void func();");
+    trimmingOf("protected public void func();")//
+        .gives("public protected void func();")//
+        .stays();
   }
 
   @Test public void A$c_2() {
     trimmingOf("public final class A{" + "synchronized public void fun(final int a) {}" + "final private String s = \"Alex\";" + "}")
-        .gives("public final class A{" + "public synchronized void fun(final int a) {}" + "private final String s = \"Alex\";" + "}").stays();
+        .gives("public final class A{" + "public synchronized void fun(final int a) {}" + "private final String s = \"Alex\";" + "}")//
+        .stays();
   }
 
   public void A$d() {
     trimmingOf("protected public final class A{}")//
-        .gives("public protected class A{}");
+        .gives("public protected class A{}")//
+        .stays();
   }
 
   @Test public void A$d_1() {
-    trimmingOf("abstract class A {}").stays();
+    trimmingOf("abstract class A {}")//
+        .stays();
   }
 
   @Test public void A$e() {
     trimmingOf("protected public final class A{volatile static int a;}")//
-        .gives("public protected final class A{volatile static int a;}").gives("public protected final class A{static volatile int a;}").stays();
+        .gives("public protected final class A{volatile static int a;}")//
+        .gives("public protected final class A{static volatile int a;}")//
+        .stays();
   }
 
   @Test public void A$g() {
-    trimmingOf("protected public final public enum Level { HIGH, MEDIUM, LOW}").gives("protected public public enum Level { " + "HIGH, MEDIUM, LOW}")
+    trimmingOf("protected public final public enum Level { HIGH, MEDIUM, LOW}")//
+        .gives("protected public public enum Level { " + "HIGH, MEDIUM, LOW}")
         .gives("public protected enum Level {HIGH, MEDIUM, LOW}")//
         .stays();
   }
 
   public void A$h() {
     trimmingOf("protected public int a;")//
-        .gives("public protected int a;");
+        .gives("public protected int a;")//
+        .stays();
   }
 
   public void A$i() {
     trimmingOf("protected public int a;")//
-        .gives("public protected int a;");
+        .gives("public protected int a;")//
+        .stays();
   }
 
   public void A$q() {
     trimmingOf("protected public int a;")//
-        .gives("public protected int a;");
+        .gives("public protected int a;")//
+        .stays();
   }
 
   public void A$w() {
     trimmingOf("protected public int a;")//
-        .gives("public protected int a;");
+        .gives("public protected int a;")//
+        .stays();
   }
 
   public void A$y() {
     trimmingOf("synchronized volatile public int a;")//
-        .gives("public volatile synchronized int a;");
+        .gives("public volatile synchronized int a;")//
+        .stays();
   }
 
   public void A$z() {
     trimmingOf("volatile private int a;")//
-        .gives("private volatile int a;");
+        .gives("private volatile int a;")//
+        .stays();
   }
 }
