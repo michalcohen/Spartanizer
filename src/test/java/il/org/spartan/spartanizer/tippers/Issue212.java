0502f425a53b7510e9bd2a6374102ec835cc765b
Yossi Gil <yossi.gil@gmail.com>
2016-09-30 18:24:47 +0300

    Clean one failing test

--- src/test/java/il/org/spartan/spartanizer/tippers/Issue212.java
+++ src/test/java/il/org/spartan/spartanizer/tippers/Issue212.java
@@ -10,20 +10,18 @@ import org.junit.runners.*;
  * @since 2016 */
 @FixMethodOrder(MethodSorters.NAME_ASCENDING) @SuppressWarnings({ "static-method", "javadoc" }) public final class Issue212 {
   @Test @Ignore("Pending Issue") public void chocolate1() {
-    trimmingOf("for(int $=0;$<a.length;++$)sum +=$;").gives("for(int $=0;$<a.length;++$,sum +=$);").stays();
+    trimmingOf("for(int $=0;$<a.length;++$)sum +=$;")//
+        .gives("for(int $=0;$<a.length;++$,sum +=$);").stays();
   }
 
   @Test @Ignore("Pending Issue") public void chocolate2() {
-    trimmingOf("for(int i=0, j=0;i<a.length;++j)sum +=i+j;").gives("for(int i=0, j=0;i<a.length;++j,sum +=i+j);").stays();
+    trimmingOf("for(int i=0, j=0;i<a.length;++j)sum +=i+j;")//
+        .gives("for(int i=0, j=0;i<a.length;++j,sum +=i+j);").stays();
   }
 
   @Test public void vanilla01() {
-    trimmingOf("for(int i=0;i<a.length;++i)sum+=i;")//
-        .gives("for(int ¢=0;¢<a.length;++¢)sum+=¢;").stays();
-  }
-
-  @Test public void vanilla02() {
-    trimmingOf(" for (int i = 2; i < xs.size(); ++i)\n" + "    step.extendedOperands($).add(duplicate.of(xs.get(i)));")
-        .gives(" for (int ¢ = 2; ¢ < xs.size(); ++¢)\n" + "    step.extendedOperands($).add(duplicate.of(xs.get(¢)));").stays();
+    trimmingOf("for(int i=0;i<a.length;++i)if(a)sum+=i;")//
+        .gives("for(int ¢=0;¢<a.length;++¢)if(a)sum+=¢;")//
+        .stays();
   }
 }
