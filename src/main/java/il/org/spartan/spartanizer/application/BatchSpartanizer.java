b681dbc4edffaf8c31432b9c0bc99d9ea7b90f0d
M4tt30rru <matteo.orru@diee.unica.it>
2016-09-30 22:32:26 +0300

    update

--- src/main/java/il/org/spartan/spartanizer/application/BatchSpartanizer.java
+++ src/main/java/il/org/spartan/spartanizer/application/BatchSpartanizer.java
@@ -355,18 +355,6 @@ public final class BatchSpartanizer {
     System.out.println("  -d       default directory: use the current directory for the analysis");
     System.out.println("  -o       output directory: here go the results of the analysis");
     System.out.println("  -i       input directory: place here the projects that you want to analyze.");
-//    System.out.println("  -N       Do not overwrite existing files (writes the Spartanized output to a new file in the same directory)");
-//    System.out.println("  -C<num>  Maximum number of Spartanizaion rounds for each file (default: 20)");
-//    System.out.println("  -E       Display statistics for each file separately");
-//    System.out.println("  -V       Be verbose");
-//    System.out.println("  -L       printout logs");
-//    System.out.println("");
-//    System.out.println("Print statistics:");
-//    System.out.println("  -l       Show the number of lines before and after spartanization");
-//    System.out.println("  -r       Show the number of Spartanizaion made in each round");
-//    System.out.println("");
-//    System.out.println("Output:");
-//    System.out.println("  -logPath Output dir for logs");
     System.out.println("");
   }
 }
