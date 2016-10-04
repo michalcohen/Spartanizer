80be28b7be44e49d7c7e8b99468cdf8a7e4e5f00
orimarco <marcovitch.ori@gmail.com>
2016-10-05 00:24:19 +0300

    empty method

--- src/test/java/il/org/spartan/spartanizer/tippers/InfixIndexOfToStringContainsTest.java
+++ src/test/java/il/org/spartan/spartanizer/tippers/InfixIndexOfToStringContainsTest.java
@@ -9,11 +9,11 @@ public class InfixIndexOfToStringContainsTest {
   @SuppressWarnings("static-method") @Test public void testMutation1() {
     trimmingOf("if(str.indexOf(\"stringy\") >= 0) return true;").gives("if(str.contains(\"stringy\")) return true;").stays();
   }
-  
+
   @SuppressWarnings("static-method") @Test public void testMutation2() {
     trimmingOf("if(str.indexOf(stringy) >= 0) return true;").gives("if(str.contains(stringy)) return true;").stays();
   }
-  
+
   @SuppressWarnings("static-method") @Test public void testMutation0() {
     trimmingOf("str.indexOf(stringy) >= 0").gives("str.contains(stringy)").stays();
   }
