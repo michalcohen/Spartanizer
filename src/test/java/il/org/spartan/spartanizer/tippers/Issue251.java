5d8af763816d0f6c387c506b0e07ccabe7299261
Yossi Gil <yossi.gil@gmail.com>
2016-10-04 15:47:31 +0300

    #359: test class

--- src/test/java/il/org/spartan/spartanizer/tippers/Issue251.java
+++ src/test/java/il/org/spartan/spartanizer/tippers/Issue251.java
@@ -8,57 +8,75 @@ import org.junit.*;
  * @since 2016-09-26 */
 @SuppressWarnings({ "static-method", "javadoc" }) public class Issue251 {
   @Test public void Issue302_test() {
-    trimmingOf("if(b()){int i;}").stays();
+    trimmingOf("if(b()){int i;}")//
+        .stays() //
+    ;
   }
 
   @Test public void t01() {
-    trimmingOf("if(b==true){int i;}").gives("{}").gives("").stays();
+    trimmingOf("if(b==true){int i;}")//
+        .gives("{}")//
+        .gives("")//
+        .stays();
   }
 
   @Test public void t02() {
-    trimmingOf("if(b){int i;int b; b=i+1;g();}").gives("if(!b)return;int i;int b=i+1;b=i+1;g();");
+    trimmingOf("if(b){int i;int b; b=i+1;g();}")//
+        .gives("if(!b)return;int i;int b=i+1;b=i+1;g();");
   }
 
   @Test public void t03() {
-    trimmingOf("if(b){int i;int j;int k;}").gives("{}").gives("").stays();
+    trimmingOf("if(b){int i;int j;int k;}")//
+        .gives("{}")//
+        .gives("")//
+        .stays();
   }
 
   @Test public void t04() {
-    trimmingOf("if(b){int i;int j;}else{int tipper;}").gives("{}").gives("").stays();
+    trimmingOf("if(b){int i;int j;}else{int tipper;}")//
+        .gives("{}")//
+        .gives("")//
+        .stays();
   }
 
   @Test public void t05() {
-    trimmingOf("if(b)g();").stays();
+    trimmingOf("if(b)g();")//
+        .stays() //
+    ;
   }
 
   @Test public void t06() {
-    trimmingOf("if(b()){int i;}").stays();
+    trimmingOf("if(b()){int i;}")//
+        .stays() //
+    ;
   }
 
   @Test public void t07() {
-    trimmingOf("if(b){int i;int j;}else{g();}").gives("if(!b)g();").stays();
-  }
-
-  @Test public void t08() {
-    trimmingOf("if(b){int i;int j;}else{g();int tipper;}").gives("if(!b){g();int tipper;}");
-  }
-
-  @Test public void t09() {
-    trimmingOf("if(b){int i;int j;g();}else{int q;int tipper;}")//
-        .gives("if(!b){int q;int tipper;}else{int i;int j;g();}")//
-        .gives("if(b){int i;int j;g();}");
+    trimmingOf("if(b){int i;int j;}else{g();}")//
+        .gives("if(!b)g();")//
+        .stays();
   }
 
   @Test public void t10() {
-    trimmingOf("if(b==true){int i=5;}").gives("{}").gives("").stays();
+    trimmingOf("if(b==true){int i=5;}")//
+        .gives("{}")//
+        .gives("")//
+        .stays() //
+    ;
   }
 
   @Test public void t11() {
-    trimmingOf("if(b==true){int i=g();}").gives("if(b){int i=g();}").stays();
+    trimmingOf("if(b==true){int i=g();}")//
+        .gives("if(b){int i=g();}")//
+        .stays() //
+    ;
   }
 
   @Test public void t12() {
-    trimmingOf("if(b==true){int i=5,q=g();}").gives("if(b){int i=5,q=g();}").stays();
+    trimmingOf("if(b==true){int i=5,q=g();}")//
+        .gives("if(b){int i=5,q=g();}")//
+        .stays() //
+    ;
   }
 
   @Test public void t13() {
@@ -72,46 +90,61 @@ import org.junit.*;
   }
 
   @Test public void t15() {
-    trimmingOf("if(b==q()){int i;}").stays();
+    trimmingOf("if(b==q()){int i;}")//
+        .stays() //
+    ;
   }
 
   @Test public void t16() {
-    trimmingOf("while(b==q){int i;}").gives("{}");
+    trimmingOf("while(b==q){int i;}")//
+        .gives("{}");
   }
 
   @Test public void t17() {
-    trimmingOf("while(b==q){if(tipper==q()){int i;}}").gives("while(b==q)if(tipper==q()){int i;}");
+    trimmingOf("while(b==q){if(tipper==q()){int i;}}")//
+        .gives("while(b==q)if(tipper==q()){int i;}")//
+        .stays();
   }
 
-  @Test @Ignore("Pending Issue") public void t18() {
-    trimmingOf("while(b==q){int i;double tipper; x=tipper+i;}").gives("for(;b==q;x=tipper+i){int i;double tipper;}");
-  }
+
 
   @Test public void t19() {
-    trimmingOf("while(b==q){g();if(tipper==q){int i;int j;}}").gives("while(b==q){g();{}}");
+    trimmingOf("while(b==q){g();if(tipper==q){int i;int j;}}")//
+        .gives("while(b==q){g();{}}")//
+        .gives("while(b==q)g();")//
+        .stays();
   }
 
-  @Test public void t20() {
-    trimmingOf("for(;b==q;){int i;}").gives("{}");
-  }
+
 
   @Test public void t21() {
-    trimmingOf("for(i=1;b==q;++i){if(tipper==q()){int i;}}").gives("for(i=1;b==q;++i)if(tipper==q()){int i;}");
+    trimmingOf("for(i=1;b==q;++i){if(tipper==q()){int i;}}")//
+        .gives("for(i=1;b==q;++i)if(tipper==q()){int i;}")//
+        .stays();
   }
 
   @Test public void t22() {
-    trimmingOf("for(;b==q;){g();if(tipper==q){int i;int j;}}").gives("for(;b==q;){g();{}}");
+    trimmingOf("for(;b==q;){g();if(tipper==q){int i;int j;}}")//
+        .gives("for(;b==q;){g();{}}")//
+        .gives("for(;b==q;)g();")//
+        .stays();
   }
 
   @Test public void t23() {
-    trimmingOf("for(i=1;b==q();++i){if(tipper==q()){int i;}}").gives("for(i=1;b==q();++i)if(tipper==q()){int i;}");
+    trimmingOf("for(i=1;b==q();++i){if(tipper==q()){int i;}}")//
+        .gives("for(i=1;b==q();++i)if(tipper==q()){int i;}")//
+        .stays();
   }
 
   @Test public void t24() {
-    trimmingOf("for(i=tipper();b==q;++i){if(tipper==q()){int i;}}").gives("for(i=tipper();b==q;++i)if(tipper==q()){int i;}");
+    trimmingOf("for(i=tipper();b==q;++i){if(tipper==q()){int i;}}")//
+        .gives("for(i=tipper();b==q;++i)if(tipper==q()){int i;}")//
+        .stays();
   }
 
   @Test public void t25() {
-    trimmingOf("for(i=4;b==q;f=i()){if(tipper==q()){int i;}}").gives("for(i=4;b==q;f=i())if(tipper==q()){int i;}");
+    trimmingOf("for(i=4;b==q;f=i()){if(tipper==q()){int i;}}")//
+        .gives("for(i=4;b==q;f=i())if(tipper==q()){int i;}")//
+        .stays();
   }
 }
---
+++ src/test/java/il/org/spartan/spartanizer/tippers/Issue359.java
@@ -0,0 +1,33 @@
+package il.org.spartan.spartanizer.tippers;
+
+import static il.org.spartan.spartanizer.tippers.TrimmerTestsUtils.*;
+
+import org.junit.*;
+import org.junit.runners.*;
+
+/** @author Yossi Gil
+ * @since 2016 */
+@FixMethodOrder(MethodSorters.NAME_ASCENDING) //
+@SuppressWarnings({ "static-method", "javadoc" }) //
+@Ignore public class Issue359 {
+  @Test public void t08() {
+    trimmingOf("if(b){int i;int j;}else{g();int tipper;}")//
+        .gives("if(!b){g();int tipper;}")//
+        .stays() //
+    ;
+  }
+
+  @Test public void t20() {
+    trimmingOf("for(;b==q;){int i;}")//
+        .gives("for(;b==q;);")//
+        .stays();
+  }
+
+  @Test public void t09() {
+    trimmingOf("if(b){int i;int j;g();}else{int q;int tipper;}")//
+        .gives("if(!b){int q;int tipper;}else{int i;int j;g();}")//
+        .gives("if(b){int i;int j;g();}")//
+        .stays() //
+    ;
+  }
+}
