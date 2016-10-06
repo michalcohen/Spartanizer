b9b52f78451885a7b14666919aa9c5db44fb6c6f
Yossi Gil <yossi.gil@gmail.com>
2016-10-06 22:40:11 +0300

    Fix implementation to follow patters of categories

--- src/main/java/il/org/spartan/spartanizer/dispatch/Nanos.java
+++ src/main/java/il/org/spartan/spartanizer/dispatch/Nanos.java
@@ -3,5 +3,9 @@ package il.org.spartan.spartanizer.dispatch;
 /** Auxiliary type: currently unused, will be used
  * when we have categories of nano-patterns */
 public interface Nanos extends TipperCategory {
-  /* Empty intentionally */
+  final String label = "Nano";
+
+  @Override default String description() {
+    return label;
+  }
 }
