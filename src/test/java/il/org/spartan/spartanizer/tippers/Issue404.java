dfac6179d218333bc1e34ad2ef7d90e4ac3c5982
Yossi Gil <yossi.gil@gmail.com>
2016-10-08 02:54:47 +0300

    TDD/Issue #404: Javadoc explaining the Dewey convention for naming @Test methods

--- src/test/java/il/org/spartan/spartanizer/tippers/Issue404.java
+++ src/test/java/il/org/spartan/spartanizer/tippers/Issue404.java
@@ -5,18 +5,52 @@ import static il.org.spartan.spartanizer.tippers.TrimmerTestsUtils.*;
 import org.junit.*;
 import org.junit.runners.*;
 
-/** A test class constructed by TDD for {@link dig.stringLiterals( @author Yossi
- * Gil
+/** A test class constructed by TDD for {@link dig.stringLiterals}
+ * @author Yossi Gil
  * @since 2016 */
 @FixMethodOrder(MethodSorters.NAME_ASCENDING) //
 @SuppressWarnings({ "static-method", "javadoc" }) //
 public class Issue404 {
-  /** Correct way of trimming does not change */
-  @Test public void Z$140() {
-    trimmingOf("a").stays();
-  }
-
-  /** Ensure that there is a type named {@link dig} */
+  /** Ensure that there is a type named {@link dig}
+   * <p>
+   * Meta information: There are no established rules on names of test methods.
+   * This class demonstrates the <b>Dewey<b> notation: A pattern of -of naming
+   * methods as follows (variations are possible):
+   * <ul>
+   * <li><code>a()<code>, <code>b<()code>, <code>c()<code>, ...
+   * <li>and then, when you need to study a failure of <code>w()</code> better,
+   * <code>wa()</code>, <code>wb()</code> <code>wc()</code>, etc.
+   * <li>and then, when you fixed the fault at <code>w()</code>, proceed with
+   * series, <code>x()</code>, <code>y()</code>, etc.
+   * <li>and then, when you reached <code>z()</code>, and more names are needed,
+   * rename the sequence of methods generated so far:
+   *
+   * <pre>
+   a(), b(), c(), ..., w(),  wa(), wb(), wc(), ..., x(), y(), z()
+   * </pre>
+   *
+   * to (say)
+   *
+   * <pre>
+   Aa(), Ab(), Ac(), ..., Aw(),  Awa(), Awb(), Awc(), ..., Ax(), Ay(), Az()
+   * </pre>
+   *
+   * and proceed to generating tests named
+   *
+   * <pre>
+   a(), b(), c(), ..., w(),  wa(), wb(), wc(), ..., x(), y(), z()
+   * </pre>
+   **
+   * <li>and then, when you finish the entire
+   * </ul>
+   * <p>
+   * <b>be sure to use</b>
+   *
+   * <pre>
+  &#64;FixMethodOrder(MethodSorters.NAME_ASCENDING) //
+   * </pre>
+   *
+   * annotation on your test class */
   @Test public void a() {
     dig.class.hashCode();
   }
@@ -44,5 +78,10 @@ public class Issue404 {
 
   @Test public void g() {
     assert dig.stringLiterals(null) != null;
+  }
+
+  /** Correct way of trimming does not change */
+  @Test public void Z$140() {
+    trimmingOf("a").stays();
   }
 }
