d09f90c32a644c9ad2a890935d0d05a9ca13c58a
Yossi Gil <yossi.gil@gmail.com>
2016-10-07 12:24:20 +0300

    TDD/Issue #404: Minimal change to go GREEN

--- src/test/java/il/org/spartan/spartanizer/tippers/dig.java
+++ src/test/java/il/org/spartan/spartanizer/tippers/dig.java
@@ -4,7 +4,7 @@ package il.org.spartan.spartanizer.tippers;
  * @author Yossi Gil
  * @since 2016 */
 public interface dig {
-  static Void stringLiterals(Object object) {
-    return null;
+  static Object stringLiterals(Object object) {
+    return new Object();
   }
 }
