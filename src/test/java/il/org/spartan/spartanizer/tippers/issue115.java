f78b238f9ad8f51cfb36efc9341be417e80faf2c
dngreenst <dngreenst@gmail.com>
2016-10-07 18:40:51 +0300

    #356, #115 - Moved failing @Ignore'd tests to their own test class

---
+++ src/test/java/il/org/spartan/spartanizer/tippers/issue115.java
@@ -0,0 +1,43 @@
+package il.org.spartan.spartanizer.tippers;
+
+import static il.org.spartan.spartanizer.tippers.TrimmerTestsUtils.*;
+
+import org.junit.*;
+
+/**
+ * Test class for issue 115 - XOR parsing bug with trimmingOf testing utility.
+ * @since 2016 */
+@SuppressWarnings("static-method")// 
+@Ignore("Yossi - parsing with spaces, does not parse without. "//
+      + "Space from '=' does not seem to be required") //
+public class issue115 {
+  @Test public void trimmerBugXOR_Notparsing01() {
+    trimmingOf("j=j^k")//
+        .gives("j^=k");
+  }
+  
+  @Test public void trimmerBugXOR_Notparsing02() {
+    trimmingOf("j = j ^ k")//
+        .gives("j^=k");
+  }
+  
+  @Test public void trimmerBugXOR_Notparsing03() {
+    trimmingOf("j = j^ k")//
+        .gives("j ^= k");
+  }
+  
+  @Test public void trimmerBugXOR_Notparsing04() {
+    trimmingOf("j = j ^k")//
+        .gives("j ^= k");
+  }
+
+  @Test public void trimmerBugXOR_Parsing01() {
+    trimmingOf("j = j ^ k")//
+        .gives("j ^= k");
+  }
+  
+  @Test public void trimmerBugXOR_Parsing02() {
+    trimmingOf("j = j ^ k")//
+    .gives("j ^=k");
+  }
+}
