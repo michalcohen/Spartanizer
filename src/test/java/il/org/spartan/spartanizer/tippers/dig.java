b2516601dbb2945629a7e4e010ff9ab07ae0bd7f
Yossi Gil <yossi.gil@gmail.com>
2016-10-07 11:33:26 +0300

    TDD/Issue #404: Eclipse's default implementation of missing method.

--- src/test/java/il/org/spartan/spartanizer/tippers/dig.java
+++ src/test/java/il/org/spartan/spartanizer/tippers/dig.java
@@ -4,4 +4,8 @@ package il.org.spartan.spartanizer.tippers;
  * @author Yossi Gil 
  * @since 2016 */
 public interface dig {
+
+  static void stringLiterals(Object object) {
+    // TODO Auto-generated method stub
+  }
 }
