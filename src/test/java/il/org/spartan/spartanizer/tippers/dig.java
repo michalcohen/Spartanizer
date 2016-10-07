07ddf715203e9ab17f3083faf17863142aebd296
Yossi Gil <yossi.gil@gmail.com>
2016-10-07 12:30:34 +0300

    Minimal change to restabilize

--- src/test/java/il/org/spartan/spartanizer/tippers/dig.java
+++ src/test/java/il/org/spartan/spartanizer/tippers/dig.java
@@ -1,10 +1,12 @@
 package il.org.spartan.spartanizer.tippers;
 
+import java.util.*;
+
 /** ???
  * @author Yossi Gil
  * @since 2016 */
 public interface dig {
   static Object stringLiterals(Object object) {
-    return new Object();
+    return  Collections.EMPTY_LIST;
   }
 }
