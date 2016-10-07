f86289d1deff6d46b7088cf0981d2582a908180b
Yossi Gil <yossi.gil@gmail.com>
2016-10-07 23:01:47 +0300

    TDD/Issue #413: Edit test class a bit giving an Eclipse readable reference to the function being tested. This link will make our class more immune to architectural changes.

--- src/test/java/il/org/spartan/spartanizer/tippers/Issue413.java
+++ src/test/java/il/org/spartan/spartanizer/tippers/Issue413.java
@@ -5,11 +5,13 @@ import static il.org.spartan.spartanizer.tippers.TrimmerTestsUtils.*;
 import org.junit.*;
 import org.junit.runners.*;
 
-/** @author Yossi Gil
+/** 
+ * Tests {@link Essence#stringRemove}
+ * @author Yossi Gil
  * @since 2016 */
-@FixMethodOrder(MethodSorters.NAME_ASCENDING) @SuppressWarnings({ "static-method", "javadoc" }) public class Issue413 {
-  /** Correct way of trimming does not change */
-  @Test public void Z$140() {
-    trimmingOf("a").stays();
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)//
+@SuppressWarnings({ "static-method", "javadoc" }) //
+public class Issue413 {
+  @Test public void a() {
   }
 }
