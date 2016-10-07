fa7e182afa13cf6928131efa53649bae50c68e4e
Yossi Gil <yossi.gil@gmail.com>
2016-10-07 11:41:58 +0300

    TDD/Issue #404: shortest change to go back to go GREEN

--- src/test/java/il/org/spartan/spartanizer/tippers/dig.java
+++ src/test/java/il/org/spartan/spartanizer/tippers/dig.java
@@ -1,11 +1,10 @@
 package il.org.spartan.spartanizer.tippers;
 
 /** ???
- * @author Yossi Gil 
+ * @author Yossi Gil
  * @since 2016 */
 public interface dig {
-
-  static void stringLiterals(Object object) {
-    // TODO Auto-generated method stub
+  static Void stringLiterals(Object object) {
+    return null;
   }
 }
