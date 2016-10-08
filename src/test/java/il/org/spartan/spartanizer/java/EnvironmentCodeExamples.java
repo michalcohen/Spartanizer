3f96fc085782e9db3fe565c1dbd84d441efc9b6e
Yossi Gil <yossi.gil@gmail.com>
2016-10-08 04:17:34 +0300

    Remove redundant @Ignore

--- src/test/java/il/org/spartan/spartanizer/java/EnvironmentCodeExamples.java
+++ src/test/java/il/org/spartan/spartanizer/java/EnvironmentCodeExamples.java
@@ -4,7 +4,7 @@ import org.junit.*;
 
 import il.org.spartan.spartanizer.annotations.*;
 
-@Ignore("This should never be furn") @SuppressWarnings("all") public final class EnvironmentCodeExamples {
+@SuppressWarnings("all") public final class EnvironmentCodeExamples {
   static class EX02 {
     int x = 1;
     @FlatEnvUse({ @Id(name = "x", clazz = "int") }) int y;
