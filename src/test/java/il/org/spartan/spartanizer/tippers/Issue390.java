9c975823d6ea7edaf6b77bdf15daa4ab32925cd4
Yossi Gil <yossi.gil@gmail.com>
2016-10-09 02:19:15 +0300

    Polishing

--- src/test/java/il/org/spartan/spartanizer/tippers/Issue390.java
+++ src/test/java/il/org/spartan/spartanizer/tippers/Issue390.java
@@ -1,10 +1,17 @@
 package il.org.spartan.spartanizer.tippers;
 
-import static il.org.spartan.spartanizer.tippers.TrimmerTestsUtils.*;
+import static il.org.spartan.azzert.*;
 
 import org.junit.*;
 
+import il.org.spartan.*;
+import il.org.spartan.spartanizer.engine.*;
+
 /** @author Alex Kopzon
  * @since 2016  */
-@SuppressWarnings("static-method") public class Issue390 {
+@SuppressWarnings("static-method") 
+public class Issue390 {
+  @Test public void demoOfAzzert() {
+    azzert.that(NameGuess.of("__"), is(NameGuess.ANONYMOUS));
+  }
 }
