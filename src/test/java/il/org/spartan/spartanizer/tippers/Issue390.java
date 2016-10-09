5307841a3340677a6590b767e44804a1563ea70c
Yossi Gil <yossi.gil@gmail.com>
2016-10-09 14:14:46 +0300

    Fewer warnings

--- src/test/java/il/org/spartan/spartanizer/tippers/Issue390.java
+++ src/test/java/il/org/spartan/spartanizer/tippers/Issue390.java
@@ -2,24 +2,14 @@ package il.org.spartan.spartanizer.tippers;
 
 import static il.org.spartan.azzert.*;
 import static il.org.spartan.spartanizer.engine.into.*;
-import static il.org.spartan.spartanizer.tippers.TrimmerTestsUtils.*;
 
 import org.junit.*;
-import org.junit.runners.*;
 
 import il.org.spartan.*;
 import il.org.spartan.spartanizer.ast.factory.*;
 import il.org.spartan.spartanizer.ast.navigate.*;
 import il.org.spartan.spartanizer.engine.*;
 import il.org.spartan.spartanizer.java.*;
-
-
-import static il.org.spartan.azzert.*;
-
-import org.junit.*;
-
-import il.org.spartan.*;
-import il.org.spartan.spartanizer.engine.*;
 
 /** @author Alex Kopzon
  * @since 2016 */
