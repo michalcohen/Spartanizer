3ae5e08d660d58c22b2e3388520eea887b28b771
dngreenst <dngreenst@gmail.com>
2016-10-09 16:59:02 +0300

    #404 GREEN

--- src/test/java/il/org/spartan/spartanizer/tippers/dig.java
+++ src/test/java/il/org/spartan/spartanizer/tippers/dig.java
@@ -2,11 +2,16 @@ package il.org.spartan.spartanizer.tippers;
 
 import java.util.*;
 
+import org.eclipse.jdt.core.dom.*;
+
 /** ???
  * @author Yossi Gil
  * @since 2016 */
 public interface dig {
-  static Object stringLiterals(final Object o) {
-    return Collections.EMPTY_LIST;
+  static List<String> stringLiterals(final ASTNode ¢) {
+    List<String> $ = new ArrayList<>();
+    if(¢ instanceof StringLiteral)
+      $.add(((StringLiteral)¢).getLiteralValue());
+    return $;
   }
 }
