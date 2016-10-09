6b04fd18ab2cafcda463efdd39afa655887a3617
dngreenst <dngreenst@gmail.com>
2016-10-09 20:47:22 +0300

    #356

--- src/test/java/il/org/spartan/spartanizer/tippers/Issue378.java
+++ src/test/java/il/org/spartan/spartanizer/tippers/Issue378.java
@@ -7,12 +7,12 @@ import org.junit.*;
 
 /** @author Alex Kopzon
  * @since 2016 */
-@SuppressWarnings("static-method") public class Issue378 {
+@SuppressWarnings("static-method") @Ignore public class Issue378 {
   @Test public void a() {
     trimmingOf("void func(int i) {return something_else;}").gives("void func(int __) {return something_else;}").stays();
   }
 
-  @Ignore @Test public void b() {
+  @Test public void b() {
     trimmingOf("void func(int notJD) {return something_else;}").stays();
   }
 }
