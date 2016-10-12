d89fc98f25be6645af1b76bcb92959c16ed836dc
M4tt30rru <matteo.orru@diee.unica.it>
2016-10-12 22:46:40 +0300

    Added count tippers functionality

--- src/main/java/il/org/spartan/spartanizer/cmdline/AbstractBatch.java
+++ src/main/java/il/org/spartan/spartanizer/cmdline/AbstractBatch.java
@@ -4,6 +4,7 @@ import static il.org.spartan.spartanizer.cmdline.system.*;
 import static il.org.spartan.tide.*;
 
 import java.io.*;
+import java.util.*;
 import java.util.Map.*;
 
 import org.eclipse.jdt.core.dom.*;
@@ -20,7 +21,8 @@ import il.org.spartan.spartanizer.engine.*;
 import il.org.spartan.spartanizer.tipping.*;
 import il.org.spartan.utils.*;
 
-/** @author Yossi Gil
+/**
+ * @author Yossi Gil 
  * @since 2016 */
 abstract class AbstractBatch {
   protected static final String folder = "/tmp/";
@@ -35,8 +37,20 @@ abstract class AbstractBatch {
   protected String reportFileName;
   protected Toolbox toolbox = new Toolbox();
   protected final ChainStringToIntegerMap spectrum = new ChainStringToIntegerMap();
+  protected final ChainStringToIntegerMap coverage = new ChainStringToIntegerMap();
   protected CSVStatistics spectrumStats;
-  private final String spectrumFileName;
+  protected CSVStatistics coverageStats;
+  private String spectrumFileName;
+  private String coverageFileName;
+  static String presentFileName;
+  static String presentMethod;
+  protected static List<Class<? extends BodyDeclaration>> selectedNodeTypes = as.list(MethodDeclaration.class);
+  int tippersAppliedOnCurrentObject;
+
+  public static void main(String[] args) {
+    System.out.println(system.p(12, 15));
+    System.out.println(system.p(18, 15));
+  }
 
   AbstractBatch(final String path) {
     this(path, system.folder2File(path));
@@ -48,29 +62,39 @@ abstract class AbstractBatch {
     afterFileName = folder + name + ".after.java";
     reportFileName = folder + name + ".CSV";
     spectrumFileName = folder + name + ".spectrum.CSV";
+    coverageFileName = folder + name + ".coverage.CSV";
   }
 
-  public void consolidateTips(final ASTRewrite r, final CompilationUnit u) {
+  public void consolidateTips(final ASTRewrite r, final BodyDeclaration u) {
     toolbox = Toolbox.defaultInstance();
     u.accept(new DispatchingVisitor() {
       @Override protected <N extends ASTNode> boolean go(final N n) {
         TrimmerLog.visitation(n);
-        // if astnode is in selectedNodeType check is true
-        if (!check(n) || disabling.on(n))
+        if (disabling.on(n))
           return true;
-        final Tipper<N> w = getTipper(n);
-        if (w == null)
+        final Tipper<N> tipper = getTipper(n);
+        if (tipper == null)
           return true;
         Tip s = null;
         try {
-          s = w.tip(n, exclude);
-          tick(n, w);
+          s = tipper.tip(n, exclude);
+          tick(n, tipper);
         } catch (final TipperFailure f) {
           monitor.debug(this, f);
         }
-        if (s != null)
+        if (s != null) {
+          ++tippersAppliedOnCurrentObject;
+          tick2(n, tipper); // save coverage info
           TrimmerLog.application(r, s);
+        }
         return true;
+      }
+
+      <N extends ASTNode> void tick2(N n, Tipper<N> w) {
+        String key = presentFileName + "-" + presentMethod + monitor.className(w.getClass());
+        if (!coverage.containsKey(key))
+          coverage.put(key, 0);
+        coverage.put(key, coverage.get(key) + 1);
       }
 
       /** @param n
@@ -83,7 +107,7 @@ abstract class AbstractBatch {
 
       /** @param w */
       <N extends ASTNode> void tick(final Tipper<N> w) {
-        final String key = monitor.className(w.getClass());
+        String key = monitor.className(w.getClass());
         if (!spectrum.containsKey(key))
           spectrum.put(key, 0);
         spectrum.put(key, spectrum.get(key) + 1);
@@ -95,17 +119,15 @@ abstract class AbstractBatch {
     });
   }
 
-  abstract boolean check(ASTNode n);
-
-  public ASTRewrite createRewrite(final CompilationUnit ¢) {
-    final ASTRewrite $ = ASTRewrite.create(¢.getAST());
-    consolidateTips($, ¢);
+  public ASTRewrite createRewrite(final BodyDeclaration u) {
+    final ASTRewrite $ = ASTRewrite.create(u.getAST());
+    consolidateTips($, u);
     return $;
   }
 
   public String fixedPoint(final String from) {
     for (final Document $ = new Document(from);;) {
-      final CompilationUnit u = (CompilationUnit) makeAST.COMPILATION_UNIT.from($.get());
+      final BodyDeclaration u = (BodyDeclaration) makeAST.CLASS_BODY_DECLARATIONS.from($.get());
       final ASTRewrite r = createRewrite(u);
       final TextEdit e = r.rewriteAST($, null);
       try {
@@ -119,28 +141,34 @@ abstract class AbstractBatch {
     }
   }
 
-  public ASTRewrite rewriterOf(final CompilationUnit ¢) {
+  public ASTRewrite rewriterOf(final BodyDeclaration ¢) {
     final ASTRewrite $ = ASTRewrite.create(¢.getAST());
     consolidateTips($, ¢);
     return $;
   }
 
-  public static Process shellEssenceMetrics(final String fileName) {
-    return system.bash("./essence < " + fileName + " >" + essenced(fileName));
-  }
-
   protected void fire() {
     go();
     reportSpectrum();
+    reportCoverage();
     runEssence();
     runWordCount();
   }
 
-  /**
-   *
-   */
+  private void reportCoverage() {
+    for (Entry<String, Integer> ¢ : coverage.entrySet()) {
+      int i = ¢.getKey().indexOf(".java");
+      String file = ¢.getKey().substring(0, i + 5);
+      coverageStats.put("File", file);
+      coverageStats.put("Method", ¢.getKey());
+      coverageStats.put("Spartanization", ¢.getValue());
+      coverageStats.nl();
+    }
+    System.err.print("\n Coverage: " + coverageStats.close());
+  }
+
   private void reportSpectrum() {
-    for (final Entry<String, Integer> ¢ : spectrum.entrySet()) {
+    for (Entry<String, Integer> ¢ : spectrum.entrySet()) {
       spectrumStats.put("Tipper", ¢.getKey());
       spectrumStats.put("Times", ¢.getValue());
       spectrumStats.nl();
@@ -148,65 +176,71 @@ abstract class AbstractBatch {
     System.err.print("\n Spectrum: " + spectrumStats.close());
   }
 
-  boolean go(final BodyDeclaration ¢) {
-    System.out.println("Free memory (bytes): " + Runtime.getRuntime().freeMemory());
-    final int length = ¢.getLength();
-    final int tokens = metrics.tokens(¢ + "");
-    final int nodes = metrics.nodesCount(¢);
-    final int body = metrics.bodySize(¢);
-    final int tide = clean(¢ + "").length();
-    final int essence = Essence.of(¢ + "").length();
-    // perform spartanization
-    final String out = fixedPoint(¢);
+  boolean go(final ASTNode input) {
+    tippersAppliedOnCurrentObject = 0;
+    final int length = input.getLength();
+    final int tokens = metrics.tokens(input + "");
+    final int nodes = metrics.nodesCount(input);
+    final int body = metrics.bodySize(input);
+    final int statements = extract.statements(az.methodDeclaration(input).getBody()).size();
+    final int tide = clean(input + "").length();
+    final int essence = Essence.of(input + "").length();
+    final String out = fixedPoint(input);
     final int length2 = out.length();
     final int tokens2 = metrics.tokens(out);
     final int tide2 = clean(out + "").length();
     final int essence2 = Essence.of(out + "").length();
     final int wordCount = code.wc(il.org.spartan.spartanizer.cmdline.Essence.of(out + ""));
-    final ASTNode from = makeAST.COMPILATION_UNIT.from(out);
-    final int nodes2 = metrics.nodesCount(from);
-    final int body2 = metrics.bodySize(from);
-    System.err.println(++done + " " + extract.category(¢) + " " + extract.name(¢));
-    // printout
-    befores.print(¢);
+    final ASTNode to = makeAST.CLASS_BODY_DECLARATIONS.from(out);
+   final int nodes2 = metrics.nodesCount(to);
+    final int body2 = metrics.bodySize(to);
+    MethodDeclaration methodDeclaration = az.methodDeclaration(to);
+    int statements2 = methodDeclaration == null ? -1 : extract.statements(methodDeclaration.getBody()).size();
+    System.err.println(++done + " " + extract.category(input) + " " + extract.name(input));
+    befores.print(input);
     afters.print(out);
-    // generate csv with statistics
     report.summaryFileName();
     report//
         .put("File", currentFile)//
-        .put("TipperCategory", extract.category(¢))//
-        .put("Name", extract.name(¢))//
+        .put("Category", extract.category(input))//
+        .put("Name", extract.name(input))//
+        .put("# Tippers", tippersAppliedOnCurrentObject) //
         .put("Nodes1", nodes)//
         .put("Nodes2", nodes2)//
         .put("Δ Nodes", nodes - nodes2)//
         .put("δ Nodes", system.d(nodes, nodes2))//
-        .put("δ Nodes %", Double.parseDouble(removePercentChar(system.p(nodes, nodes2))))//
+        .put("δ Nodes %", ((system.p(nodes, nodes2))))//
         .put("Body", body)//
         .put("Body2", body2)//
         .put("Δ Body", body - body2)//
         .put("δ Body", system.d(body, body2))//
-        .put("% Body", Double.parseDouble(removePercentChar(system.p(body, body2))))//
+        .put("% Body", ((system.p(body, body2))))//
         .put("Length1", length)//
         .put("Tokens1", tokens)//
         .put("Tokens2", tokens2)//
         .put("Δ Tokens", tokens - tokens2)//
         .put("δ Tokens", system.d(tokens, tokens2))//
-        .put("% Tokens", Double.parseDouble(removePercentChar(system.p(tokens, tokens2))))//
+        .put("% Tokens", ((system.p(tokens, tokens2))))//
         .put("Length1", length)//
         .put("Length2", length2)//
         .put("Δ Length", length - length2)//
         .put("δ Length", system.d(length, length2))//
-        .put("% Length", Double.parseDouble(removePercentChar(system.p(length, length2))))//
+        .put("% Length", ((system.p(length, length2))))//
         .put("Tide1", tide)//
         .put("Tide2", tide2)//
         .put("Δ Tide2", tide - tide2)//
         .put("δ Tide2", system.d(tide, tide2))//
-        .put("δ Tide2", Double.parseDouble(removePercentChar(system.p(tide, tide2))))//
+        .put("δ Tide2", ((system.p(tide, tide2))))//
         .put("Essence1", essence)//
         .put("Essence2", essence2)//
         .put("Δ Essence", essence - essence2)//
         .put("δ Essence", system.d(essence, essence2))//
-        .put("% Essence", Double.parseDouble(removePercentChar(system.p(essence, essence2))))//
+        .put("% Essence", ((system.p(essence, essence2))))//
+        .put("Statements1", statements)//
+        .put("Statement2", statements2)//
+        .put("Δ Statement", statements - statements2)//
+        .put("δ Statement", system.d(statements, statements2))//
+        .put("% Statement", ((system.p(essence, essence2))))//
         .put("Words)", wordCount).put("R(T/L)", system.ratio(length, tide)) //
         .put("R(E/L)", system.ratio(length, essence)) //
         .put("R(E/T)", system.ratio(tide, essence)) //
@@ -218,34 +252,25 @@ abstract class AbstractBatch {
 
   void go(final CompilationUnit u) {
     u.accept(new ASTVisitor() {
-      @Override public boolean visit(final AnnotationTypeMemberDeclaration ¢) {
-        return go(¢);
-      }
-
-      @Override public boolean visit(final MethodDeclaration ¢) {
-        return go(¢);
-      }
-
-      @Override public boolean visit(final TypeDeclaration ¢) {
-        return go(¢);
-      }
-
-      @Override public boolean visit(final EnumConstantDeclaration ¢) {
-        return go(¢);
-      }
-
-      @Override public boolean visit(final FieldDeclaration ¢) {
-        return go(¢);
-      }
-
-      @Override public boolean visit(final EnumDeclaration ¢) {
-        return go(¢);
-      }
-
-      @Override public boolean visit(final Initializer ¢) {
-        return go(¢);
+      @Override public boolean preVisit2(ASTNode ¢) {
+        return !selectedNodeTypes.contains(¢.getClass()) || !filter(¢) || go(¢);
       }
     });
+  }
+
+  boolean filter(ASTNode ¢) {
+    return true;
+  }
+
+  static String getEnclosingMethodName(final BodyDeclaration ¢) {
+    ASTNode parentNode = ¢.getParent();
+    assert parentNode != null;
+    while (parentNode.getNodeType() != ASTNode.METHOD_DECLARATION) {
+      if (parentNode instanceof CompilationUnit)
+        return null;
+      parentNode = parentNode.getParent();
+    }
+    return ((MethodDeclaration) parentNode).getName() + "";
   }
 
   void go(final File f) {
@@ -283,13 +308,17 @@ abstract class AbstractBatch {
       afters = a;
       report = new CSVStatistics(reportFileName, "property");
       spectrumStats = new CSVStatistics(spectrumFileName, "property");
-      for (final File ¢ : new FilesGenerator(".java").from(inputPath))
+      coverageStats = new CSVStatistics(coverageFileName, "property");
+      for (final File ¢ : new FilesGenerator(".java").from(inputPath)) {
+        presentFileName = ¢.getName();
+        System.out.println("Free memory (bytes): " + Unit.BYTES.format(Runtime.getRuntime().freeMemory()));
         go(¢);
+      }
     } catch (final IOException x) {
       x.printStackTrace();
-      System.err.println(done + " files processed; processing of " + inputPath + " failed for some I/O reason");
+      System.err.println(done + " items processed; processing of " + inputPath + " failed for some I/O reason");
     }
-    System.err.print("\n Done: " + done + " files processed.");
+    System.err.print("\n Done: " + done + " items processed.");
     System.err.print("\n Summary: " + report.close());
   }
 
@@ -298,13 +327,17 @@ abstract class AbstractBatch {
   }
 
   void runEssence() {
-    shellEssenceMetrics(beforeFileName);
-    shellEssenceMetrics(afterFileName);
+    system.shellEssenceMetrics(beforeFileName);
+    system.shellEssenceMetrics(afterFileName);
   }
 
   /** @param ¢
    * @return */
-  String fixedPoint(final BodyDeclaration ¢) {
+  String fixedPoint(final ASTNode ¢) {
     return fixedPoint(¢ + "");
+  }
+
+  @SuppressWarnings("static-method") public void selectedNodes(@SuppressWarnings("unchecked") final Class<? extends BodyDeclaration>... ¢) {
+    selectedNodeTypes = as.list(¢);
   }
 }
--- src/main/java/il/org/spartan/spartanizer/cmdline/Spartanizer.java
+++ src/main/java/il/org/spartan/spartanizer/cmdline/Spartanizer.java
@@ -1,18 +1,10 @@
 package il.org.spartan.spartanizer.cmdline;
 
-import java.util.*;
-
-import org.eclipse.jdt.core.dom.*;
-
-import il.org.spartan.*;
-
 /** Scans files named by folder, ignore test files, and collect statistics, on
  * classes, methods, etc.
- * @author Yossi Gil
+ * @autho/home/matteo/MUTATION_TESTING/test-spartanizer/projects/commons-bcelr Yossi Gil
  * @year 2015 */
 public final class Spartanizer extends AbstractBatch {
-  static final List<Class<? extends BodyDeclaration>> selectedNodeTypes = as.list(MethodDeclaration.class);
-
   public static void main(final String[] args) {
     for (final String ¢ : args.length != 0 ? args : new String[] { "." })
       new Spartanizer(¢).fire();
@@ -20,17 +12,5 @@ public final class Spartanizer extends AbstractBatch {
 
   Spartanizer(final String path) {
     super(path);
-  }
-
-  @Override protected boolean check(final ASTNode ¢) {
-    return !selectedNodeTypes.contains(¢.getClass());
-  }
-
-  /**
-   * @param class1
-   */
-  public void selectedNodes(Class<TypeDeclaration> class1) {
-    // TODO Auto-generated method stub
-    
   }
 }
