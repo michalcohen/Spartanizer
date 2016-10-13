f541fa8b634279b042f87b6708b03a56c766b439
M4tt30rru <matteo.orru@diee.unica.it>
2016-10-13 15:33:37 +0300

    update

--- src/main/java/il/org/spartan/spartanizer/cmdline/Spartanizer.java
+++ src/main/java/il/org/spartan/spartanizer/cmdline/Spartanizer.java
@@ -2,8 +2,7 @@ package il.org.spartan.spartanizer.cmdline;
 
 /** Scans files named by folder, ignore test files, and collect statistics, on
  * classes, methods, etc.
- * @autho/home/matteo/MUTATION_TESTING/test-spartanizer/projects/commons-bcelr Yossi
- *                                                                             Gil
+ * @author Yossi Gil
  * @year 2015 */
 public final class Spartanizer extends AbstractBatch {
   public static void main(final String[] args) {
