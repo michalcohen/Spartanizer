49c45b0a89368da5970b8951863a62df85d8cb4a
orimarco <marcovitch.ori@gmail.com>
2016-10-13 11:58:56 +0300

    Javadoc adding patterns. thanks to @OriRoth

--- src/main/java/il/org/spartan/spartanizer/research/patterns/Setter.java
+++ src/main/java/il/org/spartan/spartanizer/research/patterns/Setter.java
@@ -1,20 +1,14 @@
 package il.org.spartan.spartanizer.research.patterns;
 
 import java.util.*;
-
 import org.eclipse.jdt.core.dom.*;
-import org.eclipse.jdt.core.dom.rewrite.*;
-import org.eclipse.text.edits.*;
-
 import il.org.spartan.spartanizer.ast.navigate.*;
 import il.org.spartan.spartanizer.ast.safety.*;
-import il.org.spartan.spartanizer.engine.*;
-import il.org.spartan.spartanizer.tipping.*;
 
 /** @author Ori Marcovitch
  * @since 2016 */
-public class Setter extends NanoPatternTipper<MethodDeclaration> {
-  @Override protected boolean prerequisite(MethodDeclaration ¢) {
+public class Setter extends JavadocMarkerNanoPattern<MethodDeclaration> {
+  @Override protected boolean morePrerequisites(MethodDeclaration ¢) {
     if (step.parameters(¢).size() != 1)
       return false;
     @SuppressWarnings("unchecked") List<Statement> ss = ¢.getBody().statements();
@@ -28,11 +22,7 @@ public class Setter extends NanoPatternTipper<MethodDeclaration> {
     return ¢.getName() + " is a setter method";
   }
 
-  @Override public Tip tip(MethodDeclaration ¢) {
-    return new Tip(description(¢), ¢, this.getClass()) {
-      @SuppressWarnings("unused") @Override public void go(final ASTRewrite r, final TextEditGroup g) {
-        // TODO: empty
-      }
-    };
+  @Override protected String javadoc() {
+    return "[[Setter]]";
   }
 }
