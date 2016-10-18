21366cfdcb4c2e2d8d3e30ee7a2a97b0f6b56512
Yossi Gil <yossi.gil@gmail.com>
2016-10-18 12:54:15 +0300

    we do not allow names such essenceNew, RemoveZeroes2, etc. #200

--- src/main/java/il/org/spartan/spartanizer/cmdline/code.java
+++ src/main/java/il/org/spartan/spartanizer/cmdline/code.java
@@ -3,12 +3,16 @@ package il.org.spartan.spartanizer.cmdline;
 /** @author Yossi Gil
  * @since 2016 */
 public interface code {
-  static String essenceNew(final String codeFragment) {
-    return codeFragment.replaceAll("//.*?\r\n", "\n").replaceAll("/\\*(?=(?:(?!\\*/)[\\s\\S])*?)(?:(?!\\*/)[\\s\\S])*\\*/", "")
-        .replaceAll("^\\s*$", "").replaceAll("^\\s*\\n", "").replaceAll("\\s*$", "").replaceAll("\\s+", " ")
-        .replaceAll("\\([^a-zA-Z]\\) \\([^a-zA-Z]\\)", "\\([^a-zA-Z]\\)\\([^a-zA-Z]\\)")
-        .replaceAll("\\([^a-zA-Z]\\) \\([a-zA-Z]\\)", "\\([^a-zA-Z]\\)\\([a-zA-Z]\\)")
-        .replaceAll("\\([a-zA-Z]\\) \\([^a-zA-Z]\\)", "\\([a-zA-Z]\\)\\([^a-zA-Z]\\)");
+  static String essence(final String codeFragment) {
+    return codeFragment.replaceAll("//.*?\r\n", "\n")//
+        .replaceAll("/\\*(?=(?:(?!\\*/)[\\s\\S])*?)(?:(?!\\*/)[\\s\\S])*\\*/", "")
+        .replaceAll("^\\s*$", "")//
+        .replaceAll("^\\s*\\n", "")//
+        .replaceAll("\\s*$", "")//
+        .replaceAll("\\s+", " ")
+        .replaceAll("\\([^a-zA-Z¢$_]\\) \\([^a-zA-Z¢$_]\\)", "\\([^a-zA-Z]\\)\\([^a-zA-Z]\\)")
+        .replaceAll("\\([^a-zA-Z¢$_]\\) \\([a-zA-Z¢$_]\\)", "\\([^a-zA-Z]\\)\\([a-zA-Z]\\)")
+        .replaceAll("\\([a-zA-Z¢$_]\\) \\([^a-zA-Z¢$_]\\)", "\\([a-zA-Z]\\)\\([^a-zA-Z]\\)");
   }
 
   static int wc(final String $) {
