9c14e585eca30dfddcb9e205338b1114612771d3
Ori Roth <ori.rothh@gmail.com>
2016-10-18 16:17:04 +0300

    Spartanized Linguistic

--- src/main/java/il/org/spartan/spartanizer/engine/Linguistic.java
+++ src/main/java/il/org/spartan/spartanizer/engine/Linguistic.java
@@ -9,84 +9,104 @@ import il.org.spartan.*;
  * @author Ori Roth
  * @since 2.6 */
 public interface Linguistic {
-  /** Error string, replacing null/error value. */
-  public static final String UNKNOWN = "???";
-  public static final String SEPARATOR = ", ";
+  /**
+   * Error string, replacing null/error value. 
+   */
+  final String UNKNOWN = "???";
+  final String SEPARATOR = ", ";
 
-  /** Cut string's suffix to maximal length.
-   * @param s JD
-   * @param l JD
-   * @return cut string */
-  public static String trim(final String s, final int l) {
+  /**
+   * Cut string's suffix to maximal length.
+   * @param s  JD
+   * @param l  JD
+   * @return  cut string 
+   */
+  static String trim(final String s, final int l) {
     return s == null || s.length() < l ? s : s.substring(0, l);
   }
 
-  /** Get the plural form of the word if needed, by adding an 's' to its end.
-   * @param s string to be pluralize
-   * @param i count
-   * @return fixed string */
-  public static String plurals(final String s, final Integer i) {
+  /**
+   * Get the plural form of the word if needed, by adding an 's' to its end.
+   * @param s  string to be pluralize
+   * @param i  count
+   * @return  fixed string 
+   */
+  static String plurals(final String s, final Integer i) {
     return i == null ? UNKNOWN + " " + s + "s" : i.intValue() != 1 ? i + " " + s + "s" : "one " + s;
   }
 
-  /** Get the plural form of the word if needed, by adding an 'es' to its end.
-   * @param s string to be pluralize
-   * @param i count
-   * @return fixed string */
-  public static String plurales(final String s, final Integer i) {
+  /**
+   * Get the plural form of the word if needed, by adding an 'es' to its end.
+   * @param s  string to be pluralize
+   * @param i  count
+   * @return  fixed string 
+   */
+  static String plurales(final String s, final Integer i) {
     return i == null ? UNKNOWN + " " + s + "es" : i.intValue() != 1 ? i + " " + s + "es" : "one " + s;
   }
 
-  /** Get the plural form of the word if needed, by adding an 's' to its end.
-   * @param s string to be pluralize
-   * @param i count
-   * @return fixed string */
-  public static String plurals(final String s, final AtomicInteger i) {
+  /**
+   * Get the plural form of the word if needed, by adding an 's' to its end.
+   * @param s  string to be pluralize
+   * @param i  count
+   * @return  fixed string 
+   */
+  static String plurals(final String s, final AtomicInteger i) {
     return i == null ? UNKNOWN + " " + s + "s" : i.intValue() != 1 ? i + " " + s + "s" : "one " + s;
   }
 
-  /** Get the plural form of the word if needed, by adding an 'es' to its end.
-   * @param s string to be pluralize
-   * @param i count
-   * @return fixed string */
-  public static String plurales(final String s, final AtomicInteger i) {
+  /**
+   * Get the plural form of the word if needed, by adding an 'es' to its end.
+   * @param s  string to be pluralize
+   * @param i  count
+   * @return  fixed string 
+   */
+  static String plurales(final String s, final AtomicInteger i) {
     return i == null ? UNKNOWN + " " + s + "es" : i.intValue() != 1 ? i + " " + s + "es" : "one " + s;
   }
 
-  /** Get the plural form of the word if needed, by adding an 's' to its end.
-   * @param s string to be pluralize
-   * @param i count
-   * @return fixed string */
-  public static String plurals(final String s, final int i) {
+  /**
+   * Get the plural form of the word if needed, by adding an 's' to its end.
+   * @param s  string to be pluralize
+   * @param i  count
+   * @return  fixed string 
+   */
+  static String plurals(final String s, final int i) {
     return i == 1 ? "one " + s : i + " " + s + "s";
   }
 
-  /** Get the plural form of the word if needed, by adding an 'es' to its end.
-   * @param s string to be pluralize
-   * @param i count
-   * @return fixed string */
-  public static String plurales(final String s, final int i) {
+  /**
+   * Get the plural form of the word if needed, by adding an 'es' to its end.
+   * @param s  string to be pluralize
+   * @param i  count
+   * @return  fixed string 
+   */
+  static String plurales(final String s, final int i) {
     return i == 1 ? "one " + s : i + " " + s + "es";
   }
 
-  /** @param ¢ something
-   * @return printable {@link String} for it */
-  public static <X> String nanable(final X ¢) {
-    return ¢ == null ? UNKNOWN : ¢ + "";
+  /**
+   * @param ¢  something
+   * @return  printable  {@link String}  for it 
+   */
+  static <X> String nanable(final X ¢) {
+    return ¢ != null ? ¢ + "" : UNKNOWN;
   }
 
-  /** @param x something
-   * @param f function to be conducted on x in case it is not null
-   * @return printable {@link String} for f(x) */
-  public static <X> String nanable(final X x, final Function<X, ?> f) {
+  /**
+   * @param x  something
+   * @param f  function to be conducted on x in case it is not null
+   * @return  printable  {@link String}  for f(x) 
+   */
+  static <X> String nanable(final X x, final Function<X, ?> f) {
     return x == null ? UNKNOWN : f.apply(x) + "";
   }
 
-  public static String merge(final Object[] os) {
-    return separate.these(os).by(SEPARATOR);
+  static String merge(final Object[] ¢) {
+    return separate.these(¢).by(SEPARATOR);
   }
 
-  public static String merge(final Object[] os, final String separator) {
+  static String merge(final Object[] os, final String separator) {
     return separate.these(os).by(separator);
   }
 }
