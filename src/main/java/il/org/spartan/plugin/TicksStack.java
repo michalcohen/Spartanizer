67decbdf095d3f13f79a367b36680ea18effcc54
Yossi Gil <yossi.gil@gmail.com>
2016-10-22 21:28:43 +0300

    Move to its own file

---
+++ src/main/java/il/org/spartan/plugin/TicksStack.java
@@ -0,0 +1,28 @@
+package il.org.spartan.plugin;
+
+import java.util.*;
+
+/** A simple stack, to be extended with more delegators, in the unlikely event
+ * that the need arises.
+ * <p>
+ * Vanilla flavor to used for quick implementation of {@link Listener}.
+ * @see #push(Object...)
+ * @see #pop(Object...)
+ * @see #empty()
+ * @author Yossi Gil
+ * @year 2016 */
+public class TicksStack {
+  public final Stack<Object[]> inner = new Stack<>();
+
+  public Object[] pop() {
+    return inner.pop();
+  }
+
+  public void push(final Object... os) {
+    inner.push(os);
+  }
+
+  public Object[] top() {
+    return inner.lastElement();
+  }
+}
