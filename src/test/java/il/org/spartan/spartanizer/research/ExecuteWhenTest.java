e8f35670414985898a7e59ede45c4afd10c59d5d
Yossi Gil <yossi.gil@gmail.com>
2016-10-23 12:20:11 +0300

    straighten up

--- src/test/java/il/org/spartan/spartanizer/research/ExecuteWhenTest.java
+++ src/test/java/il/org/spartan/spartanizer/research/ExecuteWhenTest.java
@@ -9,13 +9,17 @@ import il.org.spartan.spartanizer.research.patterns.*;
 
 /** @author Ori Marcovitch
  * @since 2016 */
-@SuppressWarnings("static-method") public class ExecuteWhenTest {
+@Ignore //
+@SuppressWarnings("static-method") //
+public class ExecuteWhenTest {
   @Test public void basic() {
-    trimmingOf("if(x == 8) print(8);").withTipper(IfStatement.class, new ExecuteWhen()).gives("execute((x) -> print(8)).when(x==8);");
+    trimmingOf("if(x==8)p(8);").withTipper(IfStatement.class, new ExecuteWhen())//
+        .gives("execute((x)->p(8)).when(x==8);");
   }
 
-  @Test public void comlicated() {
-    trimmingOf("if(x == 8 && iz.Literal(lit) || bigDaddy(d)) a.b()._(f,g).f.x(8,g,h*p);").withTipper(IfStatement.class, new ExecuteWhen())
-        .gives("execute((x)->a.b()._(f,g).f.x(8,g,h*p)).when(x == 8 && iz.Literal(lit) || bigDaddy(d));");
+  @Test public void complicated() {
+    trimmingOf("if(x == 8 && iz.t(i) || b(d)) a.b()._(f,g).f.x(8,g,h*p);")//
+        .withTipper(IfStatement.class, new ExecuteWhen())//
+        .gives("execute((x)->a.b()._(f,g).f.x(8,g,h*p)).when(x == 8 && iz.t(i) || b(d));");
   }
 }
