287609d61357b2a342721de1e5c6a90dba35008a
M4tt30rru <matt.orru@gmail.com>
2016-10-23 19:13:22 +0300

    new command line client

---
+++ src/main/java/il/org/spartan/spartanizer/cmdline/CollectApplicator.java
@@ -0,0 +1,17 @@
+package il.org.spartan.spartanizer.cmdline;
+
+import il.org.spartan.plugin.*;
+
+/**
+ * This is an applicator that collects data of a project
+ * @author matteo
+ */
+
+public class CollectApplicator extends Applicator{
+
+  @Override public void go() {
+    
+  }
+
+  
+}
