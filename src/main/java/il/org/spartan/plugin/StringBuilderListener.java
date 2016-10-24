4245aa4fa6d0892cdb2715a319fdf9f65fe3c60b
Yossi Gil <yossi.gil@gmail.com>
2016-10-24 12:24:10 +0300

    Two fewer warnings

---
+++ src/main/java/il/org/spartan/plugin/StringBuilderListener.java
@@ -0,0 +1,41 @@
+package il.org.spartan.plugin;
+
+import static il.org.spartan.plugin.Listener.*;
+
+import il.org.spartan.utils.*;
+
+/** A kind of {@link Listener} that records a long string of the message it
+ * got.
+ * @author Yossi Gil
+ * @since 2016 */
+public class StringBuilderListener implements Listener {
+  private static Tab tab = new Tab();
+
+  private static String trim(final Object ¢) {
+    return (¢ + "").substring(1, 35);
+  }
+
+  private final StringBuilder $ = new StringBuilder();
+
+  public String $() {
+    return $ + "";
+  }
+
+  @Override public void pop(final Object... ¢) {
+    $.append(tab.end());
+    Listener.super.pop(¢);
+  }
+
+  @Override public void push(final Object... ¢) {
+    $.append(tab.begin());
+    Listener.super.push(¢);
+  }
+
+  @Override public void tick(final Object... os) {
+    $.append(newId() + ": ");
+    final Separator s = new Separator(", ");
+    for (final Object ¢ : os)
+      $.append(s + trim(¢));
+    $.append('\n');
+  }
+}
