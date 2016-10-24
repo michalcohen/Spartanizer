f437824d96c237ec49303d5e80fcc6e90db945aa
Yossi Gil <yossi.gil@gmail.com>
2016-10-24 13:52:28 +0300

    More testing for #490

--- laconization-cases/FilteredEntryMultimap.java
+++ laconization-cases/FilteredEntryMultimap.java
@@ -1,19 +1,16 @@
-/*
- * Copyright (C) 2012 The Guava Authors
+/* Copyright (C) 2012 The Guava Authors
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
  *
  * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License. */
 package com.google.common.collect;
 
 import static com.google.common.base.Preconditions.checkNotNull;
@@ -55,7 +52,23 @@ class FilteredEntryMultimap<K, V> extends AbstractMultimap<K, V> implements Filt
       return satisfies(key, value);
     }
   }
+  class A {
+    void g() {
+      class V {
+        public boolean r() {
+            Iterator<Entry<K, C<V>>> e = f();
+            while (e.hasNext()) 
+              a++;
+        }
+      }
+    }
+  }
 
+void r() {
+            Iterator<Entry<K, C<V>>> e = f();
+            while (e.g()) 
+              ++a;
+        }
   @WeakOuter
   class AsMap extends ViewCachingAbstractMap<K, Collection<V>> {
     @Override
@@ -88,18 +101,25 @@ class FilteredEntryMultimap<K, V> extends AbstractMultimap<K, V> implements Filt
       return new ValuesImpl();
     }
   }
-
-  @Override
-  Multiset<K> createKeys() {
-    return new Keys();
+    public void f() {
+      
+      return new O() {
+        private boolean g() {
+          return r(new Predicate<Map.Entry<K, Collection<V>>>(){});
+        }
+      };
+    }
+  @class K {
+    public void f() {
+      return new O() {
+        private boolean g() {
+          return r(new Predicate<Map.Entry<K, Collection<V>>>(){});
+        }
+      };
+    }
   }
-
   @WeakOuter
   class Keys extends Multimaps.Keys<K, V> {
-    Keys() {
-      super(FilteredEntryMultimap.this);
-    }
-
     @Override
     public Set<Multiset.Entry<K>> entrySet() {
       return new Multisets.EntrySet<K>() {
@@ -118,4 +138,31 @@ class FilteredEntryMultimap<K, V> extends AbstractMultimap<K, V> implements Filt
       };
     }
   }
+public void f1() {
+      return new O() {
+        private boolean g() {
+          return r(new O<Map.Entry<K, Collection<V>>>(){});
+        }
+      };
+    }
+public void f2() {
+      return new O() {
+        private boolean g() {
+          return r(new O<Map.Entry<K, Collection<V>>>(){});
+        }
+      };
+    }
+
+public void f3() { return new O() { private boolean g() { return r(new O<M.E<K,C<V>>>(){}); } }; }
+int f4(){return new O(){private boolean g() {return r(new O<M.E<K,C<V>>>(){});}};}
+int f5(){return new O(){private boolean g() {return r(new O<M.E<K,C<V>>>(){});}};}
+int f6(){return new O(){private void g() {return r(new O<E<K,C<V>>>(){});}};}
+int f7(){return new O(){private void g() {r(new O<E<K,C<V>>>(){});}};}
+int f8(){return new O(){private void g() {r(new O<E<C<V>>>(){});}};}
+int f9(){return new O(){private void g() {new O<E<C<V>>>(){};}};}
+int f10(){return new O(){private int g() {new O<E<C<V>>>(){};}};}
+int f11(){return new O(){private int g() {new O<E<C>>(){};}};}
+int f12(){return new O(){private int g() {O<E<C<K>>> a;}};}
+
+int f13(){return new O(){boolean g() {return r(new O<M.E<K, C>>(){});}};}
 }
