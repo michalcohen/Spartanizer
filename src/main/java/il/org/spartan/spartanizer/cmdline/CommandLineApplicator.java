de99d3c3934f71af878ac4de36d9774ea6c99ae1
M4tt30rru <matt.orru@gmail.com>
2016-10-24 20:49:32 +0300

    added default methods

--- src/main/java/il/org/spartan/spartanizer/cmdline/CommandLineApplicator.java
+++ src/main/java/il/org/spartan/spartanizer/cmdline/CommandLineApplicator.java
@@ -2,38 +2,155 @@ package il.org.spartan.spartanizer.cmdline;
 
 import java.util.*;
 
+import org.eclipse.jdt.core.*;
 import org.eclipse.jdt.core.dom.*;
 
 import il.org.spartan.plugin.*;
+import il.org.spartan.spartanizer.dispatch.*;
 
 /** An {@link Applicator} suitable for the command line.
  * @author Matteo Orru'
  * @since 2016 */
 public class CommandLineApplicator extends Applicator {
-  
   private static final int PASSES_FEW = 1;
-  GUIBatchLaconizer gUIBatchLaconizer;
+  private static final int PASSES_MANY = 20;
 
-  private void foo1(){
-   selection(new CommandLineSelection(null, "selection"));
-  }
-  
-  private void foo2(){
-    CommandLineSelection sel = (CommandLineSelection) selection();
+  public static Applicator defaultApplicator() {
+    return new CommandLineApplicator().defaultSettings();
   }
 
-//  /** Initialize the selection of this applicator.
-//   * @param ¢ JD
-//   * @return this applicator */
-//  public Applicator selection(final AbstractSelection<?> ¢) {
-//    selection = ¢;
-//    return this;
-//  }
+  /** Default listener configuration of {@link GUIBatchLaconizer}. Simple printing to
+   * console. 
+   * @return this applicator */
+  public CommandLineApplicator defaultListenerNoisy() {
+    listener(os -> {
+      for (final Object ¢ : os)
+        System.out.print(¢ + " ");
+      System.out.println();
+    });
+    return this;
+  }
+
+  /** @return this */
+  private CommandLineApplicator defaultListenerSilent() {
+    listener((final Object... __) -> {
+      //
+    });
+    // listener(EventListener.simpleListener(event.class,
+    // e -> {
+    // // empty
+    // },
+    // (e, o) -> {
+    // // empty
+    // }));
+    return this;
+  }
+  // /** Initialize the selection of this applicator.
+  // * @param ¢ JD
+  // * @return this applicator */
+  // public Applicator selection(final AbstractSelection<?> ¢) {
+  // selection = ¢;
+  // return this;
+  // }
 
   // TODO Matteo: change selection() in Applicator to return AbstractSelection?
-//  public CommandLineSelection getSelection() {
-//    return selection;
+  // public CommandLineSelection getSelection() {
+  // return selection;
+  // }
+  /** @return this */
+  private CommandLineApplicator defaultPassesFew() {
+    passes(PASSES_FEW);
+    return this;
+  }
+
+  /** Default passes configuration of {@link GUIBatchLaconizer}, with many passes.
+   * @return this applicator */
+  public CommandLineApplicator defaultPassesMany() {
+    passes(PASSES_MANY);
+    return this;
+  }
+
+  // TODO Matteo: I have changed the "run action" to return number of tips
+  // committed instead of whether tips were committed (Boolean -> Integer).
+  // Added a quick fix to your code. Also I do not understand why you wrote this
+  // - we will inspect it once we meet. --or
+  /** @return this */
+  public CommandLineApplicator defaultRunAction() {
+    // final Trimmer t = new Trimmer();
+    final Spartanizer$Applicator s = new Spartanizer$Applicator();
+    setRunAction(u -> Integer.valueOf(s.apply(u, selection()) ? 1 : 0));
+    return this;
+  }
+
+//  // TODO Roth: use Policy / replacement for Trimmer.
+//  /** Default run action configuration of {@link GUIBatchLaconizer}. Spartanize the
+//   * {@link ICompilationUnit} using received {@link AbstractGUIApplicator}.
+//   * @param a JD
+//   * @return this applicator */
+//  public GUIBatchLaconizer defaultRunAction(final AbstractGUIApplicator a) {
+//    setRunAction(¢ -> Integer.valueOf(a.apply(¢, selection())));
+//    name(a.getName());
+//    return this;
 //  }
+  
+  
+  /** Default run action configuration of {@link CommandLineApplicator}.
+   * Spartanize the {@link CompilationUnit} using received
+   * {@link GUIBatchLaconizer$Applicator}.
+   * @param a JD
+   * @return this applicator */
+  public CommandLineApplicator defaultRunAction(final Spartanizer$Applicator a) {
+    setRunAction(u -> Integer.valueOf(a.apply(u, selection()) ? 1 : 0));
+    name(a.getName());
+    return this;
+  }
+
+  /** @return this */
+  private CommandLineApplicator defaultRunContext() {
+    runContext(r -> r.run());
+    return this;
+  }
+  // private void setSelection(@SuppressWarnings("unused") final
+  // AbstractSelection<CommandLineSelection> __) {
+  // }
+
+  /// ** Initialize the selection of this applicator.
+  // * @param s JD
+  // * @return this applicator */
+  // public CommandLineApplicator setSelection(final CommandLineSelection $) {
+  // selection = $;
+  // return this;
+  // }
+  /** @return this */
+  CommandLineApplicator defaultSelection() {
+    selection(CommandLineSelection.Util.get());
+    return this;
+  }
+
+  /** @param ¢ JD
+   * @return */
+  public CommandLineApplicator defaultSelection(@SuppressWarnings("rawtypes") final AbstractSelection ¢) {
+    selection(¢);
+    return this;
+  }
+  // /** @return selection of the applicator, ready to be configured. */
+  // public CommandLineSelection selection() {
+  // return this.selection;
+  // }
+
+  /** @return this */
+  private Applicator defaultSettings() {
+    return defaultListenerSilent().defaultPassesFew().defaultRunContext().defaultSelection();
+    // .defaultRunAction(new Trimmer()); ???
+  }
+
+  private void foo1() {
+    selection(new CommandLineSelection(null, "selection"));
+  }
+
+  private void foo2() {
+    final CommandLineSelection sel = (CommandLineSelection) selection();
+  }
 
   /* (non-Javadoc)
    *
@@ -58,97 +175,6 @@ public class CommandLineApplicator extends Applicator {
         // dead.add(¢);
       }
     });
-    System.err.println("go go go!");
-  }
-
-  public static CommandLineApplicator defaultApplicator() {
-    return new CommandLineApplicator().defaultSettings();
-  }
-
-  /** @return this */
-  private CommandLineApplicator defaultSettings() {
-    return defaultListenerSilent().defaultPassesFew().defaultRunContext().defaultSelection();
-  }
-
-  /** @return this */
-  CommandLineApplicator defaultSelection() {
-    setSelection(CommandLineSelection.Util.get());
-    return this;
-  }
-
-  private void setSelection(@SuppressWarnings("unused") final AbstractSelection<CommandLineSelection> __) {
-    // TODO Matteo: Auto-generated method stub
-  }
-
-//  /** Initialize the selection of this applicator.
-//   * @param s JD
-//   * @return this applicator */
-//  public CommandLineApplicator setSelection(final CommandLineSelection $) {
-//    selection = $;
-//    return this;
-//  }
-
-  /** @return this */
-  private CommandLineApplicator defaultRunContext() {
-    runContext(r -> r.run());
-    return this;
-  }
-
-  /** @return this */
-  private CommandLineApplicator defaultPassesFew() {
-    passes(PASSES_FEW);
-    return this;
-  }
-
-  /** @return this */
-  private CommandLineApplicator defaultListenerSilent() {
-    listener((final Object... __) -> {
-      //
-    });
-    // listener(EventListener.simpleListener(event.class,
-    // e -> {
-    // // empty
-    // },
-    // (e, o) -> {
-    // // empty
-    // }));
-    return this;
-  }
-
-  // TODO Matteo: I have changed the "run action" to return number of tips
-  // committed instead of whether tips were committed (Boolean -> Integer).
-  // Added a quick fix to your code. Also I do not understand why you wrote this
-  // - we will inspect it once we meet. --or
-  /** @return this */
-  public CommandLineApplicator defaultRunAction() {
-    // final Trimmer t = new Trimmer();
-    final Spartanizer$Applicator s = new Spartanizer$Applicator();
-    setRunAction(u -> Integer.valueOf(s.apply(u, selection()) ? 1 : 0));
-    return this;
-  }
-
-  /** Default run action configuration of {@link CommandLineApplicator}.
-   * Spartanize the {@link CompilationUnit} using received
-   * {@link GUIBatchLaconizer$Applicator}.
-   * @param a JD
-   * @return this applicator */
-  public CommandLineApplicator defaultRunAction(final Spartanizer$Applicator a) {
-    setRunAction(u -> Integer.valueOf(a.apply(u, selection()) ? 1 : 0));
-    return this;
-  }
-
-  /** @param ¢ JD
-   * @return */
-  public CommandLineApplicator defaultSelection(final AbstractSelection ¢) {
-    selection(¢);
-    return this;
-  }
-  // /** @return selection of the applicator, ready to be configured. */
-  // public CommandLineSelection selection() {
-  // return this.selection;
-  // }
-
-  public Applicator defaultListenerNoisy() {
-    return null;
+    // System.err.println("go go go!");
   }
 }
