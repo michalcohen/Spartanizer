295c989a5957c8e301af8322d5be75d0cfde2ef1
Yossi Gil <yossi.gil@gmail.com>
2016-10-24 13:54:37 +0300

    #200

--- src/main/java/il/org/spartan/spartanizer/cmdline/CollectApplicator.java
+++ src/main/java/il/org/spartan/spartanizer/cmdline/CollectApplicator.java
@@ -2,16 +2,10 @@ package il.org.spartan.spartanizer.cmdline;
 
 import il.org.spartan.plugin.*;
 
-/**
- * This is an applicator that collects data of a project
- * @author matteo
- */
-
-public class CollectApplicator extends Applicator{
-
+/** This is an applicator that collects data of a project
+ * @author Matteo Orru */
+public class CollectApplicator extends Applicator {
   @Override public void go() {
-    
+    //TODO: I am not sure you need one. Just use Listener(S), --yg
   }
-
-  
 }
