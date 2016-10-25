e65e36fc39a6523bf4333fe3b771afe4b66d474b
M4tt30rru <matt.orru@gmail.com>
2016-10-25 19:57:39 +0300

    CLApplicator

---
+++ src/main/java/il/org/spartan/spartanizer/cmdline/CLApplicator.java
@@ -0,0 +1,14 @@
+package il.org.spartan.spartanizer.cmdline;
+
+/**
+ * Applies consolidateTips from the command line
+ * @author matteo
+ *
+ */
+
+// TODO Matteo (reminder for myself): change this name? As well as GUI$Applicator it does not extend 
+// Applicator, so maybe we need to change both of them
+
+public class CLApplicator {
+
+}
