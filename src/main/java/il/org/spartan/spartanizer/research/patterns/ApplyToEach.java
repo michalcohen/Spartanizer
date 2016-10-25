35c3e6dd66c8eebc085be50853c96165d4805f5c
orimarco <marcovitch.ori@gmail.com>
2016-10-25 14:19:48 +0300

    fixed?

---
+++ src/main/java/il/org/spartan/spartanizer/research/patterns/ApplyToEach.java
@@ -0,0 +1,54 @@
+package il.org.spartan.spartanizer.research.patterns;
+
+import java.util.*;
+
+import org.eclipse.jdt.core.dom.*;
+import org.eclipse.jdt.core.dom.rewrite.*;
+import org.eclipse.text.edits.*;
+
+import il.org.spartan.spartanizer.ast.navigate.*;
+import il.org.spartan.spartanizer.ast.safety.*;
+import il.org.spartan.spartanizer.engine.*;
+import il.org.spartan.spartanizer.research.*;
+import il.org.spartan.spartanizer.tipping.*;
+
+/** @author Ori Marcovitch
+ * @since 2016 */
+public class ApplyToEach extends NanoPatternTipper<EnhancedForStatement> {
+  Set<UserDefinedTipper<EnhancedForStatement>> tippers = new HashSet<UserDefinedTipper<EnhancedForStatement>>() {
+    static final long serialVersionUID = 1L;
+    {
+      add(TipperFactory.tipper("for($N1 $N2 : $X) $N2.$N3($A);", "on($X).apply(¢ -> ¢.$N3($A));", ""));
+      add(TipperFactory.tipper("for($N1 $N2 : $X) $N3($N2);", "on($X).apply(¢ -> $N3(¢));", ""));
+    }
+  };
+
+  @Override public boolean canTip(final EnhancedForStatement s) {
+    for (final UserDefinedTipper<EnhancedForStatement> ¢ : tippers)
+      if (¢.canTip(s))
+        return true;
+    return false;
+  }
+
+  @Override public String description(@SuppressWarnings("unused") final EnhancedForStatement __) {
+    return "ApplyToEach pattern: conevrt to fluent API";
+  }
+
+  @Override public Tip tip(EnhancedForStatement s) {
+    return new Tip(description(s), s, this.getClass()) {
+      @Override public void go(final ASTRewrite r, final TextEditGroup g) {
+        for (final UserDefinedTipper<EnhancedForStatement> ¢ : tippers)
+          if (¢.canTip(s))
+            try {
+              ¢.tip(s).go(r, g);
+              idiomatic.addImport(az.compilationUnit(searchAncestors.forClass(CompilationUnit.class).from(s)), r);
+              Logger.logNP(s, getClass() + "");
+              return;
+            } catch (final TipperFailure x1) {
+              x1.printStackTrace();
+            }
+        assert false;
+      }
+    };
+  }
+}
