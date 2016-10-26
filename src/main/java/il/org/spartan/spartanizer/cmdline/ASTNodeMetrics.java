7839130201b90f02676f52118f5920d77bd1792a
M4tt30rru <matt.orru@gmail.com>
2016-10-26 13:29:13 +0300

    getters

--- src/main/java/il/org/spartan/spartanizer/cmdline/ASTNodeMetrics.java
+++ src/main/java/il/org/spartan/spartanizer/cmdline/ASTNodeMetrics.java
@@ -7,6 +7,11 @@ import org.eclipse.jdt.core.dom.*;
 import il.org.spartan.spartanizer.ast.navigate.*;
 import il.org.spartan.spartanizer.ast.safety.*;
 
+/**
+ * A Class that contains all the metrics for an {@link ASTNode}
+ * @author Matteo Orru'
+ */
+
 @SuppressWarnings("unused")
 public class ASTNodeMetrics {
   
@@ -33,6 +38,62 @@ public class ASTNodeMetrics {
     this.statements = extract.statements(az.methodDeclaration(n).getBody()).size();
     this.tide = clean(n + "").length();
     this.essence = Essence.of(n + "").length();
+  }
+
+  /**
+   * @return the n
+   */
+  public ASTNode getN() {
+    return n;
+  }
+
+  /**
+   * @return the length
+   */
+  public int getLength() {
+    return length;
+  }
+
+  /**
+   * @return the tokens
+   */
+  public int getTokens() {
+    return tokens;
+  }
+
+  /**
+   * @return the nodes
+   */
+  public int getNodes() {
+    return nodes;
+  }
+
+  /**
+   * @return the body
+   */
+  public int getBody() {
+    return body;
+  }
+
+  /**
+   * @return the statements
+   */
+  public int getStatements() {
+    return statements;
+  }
+
+  /**
+   * @return the tide
+   */
+  public int getTide() {
+    return tide;
+  }
+
+  /**
+   * @return the essence
+   */
+  public int getEssence() {
+    return essence;
   }
   
 }
