f5669e30d07388fb24dec5d14e0553c4c40abf02
orimarco <marcovitch.ori@gmail.com>
2016-10-28 09:42:38 +0300

    created test for #552

---
+++ src/test/java/il/org/spartan/spartanizer/tippers/Issue552.java
@@ -0,0 +1,22 @@
+package il.org.spartan.spartanizer.tippers;
+
+import static il.org.spartan.azzert.*;
+import static il.org.spartan.spartanizer.engine.into.*;
+import static il.org.spartan.spartanizer.tippers.TrimmerTestsUtils.*;
+
+import org.junit.*;
+import org.junit.runners.*;
+
+import il.org.spartan.*;
+import il.org.spartan.spartanizer.ast.factory.*;
+import il.org.spartan.spartanizer.ast.navigate.*;
+import il.org.spartan.spartanizer.engine.*;
+import il.org.spartan.spartanizer.java.*;
+
+/** Tests of {@link count.expressions}
+ * @author Ori Marcovitch
+ * @since 2016 */
+@FixMethodOrder(MethodSorters.NAME_ASCENDING) //
+@SuppressWarnings({ "static-method", "javadoc" }) //
+public class Issue552 {
+}
