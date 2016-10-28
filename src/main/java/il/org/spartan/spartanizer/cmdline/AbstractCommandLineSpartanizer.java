0666f267ab9241c3c037735a5edbb83e0147389c
M4tt30rru <matt.orru@gmail.com>
2016-10-28 21:12:29 +0300

    cleanup

--- src/main/java/il/org/spartan/spartanizer/cmdline/AbstractCommandLineSpartanizer.java
+++ src/main/java/il/org/spartan/spartanizer/cmdline/AbstractCommandLineSpartanizer.java
@@ -31,46 +31,4 @@ import il.org.spartan.spartanizer.ast.safety.*;
     // runEssence();
     // runWordCount();
   }
-
-  // running report
-  public void runReport(final Consumer<CSVStatistics> __) {
-    for (NamedFunction ¢ : functions) {
-      ¢.function().run(null);
-      ¢.name();
-    }
-  }
-
-  @FunctionalInterface public interface ToInt<R> {
-    int run(R r);
-  }
-
-  static NamedFunction m(final String name, final ToInt<ASTNode> f) {
-    return new NamedFunction(name, f);
-  }
-
-  static class NamedFunction {
-    NamedFunction(final String name, final ToInt<ASTNode> f) {
-      this.name = name;
-      this.f = f;
-    }
-
-    final String name;
-    final ToInt<ASTNode> f;
-    
-    public String name(){
-      return this.name;
-    }
-    
-    public ToInt<ASTNode> function(){
-      return this.f;
-    }
-  }
-
-  NamedFunction functions[] = as.array(//
-      m("seventeeen", (¢) -> 17), //
-      m("length", (¢) -> (¢ + "").length()), //
-      m("essence", (¢) -> Essence.of(¢ + "").length()), m("tokens", (¢) -> metrics.tokens(¢ + "")), m("nodes", (¢) -> count.nodes(¢)), //
-      m("body", (¢) -> metrics.bodySize(¢)),
-      m("methodDeclaration", (¢) -> az.methodDeclaration(¢) == null ? -1 : extract.statements(az.methodDeclaration(¢).getBody()).size()),
-      m("tide", (¢) -> clean(¢ + "").length()));
 }
