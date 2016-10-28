3f6b4e989fbba00591c8b89c735eb6f724b19316
orimarco <marcovitch.ori@gmail.com>
2016-10-28 11:05:16 +0300

    refactoring #552

--- src/main/java/il/org/spartan/spartanizer/utils/tdd/count.java
+++ src/main/java/il/org/spartan/spartanizer/utils/tdd/count.java
@@ -2,13 +2,14 @@ package il.org.spartan.spartanizer.utils.tdd;
 
 import org.eclipse.jdt.core.dom.*;
 
+import il.org.spartan.spartanizer.ast.safety.*;
 import il.org.spartan.spartanizer.utils.*;
 
 /** @author Ori Marcovitch
  * @since Oct 28, 2016 */
 public interface count {
   /** @author Ori Marcovitch
-   * @param astNode
+   * @param n JD
    * @since Oct 28, 2016 */
   static int expressions(ASTNode n) {
     if (n == null)
@@ -16,8 +17,8 @@ public interface count {
     final Int $ = new Int();
     $.inner = 0;
     n.accept(new ASTVisitor() {
-      @Override public void preVisit(ASTNode node) {
-        if (node instanceof Expression)
+      @Override public void preVisit(ASTNode ¢) {
+        if (iz.expression(¢))
           ++$.inner;
       }
     });
